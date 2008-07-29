f8e6d96f0699287cb3f59bdc7b2d4670044f2dc0
Robert Cimrman <cimrman3@ntc.zcu.cz>
2008-07-29 16:02:27 +0200

    allow complex multiplicative constant in equations

--- sfepy/fem/parseEq.py
+++ sfepy/fem/parseEq.py
@@ -3,8 +3,6 @@ from pyparsing import Combine, Literal, Word, delimitedList, Group, Optional,\
      StringStart, StringEnd, CaselessLiteral
 
 
-##
-# 12.02.2007, c
 class TermParse( object ):
     def __str__( self ):
         ss = "%s\n" % self.__class__
@@ -12,14 +10,6 @@ class TermParse( object ):
             ss += "  %s:\n    %s\n" % (key, self.__dict__[key])
         return ss
 
-##
-# 11.05.2006, c
-# 01.08.2006
-# 09.08.2006
-# 12.02.2007
-# 10.04.2007
-# 23.04.2007
-# 13.11.2007
 def collect_term( term_descs, lc, itps ):
     signs = {'+' : 1.0, '-': -1.0}
     def append( str, loc, toks ):
@@ -31,15 +21,15 @@ def collect_term( term_descs, lc, itps ):
         tp.integral = toks.term_desc.integral
         tp.region = toks.term_desc.region
         tp.flag = toks.term_desc.flag
-        tp.sign = sign * float( toks.mul[0] )
+        if toks.mul[1]:
+            tp.sign = sign * complex( ''.join( toks.mul ) )
+        else:
+            tp.sign = sign * float( toks.mul[0] )
         tp.name = term_prefix + toks.term_desc.name
         tp.args = toks.args
-#        print tp
         term_descs.append( tp )
     return append
 
-##
-# 21.05.2006, c
 def rhs( lc ):
     def aux( str, loc, toks ):
         if toks:
@@ -47,10 +37,10 @@ def rhs( lc ):
             lc[0] = '-'
     return aux
         
-##
-# c: 21.05.2006, r: 08.07.2008
 def create_bnf( term_descs, itps ):
-    """term_descs .. list of TermParse objects (sign, term_name, term_arg_names)"""
+    """term_descs .. list of TermParse objects
+    (sign, term_name, term_arg_names), where sign can be real or complex
+    multiplier"""
 
     lc = ['+'] # Linear combination context.
     equal = Literal( "=" ).setParseAction( rhs( lc ) )
@@ -62,6 +52,7 @@ def create_bnf( term_descs, itps ):
     fnumber = Combine( Word( "+-"+nums, nums ) + 
                        Optional( point + Optional( Word( nums ) ) ) +
                        Optional( e + Word( "+-"+nums, nums ) ) )
+    number = fnumber + Optional( Literal( 'j' ), default = '' )
     lbracket = Literal( '[' ).suppress()
     rbracket = Literal( ']' ).suppress()
 
@@ -73,8 +64,8 @@ def create_bnf( term_descs, itps ):
     flag = Literal( 'a' )
 
     term = Optional( Literal( '+' ) | Literal( '-' ), default = '+' )( "sign" )\
-           + Optional( fnumber + Literal( '*' ).suppress(),
-                       default = '1.0' )( "mul" ) \
+           + Optional( number + Literal( '*' ).suppress(),
+                       default = ['1.0', ''] )( "mul" ) \
            + Combine( ident( "name" )\
                       + Optional( "." + (ident( "integral" ) + "."
                                   + ident( "region" ) + "." + flag( "flag" ) |
