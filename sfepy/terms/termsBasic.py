49352aa19ffd89dbfbd8aba0838ff751cf54c3ee
Robert Cimrman <cimrman3@ntc.zcu.cz>
2008-12-09 14:03:49 +0100

    fixed dw_volume_integrate

--- sfepy/terms/termsBasic.py
+++ sfepy/terms/termsBasic.py
@@ -45,7 +45,7 @@ class IntegrateVolumeOperatorTerm( Term ):
         ap, vg = virtual.get_approximation( self.get_current_group(), 'Volume' )
         n_el, n_qp, dim, n_ep = ap.get_v_data_shape( self.integral_name )
 
-        field_dim = par.field.dim[0]
+        field_dim = virtual.field.dim[0]
         assert_( field_dim == 1 )
         
         if diff_var is None:
