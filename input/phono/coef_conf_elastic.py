01ec0cb33e9c0f2d575107bd6d9569289f43fb77
Robert Cimrman <cimrman3@ntc.zcu.cz>
2008-12-12 16:03:46 +0100

    phono: input update

--- input/phono/coef_conf_elastic.py
+++ input/phono/coef_conf_elastic.py
@@ -1,31 +1,79 @@
 from sfepy.fem.periodic import *
+from sfepy.homogenization.coefs import CorrDimDim, ShapeDimDim, CoefSymSym
+
 from regions import get_box_regions
+
+def expand_regions( eqs, expand ):
+    out = {}
+    for key, val in eqs.iteritems():
+        out[key] = val % expand
+    return out
+
+class CorrectorsRS( CorrDimDim ):
+    """Steady state correctors $\bar{\omega}^{rs}$."""
+
+    def get_variables( self, ir, ic, data ):
+        """data: pis"""
+        pis = data[self.requires[0]]
+        yield (self.variables[-1], pis[ir,ic])
+
+class ElasticCoef( CoefSymSym ):
+    """Homogenized elastic tensor $E_{ijkl}$."""
+
+    mode2var = {'row' : 0, 'col' : 1}
+
+    def get_variables( self, problem, ir, ic, data, mode ):
+        pis, corrs = [data[ii] for ii in self.requires]
+
+        var_name = self.variables[self.mode2var[mode]]
+        u_name = problem.variables[var_name].primary_var_name
+
+        indx = corrs.di.indx[u_name]
+        omega = corrs.states[ir,ic][indx]
+        pi = pis[ir,ic] + omega
+        yield (var_name, pi)
+
+expr_elastic = """dw_lin_elastic.i2.%s( matrix.D, Pi1, Pi2 )"""
+
+eq_rs = {
+    'eq' : """dw_lin_elastic.i2.%s( matrix.D, v1, u1 )
+            + dw_lin_elastic.i2.%s( matrix.D, v1, Pi ) = 0""",
+}
 
 def define_input( filename, region, dim, geom ):
     """Uses materials, fe of master file, merges regions."""
     filename_mesh = filename
     
+    options = {
+        'coefs' : 'coefs',
+        'requirements' : 'requirements',
+    }
+
     coefs = {
-        'E' : {'requires' : ['pis', 'corrs_phono_rs'],
-               'variables' : ['Pi1', 'Pi2'],
-               'region' : region,
-               'expression' : 'dw_lin_elastic.i2.%s( matrix.D, Pi1, Pi2 )' \
-               % region},
+        'elastic' : {
+            'mat_name' : 'C',
+            'requires' : ['pis', 'corrs_phono_rs'],
+            'variables' : ['Pi1', 'Pi2', 'pi1', 'pi2'],
+            'expression' : expr_elastic % region,
+            'class' : ElasticCoef,
+        },
     }
 
     all_periodic = ['periodic_%s' % ii for ii in ['x', 'y', 'z'][:dim] ]
+
     requirements = {
         'pis' : {
             'variables' : ['u1'],
+            'class' : ShapeDimDim,
         },
         'corrs_phono_rs' : {
-             'variables' : ['u1', 'v1', 'Pi'],
-             'ebcs' : ['fixed_u'],
-             'epbcs' : all_periodic,
-             'equations' : {'eq' : """dw_lin_elastic.i2.%s( matrix.D, v1, u1 )
-                            + dw_lin_elastic.i2.%s( matrix.D, v1, Pi ) = 0""" \
-                            % (region, region)},
-        
+            'requires' : ['pis'],
+            'variables' : ['u1', 'v1', 'Pi'],
+            'ebcs' : ['fixed_u'],
+            'epbcs' : all_periodic,
+            'equations' : expand_regions( eq_rs, (region, region) ),
+            'class' : CorrectorsRS,
+            'save_name' : 'corrs_phono',
         },
     }
 
