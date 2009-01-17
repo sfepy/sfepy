41d792e661825ac9098228acfd387d19679da857
Robert Cimrman <cimrman3@ntc.zcu.cz>
2009-01-17 18:43:49 +0100

    hom: added new-style elastic corrector and coefficient classes
    - CorrectorsElasticRS, ElasticCoef inherit from base classes
      in coefs_base.py

---
+++ sfepy/homogenization/coefs_elastic.py
@@ -0,0 +1,27 @@
+from sfepy.base.base import *
+from sfepy.homogenization.coefs import CoefSymSym
+from sfepy.homogenization.coefs import CorrDimDim, ShapeDimDim
+
+class CorrectorsElasticRS( CorrDimDim ):
+
+    def get_variables( self, ir, ic, data ):
+        """data: pis"""
+        pis = data[self.requires[0]]
+        yield (self.variables[-1], pis[ir,ic])
+
+class ElasticCoef( CoefSymSym ):
+    """Homogenized elastic tensor $E_{ijkl}$."""
+
+    mode2var = {'row' : 0, 'col' : 1}
+
+    def get_variables( self, problem, ir, ic, data, mode ):
+
+        pis, corrs = [data[ii] for ii in self.requires]
+
+        var_name = self.variables[self.mode2var[mode]]
+        c_name = problem.variables[var_name].primary_var_name
+
+        indx = corrs.di.indx[c_name]
+        omega = corrs.states[ir,ic][indx]
+        pi = pis[ir,ic] + omega
+        yield (var_name, pi)
