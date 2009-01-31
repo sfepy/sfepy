8f299891aee886492a9d2241520d3124d1629083
Robert Cimrman <cimrman3@ntc.zcu.cz>
2009-02-01 00:02:57 +0100

    convert_mesh.py: added --scale option

--- script/convert_mesh.py
+++ script/convert_mesh.py
@@ -3,6 +3,7 @@ import sys
 sys.path.append('.')
 
 from optparse import OptionParser
+import numpy as nm 
 from sfepy.fem import Mesh
 
 usage = """%prog [options] filename_in filename_out
@@ -10,8 +11,15 @@ usage = """%prog [options] filename_in filename_out
 Convert a mesh file from one SfePy-supported format to another.
 """
 
+help = {
+    'scale' : 'scale factor [default: %default]',
+}
+
 def main():
-    parser = OptionParser( usage = usage )
+    parser = OptionParser(usage=usage)
+    parser.add_option("-s", "--scale", type=int, metavar='scale',
+                      action="store", dest="scale",
+                      default=None, help=help['scale'])
     (options, args) = parser.parse_args()
 
     if len(args) != 2:
@@ -21,6 +29,11 @@ def main():
     filename_in, filename_out = args
     
     mesh = Mesh.from_file(filename_in)
+
+    if options.scale is not None:
+        tr = nm.eye(mesh.dim, dtype=nm.float64) * options.scale
+        mesh.transform_coords(tr)
+
     mesh.write(filename_out, io='auto')
 
 if __name__ == '__main__':
