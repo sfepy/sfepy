66af299666ca2b086b2585f5eca4e89455ddb289
Robert Cimrman <cimrman3@ntc.zcu.cz>
2009-02-25 17:27:59 +0100

    hom: added CorrectorsPermeability, DiffusionCoef classes

--- sfepy/homogenization/coefs_elastic.py
+++ sfepy/homogenization/coefs_elastic.py
@@ -1,6 +1,6 @@
 from sfepy.base.base import *
 from sfepy.homogenization.coefs_base import CoefSymSym, CoefSym, CorrDimDim,\
-     CoefOne, CorrOne, ShapeDimDim, TSTimes, VolumeFractions
+     CoefOne, CorrOne, CorrDim, CoefDimDim, ShapeDimDim, TSTimes, VolumeFractions
 
 class CorrectorsElasticRS( CorrDimDim ):
 
@@ -24,6 +24,38 @@ class CorrectorsPressure( CorrOne ):
         one_m = nm.ones( (vv[name_ym].field.n_nod,), dtype = nm.float64 )
         yield name_ym, one_m
 
+class CorrectorsPermeability( CorrDim ):
+
+    def __call__( self, problem = None, data = None, save_hook = None ):
+        problem = get_default( problem, self.problem )
+
+        problem.select_variables( self.variables )
+        equations = {}
+        for key, eq in self.equations.iteritems():
+            equations[key] = eq % tuple( self.regions )
+        problem.set_equations( equations )
+
+        problem.select_bcs( ebc_names = self.ebcs, epbc_names = self.epbcs )
+
+        dim = problem.domain.mesh.dim
+        states = nm.zeros( (dim,), dtype = nm.object )
+        for ir in range( dim ):
+            state = problem.solve( ir = ir )
+            assert_( problem.variables.has_ebc( state ) )
+            states[ir] = state
+
+            if save_hook is not None:
+                save_hook( state, problem, ir )
+
+        return Struct( name = self.name,
+                       states = states,
+                       di = problem.variables.di )
+
+    def make_save_hook( self, base_name, format,
+                        post_process_hook = None, file_per_var = None ):
+        return CorrDim.make_save_hook( self, base_name, format,
+                                       post_process_hook = None,
+                                       file_per_var = None )
 
 class ElasticCoef( CoefSymSym ):
     """Homogenized elastic tensor $E_{ijkl}$."""
@@ -86,3 +118,28 @@ class IRBiotModulus( CoefOne ):
             indx = corrs.di.indx[c_name]
             val = corrs.state[indx]
             yield var_name, val
+
+class DiffusionCoef( CoefDimDim ):
+    """Homogenized diffusion coefficient."""
+
+    mode2var = {'row' : 0, 'col' : 1}
+
+    def get_variables( self, problem, ir, ic, data, mode ):
+
+        pressure = problem.variables[self.variables[0]]
+        coor = pressure.field.get_coor()
+        
+        corrs = [data[ii] for ii in self.requires][0]
+
+        var_name = self.variables[self.mode2var[mode]]
+        c_name = problem.variables[var_name].primary_var_name
+        indx = corrs.di.indx[c_name]
+
+        if mode == 'row':
+            ii = ir
+        else:
+            ii = ic
+
+        val = coor[:,ii] + corrs.states[ii][indx]
+
+        yield var_name, val
