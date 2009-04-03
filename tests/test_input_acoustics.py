10098740b9e0c1ee5ac5f9ddae0caaa8177676e6
Vladimir Lukes <lukes@kme.zcu.cz>
2009-04-03 17:11:37 +0200

    'test_input_complex' replaced by 'test_input_acoustics'

---
+++ tests/test_input_acoustics.py
@@ -0,0 +1,6 @@
+input_name = 'input/acoustics.py'
+output_name = 'test_acoustics.vtk'
+
+from testsBasic import TestInput
+class Test( TestInput ):
+    pass
--- tests/test_input_complex.py
+++
@@ -1,6 +0,0 @@
-input_name = 'input/complex.py'
-output_name = 'test_complex.vtk'
-
-from testsBasic import TestInput
-class Test( TestInput ):
-    pass
