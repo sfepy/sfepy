a5d69f004796f7b6aa2554e8d8c0e131ca3b6108
Robert Cimrman <cimrman3@ntc.zcu.cz>
2009-05-13 16:52:05 +0200

    probe.py: added --auto-dir, --same-dir options

--- probe.py
+++ probe.py
@@ -23,6 +23,11 @@ help = {
     'output_format' :
     'output file format (supported by the matplotlib backend used) '\
     '[default: %default]',
+    'auto_dir' :
+    'the directory of the results file is determined automatically using the '\
+    '"output_dir" option in input file options',
+    'same_dir' :
+    'store the probe figures in the directory of the results file',
 }
 
 def main():
@@ -30,6 +35,12 @@ def main():
     parser.add_option( "-o", "", metavar = 'filename',
                        action = "store", dest = "output_filename_trunk",
                        default = None, help = help['filename'] )
+    parser.add_option( "", "--auto-dir",
+                       action = "store_true", dest = "auto_dir",
+                       default = False, help = help['auto_dir'] )
+    parser.add_option( "", "--same-dir",
+                       action = "store_true", dest = "same_dir",
+                       default = False, help = help['same_dir'] )
     parser.add_option("-f", "--format", metavar='format',
                       action="store", dest="output_format",
                       default="png", help=help['output_format'])
@@ -47,6 +58,12 @@ def main():
     conf = ProblemConf.from_file( filename_input, required, other )
     opts = conf.options
     output.prefix = get_default_attr( opts, 'output_prefix', 'probe:' )
+
+    if options.auto_dir:
+        output_dir = get_default_attr( opts, 'output_dir', '.' )
+        filename_results = os.path.join(output_dir, filename_results)
+
+    output('results in: %s' % filename_results)
 
     io = MeshIO.any_from_filename(filename_results)
     data = io.read_data(0)
@@ -66,6 +83,10 @@ def main():
 
     filename_template = options.output_filename_trunk \
                         + ('_%%d.%s' % options.output_format)
+    if options.same_dir:
+        filename_template = os.path.join(os.path.dirname(filename_results),
+                                         filename_template)
+
     for ip, probe in enumerate(probes):
         output(ip, probe.name)
         fig = probe_hook(data, probe, labels[ip], problem)
