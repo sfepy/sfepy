05963169ca026abb5a1f01ecd6dbc1aaeb797137
Robert Cimrman <cimrman3@ntc.zcu.cz>
2009-05-21 10:45:09 +0200

    updated tests/test_linear_solvers.py
    ls.scipy_direct with superlu disabled, as it is extremely slow

--- tests/test_linear_solvers.py
+++ tests/test_linear_solvers.py
@@ -40,83 +40,62 @@ equations = {
     'Temperature' : """dw_laplace.i1.Omega( coef.val, s, t ) = 0"""
 }
 
-solver_100 = {
-    'name' : 'dls100',
-    'kind' : 'ls.umfpack',
+solvers = {
+    'd00' : ('ls.umfpack',
+             {'warn' : True,}
+    ),
+##     'd01' : ('ls.scipy_direct',
+##              {'method' : 'superlu',
+##               'warn' : True,}
+##     ),
+    'i00' : ('ls.pyamg',
+             {'method' : 'ruge_stuben_solver',
+              'eps_a'   : 1e-12,}
+    ),
+    'i01' : ('ls.pyamg',
+             {'method' : 'smoothed_aggregation_solver',
+              'eps_a'   : 1e-12,}
+    ),
+    'i10' : ('ls.petsc',
+             {'method' : 'cg', # ksp_type
+              'precond' : 'icc', # pc_type
+              'eps_a' : 1e-12, # abstol
+              'eps_r' : 1e-12, # rtol
+              'i_max' : 1000,} # maxits
+    ),
+    'i20' : ('ls.scipy_iterative',
+             {'method' : 'cg',
+              'i_max'   : 1000,
+              'eps_a'   : 1e-12,}
+    ),
+    'i21' : ('ls.scipy_iterative',
+             {'method' : 'bicgstab',
+              'i_max'   : 1000,
+              'eps_a'   : 1e-12,}
+    ),
+    'i22' : ('ls.scipy_iterative',
+             {'method' : 'qmr',
+              'i_max'   : 1000,
+              'eps_a'   : 1e-12,}
+    ),
+
+    'newton' : ('nls.newton',
+                {'i_max'      : 1,
+                 'eps_a'      : 1e-10,
+                 'eps_r'      : 1.0,
+                 'macheps'   : 1e-16,
+                 'lin_red'    : 1e-1, # Linear system error < (eps_a * lin_red).
+                 'ls_red'     : 0.1,
+                 'ls_red_warp' : 0.001,
+                 'ls_on'      : 1.1,
+                 'ls_min'     : 1e-5,
+                 'check'     : 0,
+                 'delta'     : 1e-6,
+                 'is_plot'    : False,
+                 'problem'   : 'nonlinear',}
+    ),
 }
 
-solver_101 = {
-    'name' : 'ls101',
-    'kind' : 'ls.pyamg',
-
-    'method' : 'ruge_stuben_solver',
-    'eps_a'   : 1e-12,
-}
-
-solver_102 = {
-    'name' : 'ls102',
-    'kind' : 'ls.pyamg',
-
-    'method' : 'smoothed_aggregation_solver',
-    'eps_a'   : 1e-12,
-}
-
-solver_120 = {
-    'name' : 'ls120',
-    'kind' : 'ls.petsc',
-
-    'method' : 'cg', # ksp_type
-    'precond' : 'icc', # pc_type
-    'eps_a' : 1e-12, # abstol
-    'eps_r' : 1e-12, # rtol
-    'i_max' : 1000, # maxits
-}
-
-solver_110 = {
-    'name' : 'ls110',
-    'kind' : 'ls.scipy_iterative',
-
-    'method' : 'cg',
-    'i_max'   : 1000,
-    'eps_a'   : 1e-12,
-}
-
-solver_111 = {
-    'name' : 'ls111',
-    'kind' : 'ls.scipy_iterative',
-
-    'method' : 'bicgstab',
-    'i_max'   : 1000,
-    'eps_a'   : 1e-12,
-}
-
-solver_112 = {
-    'name' : 'ls112',
-    'kind' : 'ls.scipy_iterative',
-
-    'method' : 'qmr',
-    'i_max'   : 1000,
-    'eps_a'   : 1e-12,
-}
-
-solver_1 = {
-    'name' : 'newton',
-    'kind' : 'nls.newton',
-
-    'i_max'      : 1,
-    'eps_a'      : 1e-10,
-    'eps_r'      : 1.0,
-    'macheps'   : 1e-16,
-    'lin_red'    : 1e-1, # Linear system error < (eps_a * lin_red).
-    'ls_red'     : 0.1,
-    'ls_red_warp' : 0.001,
-    'ls_on'      : 1.1,
-    'ls_min'     : 1e-5,
-    'check'     : 0,
-    'delta'     : 1e-6,
-    'is_plot'    : False,
-    'problem'   : 'nonlinear', # 'nonlinear' or 'linear' (ignore i_max)
-}
 
 options = {
     'nls' : 'newton',
