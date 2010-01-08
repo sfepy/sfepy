fad49428496549840e78228327c28cbdbbee4d40
Robert Cimrman <cimrman3@ntc.zcu.cz>
2010-01-08 15:36:25 +0100

    use find_subclasses()

--- sfepy/solvers/__init__.py
+++ sfepy/solvers/__init__.py
@@ -7,29 +7,13 @@ from ts import *
 from eigen import *
 from optimize import *
 
-##
-# c: 16.10.2007, r: 03.03.2008
-var_dict = vars().items()
-solver_table = {}
+solver_table = find_subclasses(vars(),
+                               [LinearSolver, NonlinearSolver,
+                                TimeSteppingSolver, EigenvalueSolver,
+                                OptimizationSolver])
 
-for key, var in var_dict:
-    try:
-        if is_derived_class( var, LinearSolver ) or \
-               is_derived_class( var, NonlinearSolver ) or \
-               is_derived_class( var, TimeSteppingSolver ) or \
-               is_derived_class( var, EigenvalueSolver ) or \
-               is_derived_class( var, OptimizationSolver ):
-            solver_table[var.name] = var
-    except TypeError:
-        pass
-del var_dict
-
-## print solver_table
-## pause()
-
-##
-# 23.10.2007, c
-def any_from_conf( conf, **kwargs ):
-    return solver_table[conf.kind]( conf, **kwargs )
-insert_static_method( Solver, any_from_conf )
+def any_from_conf(conf, **kwargs):
+    """Create an instance of a solver class according to the configuration."""
+    return solver_table[conf.kind](conf, **kwargs)
+insert_static_method(Solver, any_from_conf)
 del any_from_conf
