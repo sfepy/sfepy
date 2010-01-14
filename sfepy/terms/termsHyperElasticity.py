015fc2c8f5812e762e438d7a592d31d99fa83996
Robert Cimrman <cimrman3@ntc.zcu.cz>
2010-01-14 10:31:09 +0100

    perfusion: new CouplingVectorScalarTL class, use VectorVector

--- sfepy/terms/termsHyperElasticity.py
+++ sfepy/terms/termsHyperElasticity.py
@@ -1,4 +1,47 @@
 from sfepy.terms.terms import *
+from sfepy.terms.terms_base import VectorVector
+
+class CouplingVectorScalarTL(Struct):
+    
+    def set_data_shape(self, apv, aps):
+        """Set element data shape and checks dimensions of approximations."""
+        if self.dof_conn_type == 'surface':
+            self.data_shape_v = apv.get_s_data_shape(self.integral_name,
+                                                     self.region.name)
+            self.data_shape_s = aps.get_s_data_shape(self.integral_name,
+                                                     self.region.name)
+        else:
+            self.data_shape_v = apv.get_v_data_shape(self.integral_name)
+            self.data_shape_s = aps.get_v_data_shape(self.integral_name)
+
+        assert_(aps.dim == (1,))
+        assert_(apv.dim == self.data_shape_v[2])
+
+    def get_shape_div(self, diff_var, chunk_size):
+        n_el, n_qp, dim, n_eps = self.data_shape_s
+
+        if diff_var is None:
+            return (chunk_size, 1, n_eps, 1), 0
+        elif diff_var == self.get_arg_name( 'state_p' ):
+            return (chunk_size, 1, n_eps, n_eps), 1
+        elif diff_var == self.get_arg_name( 'state' ):
+            n_epv = self.data_shape_v[3]
+            return (chunk_size, 1, n_eps, dim * n_epv), 2
+        else:
+            raise StopIteration
+
+    def get_shape_grad(self, diff_var, chunk_size):
+        n_el, n_qp, dim, n_epv = self.data_shape_v
+
+        if diff_var is None:
+            return (chunk_size, 1, dim * n_epv, 1), 0
+        elif diff_var == self.get_arg_name( 'state' ):
+            return (chunk_size, 1, dim * n_epv, dim * n_epv), 1
+        elif diff_var == self.get_arg_name( 'state_p' ):
+            n_eps = self.data_shape_s[3]
+            return (chunk_size, 1, dim * n_epv, n_eps), 2
+        else:
+            raise StopIteration
 
 class HyperElasticBase( Term ):
     """Base class for all hyperelastic terms in TL/UL formulation. This is not a
@@ -24,23 +67,13 @@ class HyperElasticBase( Term ):
         self.crt_data = Struct( stress = None,
                                 tan_mod = nm.array( [0], ndmin = 4 ) )
 
-    def get_shape( self, diff_var, chunk_size, apr, apc = None ):
-        self.data_shape = apr.get_v_data_shape( self.integral_name )
-        n_el, n_qp, dim, n_ep = self.data_shape
-        
-        if diff_var is None:
-            return (chunk_size, 1, dim * n_ep, 1), 0
-        elif diff_var == self.get_arg_name( 'state' ):
-            return (chunk_size, 1, dim * n_ep, dim * n_ep), 1
-        else:
-            raise StopIteration
-
     def __call__( self, diff_var = None, chunk_size = None, **kwargs ):
         call_mode, = self.get_kwargs( ['call_mode'], **kwargs )
         virtual, state = self.get_args( ['virtual', 'state'], **kwargs )
         ap, vg = virtual.get_approximation( self.get_current_group(), 'Volume' )
 
-        shape, mode = self.get_shape( diff_var, chunk_size, ap )
+        self.set_data_shape(ap)
+        shape, mode = self.get_shape(diff_var, chunk_size)
 
         cache = self.get_cache( self.function['finite_strain'][self.mode_ul], 0 )
         family_data = cache( self.family_data_names,
@@ -49,7 +82,7 @@ class HyperElasticBase( Term ):
 
         if call_mode is None:
 
-            out = self.compute_crt_data( family_data, ap, vg, mode, **kwargs )
+            out = self.compute_crt_data( family_data, mode, **kwargs )
             if mode == 0:
                 self.crt_data.stress = out
             else:
@@ -72,8 +105,7 @@ class HyperElasticBase( Term ):
             if call_mode == 'de_strain':
                 out_qp = cache( 'E', self.get_current_group(), 0, state = state )
             elif call_mode == 'de_stress':
-                out_qp = self.compute_crt_data( family_data, ap, vg, 0,
-                                                **kwargs )
+                out_qp = self.compute_crt_data( family_data, 0, **kwargs )
                 
             shape = (chunk_size, 1) + out_qp.shape[2:]
             for out, chunk in self.char_fun( chunk_size, shape ):
@@ -91,7 +123,7 @@ class HyperElasticTLBase( HyperElasticBase ):
     def __init__( self, region, name = None, sign = 1 ):
         HyperElasticBase.__init__( self, region, name, sign, mode_ul = 0 )
 
-class NeoHookeanTLTerm( HyperElasticTLBase ):
+class NeoHookeanTLTerm( VectorVector, HyperElasticTLBase ):
     r""":description: Hyperelastic neo-Hookean term. Effective stress $S_{ij} =
     \mu J^{-\frac{2}{3}}(\delta_{ij} - \frac{1}{3}C_{kk}C_{ij}^{-1})$.
     :definition:
@@ -105,7 +137,7 @@ class NeoHookeanTLTerm( HyperElasticTLBase ):
     term_function = {'stress' : terms.dq_tl_he_stress_neohook,
                      'tangent_modulus' : terms.dq_tl_he_tan_mod_neohook}
     
-    def compute_crt_data( self, family_data, ap, vg, mode, **kwargs ):
+    def compute_crt_data( self, family_data, mode, **kwargs ):
         mat = self.get_args( ['material'], **kwargs )[0]
 
         detF, trC, invC = family_data
@@ -123,7 +155,7 @@ class NeoHookeanTLTerm( HyperElasticTLBase ):
 
         return out
 
-class MooneyRivlinTLTerm( HyperElasticTLBase ):
+class MooneyRivlinTLTerm( VectorVector, HyperElasticTLBase ):
     r""":description: Hyperelastic Mooney-Rivlin term. Effective stress $S_{ij}
     = \kappa J^{-\frac{4}{3}} (C_{kk} \delta_{ij} - C_{ij} - \frac{2}{3
     } I_2 C_{ij}^{-1})$.
@@ -138,7 +170,7 @@ class MooneyRivlinTLTerm( HyperElasticTLBase ):
     term_function = {'stress' : terms.dq_tl_he_stress_mooney_rivlin,
                      'tangent_modulus' : terms.dq_tl_he_tan_mod_mooney_rivlin}
 
-    def compute_crt_data( self, family_data, ap, vg, mode, **kwargs ):
+    def compute_crt_data( self, family_data, mode, **kwargs ):
         mat = self.get_args( ['material'], **kwargs )[0]
 
         detF, trC, invC, vecC, in2C = family_data
@@ -156,7 +188,7 @@ class MooneyRivlinTLTerm( HyperElasticTLBase ):
 
         return out
 
-class BulkPenaltyTLTerm( HyperElasticTLBase ):
+class BulkPenaltyTLTerm( VectorVector, HyperElasticTLBase ):
     r""":description: Hyperelastic bulk penalty term. Stress $S_{ij}
     = K(J-1)\; J C_{ij}^{-1}$.
     :definition:
@@ -171,7 +203,7 @@ class BulkPenaltyTLTerm( HyperElasticTLBase ):
     term_function = {'stress' : terms.dq_tl_he_stress_bulk,
                      'tangent_modulus' : terms.dq_tl_he_tan_mod_bulk }
 
-    def compute_crt_data( self, family_data, ap, vg, mode, **kwargs ):
+    def compute_crt_data( self, family_data, mode, **kwargs ):
         mat = self.get_args( ['material'], **kwargs )[0]
 
         detF, invC = family_data
@@ -198,7 +230,7 @@ class HyperElasticULBase( HyperElasticBase ):
     def __init__( self, region, name = None, sign = 1 ):
         HyperElasticBase.__init__( self, region, name, sign, mode_ul = 1 )
 
-class NeoHookeanULTerm( HyperElasticULBase ):
+class NeoHookeanULTerm( VectorVector, HyperElasticULBase ):
     r""":description: Hyperelastic neo-Hookean term. Effective stress
     $\tau_{ij} =
     \mu J^{-\frac{2}{3}}(b_{ij} - \frac{1}{3}b_{kk}\delta_{ij})$.
@@ -213,7 +245,7 @@ class NeoHookeanULTerm( HyperElasticULBase ):
     term_function = {'stress' : terms.dq_ul_he_stress_neohook,
                      'tangent_modulus' : terms.dq_ul_he_tan_mod_neohook}
 
-    def compute_crt_data( self, family_data, ap, vg, mode, **kwargs ):
+    def compute_crt_data( self, family_data, mode, **kwargs ):
         mat = self.get_args( ['material'], **kwargs )[0]
 
         detF, trB, B = family_data
@@ -231,7 +263,7 @@ class NeoHookeanULTerm( HyperElasticULBase ):
 
         return out
 
-class BulkPenaltyULTerm( HyperElasticULBase ):
+class BulkPenaltyULTerm( VectorVector, HyperElasticULBase ):
     r""":description: Hyperelastic bulk penalty term. Stress $\tau_{ij}
     = K(J-1)\; J \delta_{ij}$.
     """
@@ -243,7 +275,7 @@ class BulkPenaltyULTerm( HyperElasticULBase ):
     term_function = {'stress' : terms.dq_ul_he_stress_bulk,
                      'tangent_modulus' : terms.dq_ul_he_tan_mod_bulk}
             
-    def compute_crt_data( self, family_data, ap, vg, mode, **kwargs ):
+    def compute_crt_data( self, family_data, mode, **kwargs ):
         mat = self.get_args( ['material'], **kwargs )[0]
 
         detF, B = family_data
@@ -261,7 +293,7 @@ class BulkPenaltyULTerm( HyperElasticULBase ):
 
         return out
 
-class MooneyRivlinULTerm( HyperElasticULBase ):
+class MooneyRivlinULTerm( VectorVector, HyperElasticULBase ):
     r""":description: Hyperelastic Mooney-Rivlin term.
     :definition:
     $\int_{\Omega} \cal{L}\tau_{ij}(\ul{u}) e_{ij}(\delta\ul{v})/J$
@@ -274,7 +306,7 @@ class MooneyRivlinULTerm( HyperElasticULBase ):
     term_function = {'stress' : terms.dq_ul_he_stress_mooney_rivlin,
                      'tangent_modulus' : terms.dq_ul_he_tan_mod_mooney_rivlin}
 
-    def compute_crt_data( self, family_data, ap, vg, mode, **kwargs ):
+    def compute_crt_data( self, family_data, mode, **kwargs ):
         mat = self.get_args( ['material'], **kwargs )[0]
 
         detF, trB, B, in2B = family_data
