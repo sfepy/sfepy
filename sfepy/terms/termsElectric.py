52e9fe4271ee8bc55eca685b9820239bf803444b
Robert Cimrman <cimrman3@ntc.zcu.cz>
2010-01-24 18:27:23 +0100

    docs: update docstrings in termsElectric.py for sphinx

--- sfepy/terms/termsElectric.py
+++ sfepy/terms/termsElectric.py
@@ -1,11 +1,18 @@
 from sfepy.terms.terms import *
 
 class ElectricSourceTerm( Term ):
-    r""":description: Electric source term.
-    :definition: $\int_{\Omega} c s (\nabla \phi)^2$
-    :arguments: material : $c$ (electric conductivity),
-    virtual : $s$ (test function),
-    parameter : $\phi$ (given electric potential)
+    r"""
+    :Description:
+    Electric source term.
+
+    :Definition:
+    .. math::
+        \int_{\Omega} c s (\nabla \phi)^2
+
+    :Arguments:
+    material : :math:`c` (electric conductivity),
+    virtual : :math:`s` (test function),
+    parameter : :math:`\phi` (given electric potential)
     """
     name = 'dw_electric_source'
     arg_types = ('material', 'virtual', 'parameter')
