1ff4ce9a3506d9fd3d5b0671c332d92fa7f6e4a1
Robert Cimrman <cimrman3@ntc.zcu.cz>
2010-02-10 16:12:35 +0100

    interp: test interpolation of vectors, clean-up

--- tests/test_mesh_interp.py
+++ tests/test_mesh_interp.py
@@ -5,8 +5,10 @@ from sfepy.base.conf import transform_variables, transform_fields
 from sfepy.base.testing import TestCommon
 
 fields = {
-#    'f' : ((1,1), 'real', 'Omega', {'Omega' : '3_4_P1'}),
-    'f' : ((1,1), 'real', 'Omega', {'Omega' : '3_8_Q1'}),
+    'scalar_si' : ((1,1), 'real', 'Omega', {'Omega' : '3_4_P1'}),
+    'vector_si' : ((3,1), 'real', 'Omega', {'Omega' : '3_4_P1'}),
+    'scalar_tp' : ((1,1), 'real', 'Omega', {'Omega' : '3_8_Q1'}),
+    'vector_tp' : ((3,1), 'real', 'Omega', {'Omega' : '3_8_Q1'}),
 }
 
 variables = {
@@ -17,6 +19,38 @@ variables = {
 def in_dir(adir):
     return lambda x: op.join(adir, x)
 
+def do_interpolation(m2, m1, data, field_name):
+    """Interpolate data from m1 to m2. """
+    from sfepy.fem import Domain, Fields, Variables
+    d1 = Domain('d1', m1)
+
+    omega1 = d1.create_region('Omega', 'all')
+
+    fc = {'f' : fields[field_name]}
+    ff = Fields.from_conf(transform_fields(fc), d1.regions)
+    field1 = ff[0]
+
+    vv = Variables.from_conf(transform_variables(variables), ff)
+    vv.setup_dof_info()
+    u1 = vv['u']
+    u1.set_from_mesh_vertices(data)
+
+    d2 = Domain('d2', m2)
+    omega2 = d2.create_region('Omega', 'all')
+
+    ff2 = Fields.from_conf(transform_fields(fc), d2.regions)
+    field2 = ff2[0]
+
+    vv2 = Variables.from_conf(transform_variables(variables), ff2)
+    vv2.setup_dof_info()
+    u2 = vv2['u']
+
+    # Performs interpolation, if other field differs from self.field
+    # or, in particular, is defined on a different mesh.
+    u2.set_from_other(u1, strategy='interpolation')
+
+    return u1, u2
+ 
 class Test(TestCommon):
 
     @staticmethod
@@ -25,60 +59,42 @@ class Test(TestCommon):
         return test
 
     def test_interpolation(self):
-        from sfepy.fem import Mesh, Domain, Fields, Variables
+        from sfepy.fem import Mesh
         from sfepy.base.la import make_axis_rotation_matrix
 
-#        m1 = Mesh('original mesh', 'database/simple.mesh')
-#        m1 = Mesh('original mesh', 'database/kostka_medium_tetra.mesh')
-        m1 = Mesh('original mesh', 'database/tests/block.mesh')
-#        m1 = Mesh('original mesh', '../sfepy-projects/screw_press/meshes/screw_press_3.mesh')
+        fname = in_dir(self.options.out_dir)
 
-        for ii, angle in enumerate(nm.linspace(0.0, nm.pi, 11)):
-            print '>>>>>>>>>>>>>>>>', ii, angle
-            shift = nm.array([2.0, 0.5, 0.5]) * (ii / 10.0)
-#            shift = [0.0, 0.0, 0.0]
-            mtx = make_axis_rotation_matrix([0, 1, 0], angle)
+        m1 = Mesh('original mesh', 'meshes/3d/block.mesh')
 
-            m2 = m1.copy('rotated mesh')
-            m2.transform_coors(mtx)
-            m2.coors += shift
+        datas = {}
 
-            d1 = Domain('d1', m1)
+        bbox = m1.get_bounding_box()
+        nx = bbox[1,0] - bbox[0,0]
+        data = nm.sin(4.0 * nm.pi * m1.coors[:,0:1] / nx)
+        datas['scalar'] = data
 
-            omega1 = d1.create_region('Omega', 'all')
+        data = nm.zeros_like(m1.coors)
+        data[:,0] = 0.2 * nm.sin(4.0 * nm.pi * m1.coors[:,0] / nx)
+        data[:,2] = 0.2 * nm.cos(4.0 * nm.pi * m1.coors[:,0] / nx)
+        datas['vector'] = data
 
-            ff = Fields.from_conf(transform_fields(fields), d1.regions)
-            field1 = ff[0]
+        for field_name in ['scalar_tp', 'vector_tp']:
+            for ia, angle in enumerate(nm.linspace(0.0, nm.pi, 11)):
+                self.report('%s: %d. angle: %f' % (field_name, ia, angle))
+                shift = [0.0, 0.0, 0.0]
+                mtx = make_axis_rotation_matrix([0, 1, 0], angle)
 
-            vv = Variables.from_conf(transform_variables(variables), ff)
-            vv.setup_dof_info()
-            u1 = vv['u']
+                m2 = m1.copy('rotated mesh')
+                m2.transform_coors(mtx)
 
-            bbox = m1.get_bounding_box()
-            nx = bbox[1,0] - bbox[0,0]
-            data1 = nm.sin(4.0 * nm.pi * m1.coors[:,0:1] / nx)
-            u1.set_from_mesh_vertices(data1)
+                data = datas[field_name[:6]]
+                u1, u2 = do_interpolation(m2, m1, data, field_name)
 
-            d2 = Domain('d2', m2)
-            omega2 = d2.create_region('Omega', 'all')
+                if ia == 0:
+                    u1.save_as_mesh(fname('test_mesh_interp_%s_u1.vtk'
+                                          % field_name))
 
-            ff2 = Fields.from_conf(transform_fields(fields), d2.regions)
-            field2 = ff2[0]
-
-            vv2 = Variables.from_conf(transform_variables(variables), ff2)
-            vv2.setup_dof_info()
-            u2 = vv2['u']
-
-            # Performs interpolation, if other field differs from self.field
-            # or, in particular, is defined on a different mesh.
-            u2.set_from_other(u1, strategy='interpolation')
-##             u2.set_from_other(u1, strategy='crawl')
-
-            fname = in_dir(self.options.out_dir)
-
-            if ii == 0:
-                u1.save_as_mesh(fname('test_mesh_interp_u1.vtk'))
-
-            u2.save_as_mesh(fname('test_mesh_interp_u2.%03d.vtk' % ii))
-        
+                u2.save_as_mesh(fname('test_mesh_interp_%s_u2.%03d.vtk'
+                                      % (field_name, ia)))
+       
         return True
