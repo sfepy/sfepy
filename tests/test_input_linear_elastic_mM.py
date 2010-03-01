68abe5841eed01eabbde09710856439940fb5c10
Robert Cimrman <cimrman3@ntc.zcu.cz>
2010-03-01 16:56:44 +0100

    fix test output file name

--- tests/test_input_linear_elastic_mM.py
+++ tests/test_input_linear_elastic_mM.py
@@ -1,5 +1,6 @@
 input_name = '../examples/homogenization/linear_elastic_mM.py'
-output_name = 'cylinder.vtk'
+output_name = 'test_linear_elastic_mM.vtk'
+
 from testsBasic import TestInput
 class Test( TestInput ):
     pass
