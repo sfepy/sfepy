d0e0bb2f2d99b58ac24add8341597483eb9d2308
Robert Cimrman <cimrman3@ntc.zcu.cz>
2010-03-30 15:18:51 +0200

    new prepare_cylindrical_transform(), transform_data()

--- sfepy/mechanics/tensors.py
+++ sfepy/mechanics/tensors.py
@@ -2,6 +2,7 @@
 Functions to compute some tensor-related quantities usual in continuum mechanics.
 """
 from sfepy.base.base import *
+from sfepy.base.la import dot_sequences, make_axis_rotation_matrix
 
 def dim2sym(dim):
     """
@@ -95,3 +96,87 @@ def get_von_mises_stress(stress, sym_storage=True):
                          + 6.0 * (s12**2 + s13**2 + s23**2)))[:,None]
 
     return vms
+
+def prepare_cylindrical_transform(coors, origin):
+    """
+    Prepare matrices for transforming tensors into cylindrical coordinates with
+    the axis 'z' in a given origin.
+
+    Parameters
+    ----------
+    coors : array
+        The Cartesian coordinates.
+    origin : array of length 3
+        The origin.
+
+    Returns
+    -------
+    mtx : array
+        The array of transformation matrices for each coordinate in `coors`.
+    """
+    x, y = coors[:,0] - origin[0], coors[:,1] - origin[1]
+    theta = nm.arctan2(y, x)
+
+    mtx = nm.zeros((coors.shape[0], 3, 3), dtype=nm.float64)
+    for ii, th in enumerate(theta):
+        mtx[ii] = make_axis_rotation_matrix([0.0, 0.0, 1.0], th)
+
+    return mtx
+
+def transform_data(data, coors=None, mode='cylindrical', mtx=None):
+    """
+    Transform vector or tensor data components into another coordinate system.
+
+    For vectors:
+
+    .. math::
+        \ul{v}' = M \cdot \ul{v}
+
+    For tensors (assuming orthogonal coordinates):
+
+    .. math::
+        \ull{t}' = M^{-T} \cdot \ull{t} \cdot M
+
+    Parameters
+    ----------
+    data : array
+        The vectors or tensors (symmetric storage) to be transformed.
+    coors : array
+        The Cartesian coordinates of the data. Not needed when `mtx` argument
+        is given.
+    mode : one of ['cylindrical']
+        The requested coordinate system. Not needed when `mtx` argument
+        is given.
+    mtx : array
+        The array of transformation matrices :math:`M` for each data row.
+
+    Returns
+    -------
+    new_data : array
+        The transformed data.
+    """
+    if (coors is None) and (mtx is None):
+        raise ValueError('one of (coors, mtx) arguments must be set!')
+    
+    if mtx is None:
+        if mode == 'cylindrical':
+            mtx = prepare_cylindrical_transform(coors, [0.0, 0.0, 0.0])
+
+        else:
+            raise ValueError('transformation mode %s is not supported!' % mode)
+
+    if data.shape[1] == 3: # Vectors.
+        new_data = dot_sequences(mtx, data)
+
+    elif data.shape[1] == 6: # Symmetric tensors.
+        ii = [[0, 3, 4], [3, 1, 5], [4, 5, 2]]
+
+        aux = data[:,ii]
+        aux2 = dot_sequences(dot_sequences(mtx, aux), mtx.transpose((0, 2, 1)))
+        assert nm.allclose(aux2[0], nm.dot(nm.dot(mtx[0], aux[0]), mtx[0].T))
+
+        aux3 = aux2.reshape((aux2.shape[0], 9))
+
+        new_data = aux3[:, [0, 4, 8, 1, 2, 5]]
+
+    return new_data
