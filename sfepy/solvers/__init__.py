81d11e5c0184c20f33c35a34e4c4635ec337782d
Robert Cimrman <cimrman3@ntc.zcu.cz>
2010-04-01 17:01:29 +0200

    use load_classes(), find solvers automatically

--- sfepy/solvers/__init__.py
+++ sfepy/solvers/__init__.py
@@ -1,20 +1,20 @@
+import os
+import sfepy
 from solvers import *
+from eigen import eig
 
-from ls import *
-from nls import *
-from oseen import *
-from ts import *
-from eigen import *
-from optimize import *
-from semismooth_newton import *
-
-solver_table = find_subclasses(vars(),
-                               [LinearSolver, NonlinearSolver,
-                                TimeSteppingSolver, EigenvalueSolver,
-                                OptimizationSolver])
+solver_files = sfepy.get_paths('sfepy/solvers/*.py')
+remove = ['setup.py', 'solvers.py', 'generic.py']
+solver_files = [name for name in solver_files
+                if os.path.basename(name) not in remove]
+solver_table = load_classes(solver_files,
+                            [LinearSolver, NonlinearSolver,
+                             TimeSteppingSolver, EigenvalueSolver,
+                             OptimizationSolver], package_name='sfepy.solvers')
 
 def any_from_conf(conf, **kwargs):
     """Create an instance of a solver class according to the configuration."""
     return solver_table[conf.kind](conf, **kwargs)
 insert_static_method(Solver, any_from_conf)
 del any_from_conf
+del sfepy
