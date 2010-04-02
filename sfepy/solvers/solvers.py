be84867681277cd9fddc81f3be37e1159f47c340
Robert Cimrman <cimrman3@ntc.zcu.cz>
2010-04-02 16:35:50 +0200

    polish and document (a bit) base solver classes

--- sfepy/solvers/solvers.py
+++ sfepy/solvers/solvers.py
@@ -1,30 +1,37 @@
+"""
+Base (abstract) solver classes.
+"""
 from sfepy.base.base import *
 
-##
-# 10.10.2007, c
-class Solver( Struct ):
-    def process_conf( conf ):
+class Solver(Struct):
+    """
+    Base class for all solver kinds. Takes care of processing of common
+    configuration options.
+
+    The factory method any_from_conf() can be used to create an instance of any
+    subclass.
+
+    The subclasses have to reimplement __init__() and __call__(). The
+    subclasses that implement process_conf() have to call Solver.process_conf().
+    """
+
+    @staticmethod
+    def process_conf(conf):
         """
         Ensures conf contains 'name' and 'kind'.
         """
         get = conf.get_default_attr
-        name = get( 'name', None, 'missing "name" in options!' )
-        name = get( 'kind', None, 'missing "kind" in options!' )
+        name = get('name', None, 'missing "name" in options!')
+        name = get('kind', None, 'missing "kind" in options!')
 
-        return Struct( **locals() )
-    process_conf = staticmethod( process_conf )
+        return Struct(**locals())
 
-    ##
-    # 10.10.2007, c
-    def __init__( self, conf, **kwargs ):
-        conf = self.__class__.process_conf( conf )
-        Struct.__init__( self, conf = conf, **kwargs )
+    def __init__(self, conf, **kwargs):
+        conf = self.__class__.process_conf(conf)
+        Struct.__init__(self, conf=conf, **kwargs)
 
-    ##
-    # 10.10.2007, c
-    def __call__( self, **kwargs ):
-        print 'called an abstract Solver instance!'
-        raise ValueError
+    def __call__(self, **kwargs):
+        raise ValueError('called an abstract Solver instance!')
 
 class LinearSolver(Solver):
     """
@@ -37,81 +44,66 @@ class LinearSolver(Solver):
     def __call__(self, rhs, x0=None, conf=None, mtx=None, status=None):
         raise ValueError('called an abstract LinearSolver instance!')
 
-##
-# 10.10.2007, c
-class NonlinearSolver( Solver ):
-    ##
-    # 10.10.2007, c
-    def __init__( self, conf, fun = None, fun_grad = None, lin_solver = None,
-                  status = None, **kwargs ):
-        Solver.__init__( self, conf = conf, fun = fun, fun_grad = fun_grad,
-                         lin_solver = lin_solver, status = status,
-                         **kwargs )
+class NonlinearSolver(Solver):
+    """
+    Abstract nonlinear solver class.
+    """
+
+    def __init__(self, conf, fun=None, fun_grad=None, lin_solver=None,
+                  status=None, **kwargs):
+        Solver.__init__(self, conf=conf, fun=fun, fun_grad=fun_grad,
+                        lin_solver=lin_solver, status=status, **kwargs)
     
-    ##
-    # 10.10.2007, c
-    def __call__( self, state0, conf = None, fun = None, fun_grad = None,
-                  lin_solver = None, status = None ):
-        print 'called an abstract NonlinearSolver instance!'
-        raise ValueError
+    def __call__(self, state0, conf=None, fun=None, fun_grad=None,
+                 lin_solver=None, status=None):
+        raise ValueError('called an abstract NonlinearSolver instance!')
 
-##
-# c: 06.02.2008, r: 06.02.2008
-class TimeSteppingSolver( Solver ):
-    ##
-    # c: 06.02.2008, r: 06.02.2008
-    def __init__( self, conf, step_fun = None, step_args = None, **kwargs ):
-        Solver.__init__( self, conf = conf,
-                         step_fun = step_fun, step_args = step_args, **kwargs )
+class TimeSteppingSolver(Solver):
+    """
+    Abstract time stepping solver class.
+    """
 
-    ##
-    # c: 06.02.2008, r: 06.02.2008
-    def __call__( self, state0 = None, conf = None,
-                  step_fun = None, step_args = None ):
-        print 'called an abstract TimeSteppingSolver instance!'
-        raise ValueError
+    def __init__(self, conf, step_fun=None, step_args=None, **kwargs):
+        Solver.__init__(self, conf=conf,
+                        step_fun=step_fun, step_args=step_args, **kwargs)
 
-##
-# 17.10.2007, c
-class OptimizationSolver( Solver ):
-    ##
-    # 17.10.2007, c
-    def __init__( self, conf, obj_fun = None, obj_fun_grad = None, status = None,
-                  obj_args = None,  **kwargs ):
-        Solver.__init__( self, conf = conf, obj_fun = obj_fun,
-                         obj_fun_grad = obj_fun_grad, status = status,
-                         obj_args = obj_args, **kwargs )
-    ##
-    # 17.10.2007, c
-    def __call__( self, state0, conf = None, obj_fun = None, obj_fun_grad = None,
-                  status = None, obj_args = None ):
-        print 'called an abstract OptimizationSolver instance!'
-        raise ValueError
+    def __call__(self, state0=None, conf=None, step_fun=None, step_args=None ):
+        raise ValueError('called an abstract TimeSteppingSolver instance!')
 
-##
-# c: 03.03.2008, r: 03.03.2008
-class EigenvalueSolver( Solver ):
-    ##
-    # c: 03.03.2008, r: 03.03.2008
-    def __init__( self, conf, mtx_a = None, mtx_b = None, n_eigs = None,
-                  eigenvectors = None, status = None ):
-        Solver.__init__( self, conf = conf, mtx_a = mtx_a, mtx_b = mtx_b,
-                         n_eigs = n_eigs, eigenvectors = eigenvectors,
-                         status = status )
+class OptimizationSolver(Solver):
+    """
+    Abstract optimization solver class.
+    """
+
+    def __init__(self, conf, obj_fun=None, obj_fun_grad=None, status=None,
+                 obj_args=None,  **kwargs ):
+        Solver.__init__(self, conf=conf, obj_fun=obj_fun,
+                        obj_fun_grad=obj_fun_grad, status=status,
+                        obj_args=obj_args, **kwargs)
+
+    def __call__(self, state0, conf=None, obj_fun=None, obj_fun_grad=None,
+                 status=None, obj_args=None):
+        raise ValueError('called an abstract OptimizationSolver instance!')
+
+class EigenvalueSolver(Solver):
+    """
+    Abstract eigenvalue solver class.
+    """
+
+    def __init__(self, conf, mtx_a=None, mtx_b=None, n_eigs=None,
+                 eigenvectors=None, status=None):
+        Solver.__init__(self, conf=conf, mtx_a=mtx_a, mtx_b=mtx_b,
+                        n_eigs=n_eigs, eigenvectors=eigenvectors,
+                        status=status)
                          
-    ##
-    # c: 03.03.2008, r: 03.03.2008
-    def __call__( self, mtx_a, mtx_b = None, n_eigs = None,
-                  eigenvectors = None, status = None, conf = None ):
-        print 'called an abstract EigenvalueSolver instance!'
-        raise ValueError
+    def __call__(self, mtx_a, mtx_b=None, n_eigs=None,
+                 eigenvectors=None, status=None, conf=None):
+        raise ValueError('called an abstract EigenvalueSolver instance!')
 
-    ##
-    # c: 08.04.2008, r: 08.04.2008
-    def _to_array( self, mtx_a, mtx_b = None ):
-        if hasattr( mtx_a, 'toarray' ):
+    def _to_array(self, mtx_a, mtx_b=None):
+        if hasattr(mtx_a, 'toarray'):
             mtx_a = mtx_a.toarray()
         if mtx_b is not None:
-            if hasattr( mtx_b, 'toarray' ):
+            if hasattr(mtx_b, 'toarray'):
                 mtx_b = mtx_b.toarray()
         return mtx_a, mtx_b
