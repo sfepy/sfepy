147d317817d1eed6a1b4160ecc25327f557326ca
Robert Cimrman <cimrman3@ntc.zcu.cz>
2010-05-04 12:07:37 +0200

    new Integral.integrate(), .get_actual_order(), update docstrings

--- sfepy/fem/integrals.py
+++ sfepy/fem/integrals.py
@@ -73,29 +73,68 @@ class Integral(Struct):
     def set_term(self, term):
         self.term = term
 
+    def get_actual_order(self, geometry):
+        """
+        Return the actual integration order for given geometry.
+
+        Parameters
+        ----------
+        geometry : str
+            The geometry key describing the integration domain,
+            see the keys of `sfepy.fem.quadratures.quadrature_tables`.
+
+        Returns
+        -------
+        order : int
+            If `self.order` is in quadrature tables it is this
+            value. Otherwise it is the closest higher order. If no
+            higher order is available, a warning is printed and the
+            highest available order is used.
+        """
+        table = quadrature_tables[geometry]
+        if self.order in table:
+            order = self.order
+
+        else:
+            orders = table.keys()
+            ii = nm.searchsorted(orders, self.order)
+            if ii >= len(orders):
+                order = max(orders)
+                output(self._msg1 % (self.order, geometry))
+                output(self._msg2 % order)
+
+            else:
+                order = orders[ii]
+
+        return order
+
     def get_qp(self, geometry):
+        """
+        Get quadrature point coordinates and corresponding weights for given
+        geometry.  For built-in quadratures, the integration order is
+        given by `self.get_actual_order()`.
+
+        Parameters
+        ----------
+        geometry : str
+            The geometry key describing the integration domain,
+            see the keys of `sfepy.fem.quadratures.quadrature_tables`.
+
+        Returns
+        -------
+        coors : array
+           The coordinates of quadrature points.
+        weights: array
+           The quadrature weights.
+        """
 
         if geometry in self.qps:
             qp = self.qps[geometry]
 
         else:
             if self.mode == 'builtin':
-                table = quadrature_tables[geometry]
-                if self.order in table:
-                    qp = table[self.order]
-
-                else:
-                    orders = table.keys()
-                    ii = nm.searchsorted(orders, self.order)
-                    if ii >= len(orders):
-                        order = max(orders)
-                        output(self._msg1 % (self.order, geometry))
-                        output(self._msg2 % order)
-
-                    else:
-                        order = orders[ii]
-
-                    qp = table[order]
+                order = self.get_actual_order(geometry)
+                qp = quadrature_tables[geometry][order]
 
             else:
                 qp = QuadraturePoints(None,
@@ -104,3 +143,39 @@ class Integral(Struct):
             self.qps[geometry] = qp
 
         return qp.coors, qp.weights
+
+    def integrate(self, function, order=None, geometry=None):
+        """
+        Integrate numerically a given scalar function.
+
+        Parameters
+        ----------
+        function : callable(coors)
+            The function of space coordinates to integrate.
+        order : int, optional
+            The integration order. Default is given by
+            `self.get_actual_order()`.
+        geometry : str
+            The geometry key describing the integration domain. Default
+            is `'1_2'`, i.e. a line integral in [0, 1]. For other values
+            see the keys of `sfepy.fem.quadratures.quadrature_tables`.
+
+        Returns
+        -------
+        val : float
+            The value of the integral.
+        """
+        if geometry is None:
+            geometry = '1_2'
+
+        table = quadrature_tables[geometry]
+
+        if order is None:
+            order = sorted(table.keys())[0]
+
+        qp = table[order]
+        
+        fvals = function(qp.coors)
+        val = nm.sum(fvals * qp.weights)
+
+        return val
