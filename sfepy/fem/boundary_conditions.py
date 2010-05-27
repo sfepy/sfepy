7c87a85e1d10d04f4788a2196a44555e5cbe22a9
Robert Cimrman <cimrman3@ntc.zcu.cz>
2010-05-27 12:06:56 +0200

    update boundary condition classes, add initial condition class
    - rename BoundaryConditions -> Conditions
    - rename BoundaryCondition -> Condition
    - new InitialCondition

--- sfepy/fem/boundary_conditions.py
+++ sfepy/fem/boundary_conditions.py
@@ -1,36 +1,52 @@
 """
-Dirichlet, periodic and linear combination boundary condition classes.
+The Dirichlet, periodic and linear combination boundary condition
+classes, as well as the initial condition class.
 """
 from sfepy.base.base import *
 
-class BoundaryConditions(Container):
+class Conditions(Container):
     """
-    Container for boundary conditions.
+    Container for various conditions.
     """
     @staticmethod
     def from_conf(conf):
-        bcs = []
-        for key, bc in conf.iteritems():
+        conds = []
+        for key, cc in conf.iteritems():
             if 'ebc' in key:
-                bc = EssentialBC(bc.name, bc.region, bc.dofs, key=key)
+                cond = EssentialBC(cc.name, cc.region, cc.dofs, key=key)
 
             elif 'epbc' in key:
-                bc = PeriodicBC(bc.name, bc.region, bc.dofs, bc.match, key=key)
+                cond = PeriodicBC(cc.name, cc.region, cc.dofs, cc.match, key=key)
     
             elif 'lcbc' in key:
-                bc = LinearCombinationBC(bc.name, bc.region, bc.dofs, key=key)
+                cond = LinearCombinationBC(cc.name, cc.region, cc.dofs, key=key)
+
+            elif 'ic' in key:
+                cond = InitialCondition(cc.name, cc.region, cc.dofs, key=key)
 
             else:
-                raise ValueError('unknown BC type! (%s)' % key)
+                raise ValueError('unknown condition type! (%s)' % key)
 
-            bcs.append(bc)
+            conds.append(cond)
 
-        obj = BoundaryConditions(bcs)
+        obj = Conditions(conds)
         return obj
 
     def group_by_variables(self, groups=None):
         """
-        Group boundary conditions of each variable.
+        Group boundary conditions of each variable. Each condition is a
+        group is a single condition.
+
+        Parameters
+        ----------
+        groups : dict, optional
+            If present, update the `groups` dictionary.
+
+        Returns
+        -------
+        out : dict
+            The dictionary with variable names as keys and lists of
+            single condition instances as values.
         """
         if groups is None:
             out = {}
@@ -38,10 +54,10 @@ class BoundaryConditions(Container):
         else:
             out = groups
 
-        for bc in self:
-            for single_bc in bc.iter_single():
-                vname = single_bc.dofs[0].split( '.' )[0]
-                out.setdefault(vname, BoundaryConditions()).append(single_bc)
+        for cond in self:
+            for single_cond in cond.iter_single():
+                vname = single_cond.dofs[0].split('.')[0]
+                out.setdefault(vname, Conditions()).append(single_cond)
 
         return out
 
@@ -50,8 +66,8 @@ class BoundaryConditions(Container):
         Canonize the DOF names using the full list of DOFs of a
         variable.
         """
-        for bc in self:
-            bc.canonize_dof_names(dofs)
+        for cond in self:
+            cond.canonize_dof_names(dofs)
 
     def sort(self):
         """
@@ -60,38 +76,53 @@ class BoundaryConditions(Container):
         self._objs.sort(cmp=lambda i1, i2: cmp(i1.key, i2.key))
         self.update()
 
-class BoundaryCondition(Struct):
+def _canonize(dofs, all_dofs):
+    """
+    Helper function.
+    """
+    vname, dd = dofs.split('.')
+
+    if dd == 'all':
+        cdofs = all_dofs
+
+    elif dd[0] == '[':
+        cdofs = [vname + '.' + ii.strip()
+                 for ii in dd[1:-1].split(',')]
+
+    else:
+        cdofs = [dofs]
+
+    return cdofs
+
+class Condition(Struct):
     """
     Common boundary condition methods.
     """
+    def __init__(self, name, **kwargs):
+        Struct.__init__(self, name=name, **kwargs)
+        self.is_single = False
+
     def iter_single(self):
+        """
+        Create a single condition instance for each item in self.dofs
+        and yield it.
+        """
         for dofs, val in self.dofs.iteritems():
-            single_bc = self.copy()
-            single_bc.dofs = [dofs, val]
-            yield single_bc
+            single_cond = self.copy(name=self.name)
+            single_cond.is_single = True
+            single_cond.dofs = [dofs, val]
+            yield single_cond
 
     def canonize_dof_names(self, dofs):
         """
         Canonize the DOF names using the full list of DOFs of a
         variable.
         
-        Assumes single BC instance.
+        Assumes single condition instance.
         """
-        vname, dd = self.dofs[0].split( '.' )
-        if dd == 'all':
-            cdofs = dofs
-        elif dd[0] == '[':
-            cdofs = [vname + '.' + ii.strip()
-                     for ii in dd[1:-1].split( ',' )]
-        else:
-            cdofs = [self.dofs[0]]
+        self.dofs[0] = _canonize(self.dofs[0], dofs)
 
-        self.dofs[0] = cdofs
-
-    def get_dofs(self):
-        return self.cdofs
-
-class EssentialBC(BoundaryCondition):
+class EssentialBC(Condition):
     """
     Essential boundary condidion.
 
@@ -108,9 +139,9 @@ class EssentialBC(BoundaryCondition):
         The sorting key.
     """
     def __init__(self, name, region, dofs, key=''):
-        Struct.__init__(self, name=name, region=region, dofs=dofs, key=key)
+        Condition.__init__(self, name=name, region=region, dofs=dofs, key=key)
 
-class PeriodicBC(BoundaryCondition):
+class PeriodicBC(Condition):
     """
     Periodic boundary condidion.
 
@@ -123,18 +154,27 @@ class PeriodicBC(BoundaryCondition):
     dofs : dict
         The boundary condition specification defining the DOFs in the master
         region and the corresponding DOFs in the slave region.
-    match : Function instance
-        The function that should determine corresponding nodes in the
+    match : str
+        The name of function for matching corresponding nodes in the
         two regions.
     key : str, optional
         The sorting key.
     """
     def __init__(self, name, regions, dofs, match, key=''):
-        Struct.__init__(self, name=name, regions=regions, dofs=dofs,
-                        match=match, key=key)
+        Condition.__init__(self, name=name, regions=regions, dofs=dofs,
+                           match=match, key=key)
 
+    def canonize_dof_names(self, dofs):
+        """
+        Canonize the DOF names using the full list of DOFs of a
+        variable.
+        
+        Assumes single condition instance.
+        """
+        self.dofs[0] = _canonize(self.dofs[0], dofs)
+        self.dofs[1] = _canonize(self.dofs[1], dofs)
 
-class LinearCombinationBC(BoundaryCondition):
+class LinearCombinationBC(Condition):
     """
     Linear combination boundary condidion.
 
@@ -146,9 +186,28 @@ class LinearCombinationBC(BoundaryCondition):
         The region where the boundary condition is applied.
     dofs : dict
         The boundary condition specification defining the constrained
-        DOFs and the contraint type.
+        DOFs and the constraint type.
     key : str, optional
         The sorting key.
     """
     def __init__(self, name, region, dofs, key=''):
-        Struct.__init__(self, name=name, region=region, dofs=dofs, key=key)
+        Condition.__init__(self, name=name, region=region, dofs=dofs, key=key)
+
+class InitialCondition(Condition):
+    """
+    Initial condidion.
+
+    Parameters
+    ----------
+    name : str
+        The initial condition name.
+    region : Region instance
+        The region where the initial condition is applied.
+    dofs : dict
+        The initial condition specification defining the constrained
+        DOFs and their values.
+    key : str, optional
+        The sorting key.
+    """
+    def __init__(self, name, region, dofs, key=''):
+        Condition.__init__(self, name=name, region=region, dofs=dofs, key=key)
