31d39128b36f9537ba460a378409e84ab45ddc8f
Robert Cimrman <cimrman3@ntc.zcu.cz>
2010-09-07 14:12:58 +0200

    new insert_sparse_to_csr() - insert a sparse matrix into a CSR matrix

--- sfepy/linalg/sparse.py
+++ sfepy/linalg/sparse.py
@@ -1,4 +1,5 @@
 """Some sparse matrix utilities missing in scipy."""
+from sfepy.base.base import *
 
 def save_sparse_txt(filename, mtx, fmt='%d %d %f\n'):
     """Save a CSR/CSC sparse matrix into a text file"""
@@ -21,3 +22,31 @@ def save_sparse_txt(filename, mtx, fmt='%d %d %f\n'):
 
     else:
         raise ValueError('matrix format not supported! (%s)' % mtx.format)
+
+def insert_sparse_to_csr(mtx1, mtx2, irs, ics):
+    """
+    Insert a sparse matrix `mtx2` into a CSR sparse matrix `mtx1` at
+    rows `irs` and columns `ics`. The submatrix `mtx1[irs,ics]` must
+    already be preallocated and have the same structure as `mtx2`.
+    """
+    import sfepy.fem.extmods.fem as fem
+
+    if isinstance(irs, slice):
+        irs = nm.arange(irs.start, irs.stop, irs.step, dtype=nm.int32)
+
+    if isinstance(ics, slice):
+        ics = nm.arange(ics.start, ics.stop, ics.step, dtype=nm.int32)
+
+    n_row, n_col = mtx1.shape
+
+    assert_((irs.min() >= 0) and (irs.max() < n_row))
+    assert_((ics.min() >= 0) and (ics.max() < n_col))
+
+    aux = mtx2.tocoo()
+    data = nm.ascontiguousarray(aux.data[:,None,None,None])
+    rows = irs[aux.row[:,None]]
+    cols = ics[aux.col[:,None]]
+
+    iels = nm.arange(rows.shape[0], dtype=nm.int32)
+    fem.assemble_matrix(mtx1.data, mtx1.indptr, mtx1.indices, data,
+                        iels, 1.0, rows, cols)
