9ebe37230870cadca525afc3661e2014e7d2bdf5
Robert Cimrman <cimrman3@ntc.zcu.cz>
2010-10-06 17:48:00 +0200

    new sfepy/base/compat.py module

---
+++ sfepy/base/compat.py
@@ -0,0 +1,20 @@
+"""
+This module contains functions that have different names or behavior
+depending on NumPy version.
+"""
+import numpy as nm
+
+__all__ = ['in1d', 'unique']
+
+try:
+    in1d = nm.in1d
+
+except AttributeError:
+    in1d = nm.setmember1d
+
+unique = nm.unique
+try:
+    nm.unique([0], return_index=True, return_inverse=True)
+
+except TypeError:
+    unique = nm.unique1d
