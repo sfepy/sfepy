c55f7d2f39eeaa526633a646c6dae70d9bd202a2
Robert Cimrman <cimrman3@ntc.zcu.cz>
2010-11-03 11:17:37 +0100

    update Approximation.describe_geometry()

--- sfepy/fem/fea.py
+++ sfepy/fem/fea.py
@@ -324,12 +324,12 @@ class Approximation( Struct ):
 
             if not self.is_surface:
                 ps = self.interp.poly_spaces[sd.face_type]
-                mapping = SurfaceMapping(coors, sd.econn, poly_space=ps)
 
             else:
                 ps = self.interp.poly_spaces['v']
-                mapping = SurfaceMapping(coors, sd.leconn, poly_space=ps)
 
+            econn = sd.get_connectivity(self.is_surface)
+            mapping = SurfaceMapping(coors, econn, poly_space=ps)
             sg = mapping.get_mapping(qp.vals, qp.weights)
 
             if gtype == 'surface_extra':
