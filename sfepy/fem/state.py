085df009f985966d8c8b259c9ad379681f1c4dc8
Robert Cimrman <cimrman3@ntc.zcu.cz>
2010-11-18 17:11:55 +0100

    state: new State class for handling state Variables
    - new sfepy/fem/state.py

---
+++ sfepy/fem/state.py
@@ -0,0 +1,134 @@
+"""
+Module for handling state variables.
+"""
+from sfepy.base.base import Struct
+
+class State(Struct):
+    """
+    Class holding/manipulating the state variables and corresponding DOF
+    vectors.
+
+    Manipulating the state class changes the underlying variables, and
+    hence also the corresponding equations/terms (if any).
+
+    Notes
+    -----
+    This class allows working with LCBC conditions in time-dependent
+    problems, as it keeps track of the reduced DOF vector that cannot
+    be reconstructed from the full DOF vector by using the usual
+    `variables.strip_state_vector()`.
+    """
+
+    def __init__(self, variables, vec=None):
+        """
+        Create a State instance for the given variables.
+
+        Parameters
+        ----------
+        variables : Variables instance
+            The variables.
+        vec : array, optional
+            The (initial) DOF vector corresponding to the variables.
+        """
+        Struct.__init__(self, variables=variables, vec=vec, r_vec=None)
+
+        if self.vec is None:
+            self.vec = variables.create_state_vector()
+
+    def copy(self, deep=False):
+        """
+        Copy the state. By default, the new state contains the same
+        variables, and creates new DOF vectors. If `deep` is True, also
+        the DOF vectors are copied.
+        """
+        if deep:
+            other = State(self.variables, self.vec.copy())
+            other.r_vec = self.r_vec.copy()
+
+        else:
+            other = State(self.variables)
+
+        return other
+
+    def apply_ebc(self, force_values=None):
+        """
+        Apply essential (Dirichlet) boundary conditions to the state.
+        """
+        self.variables.apply_ebc(self.vec, force_values=force_values)
+
+    def apply_ic(self, force_values=None):
+        """
+        Apply initial conditions to the state.
+        """
+        if self.r_vec is not None:
+            raise ValueError('cannot re-apply initial conditions with LCBCs!')
+
+        self.variables.apply_ic(self.vec, force_values=force_values)
+
+    def get_reduced(self):
+        """
+        Get the reduced DOF vector, with EBC and PBC DOFs removed.
+        """
+        if self.variables.has_lcbc:
+            if self.r_vec is None:
+                r_vec = self.variables.strip_state_vector(self.vec)
+                r_vec = self.variables.op_lcbc.T *  r_vec
+
+            else:
+                r_vec = self.r_vec
+
+        else:
+            r_vec = self.variables.strip_state_vector(self.vec)
+
+        return r_vec
+
+    def set_reduced(self, r_vec):
+        """
+        Set the reduced DOF vector, with EBC and PBC DOFs removed.
+        """
+        self.vec = self.variables.make_full_vec(r_vec)
+
+        if self.variables.has_lcbc:
+            self.r_vec = r_vec
+
+    def set_full(self, vec):
+        """
+        Set the full DOF vector (including EBC and PBC DOFs).
+        """
+        self.vec = vec
+
+        if self.variables.has_lcbc:
+            raise ValueError('cannot set full DOF vector with LCBCs!')
+
+    def __call__(self, var_name=None):
+        """
+        Get the full DOF vector (including EBC and PBC DOFs). If
+        `var_name` is given, return only the DOF vector corresponding to
+        the given variable.
+        """
+        if var_name is None:
+            out = self.vec
+
+        else:
+            out = self.variables.get_state_part_view(self.vec, var_name)
+
+        return out
+
+    def create_output_dict(self, fill_value=None, var_info=None,
+                           extend=True):
+        """
+        Transforms state to an output dictionary, that can be
+        passed as 'out' kwarg to Mesh.write().
+
+        Then the dictionary entries are formed by components of the
+        state vector corresponding to the unknown variables, each
+        transformed to shape (n_mesh_nod, n_dof per node) - all values
+        in extra (higher order) nodes are removed.
+
+        Examples
+        --------
+        >>> out = state.create_output_dict()
+        >>> problem.save_state('file.vtk', out=out)
+        """
+        return self.variables.state_to_output(self.vec, fill_value,
+                                              var_info, extend)
