deb38fc48d007b330eb4bb2b82e333a36c7d464e
Robert Cimrman <cimrman3@ntc.zcu.cz>
2010-12-07 18:16:04 +0100

    update tests/test_projections.py: new test_projection_tri_quad()

--- tests/test_projections.py
+++ tests/test_projections.py
@@ -1,4 +1,8 @@
+import os.path as op
 import numpy as nm
+
+import sfepy
+from sfepy.fem import Mesh, Domain, Field, FieldVariable
 
 from sfepy.base.testing import TestCommon, assert_
 
@@ -6,9 +10,7 @@ class Test(TestCommon):
 
     @staticmethod
     def from_conf(conf, options):
-        import sfepy
-        from sfepy.fem import Mesh, Domain, Field
-        mesh = Mesh.from_file('meshes/2d/rectangle_tri.mesh',
+        mesh = Mesh.from_file('meshes/2d/square_unit_tri.mesh',
                               prefix_dir=sfepy.data_dir)
         domain = Domain('domain', mesh)
 
@@ -28,6 +30,51 @@ class Test(TestCommon):
         mtx = create_mass_matrix(field)
 
         assert_(mtx.shape == (field.n_nod, field.n_nod))
-        assert_(abs(mtx.sum() - 200.0) < 1e-12)
+        assert_(abs(mtx.sum() - 1.0) < 1e-14)
 
         return True
+
+    def test_projection_tri_quad(self):
+        from sfepy.fem.projections import make_l2_projection
+
+        source = FieldVariable('us', 'unknown', self.field, 1)
+
+        coors = self.field.get_coor()
+        vals = nm.sin(2.0 * nm.pi * coors[:,0] * coors[:,1])
+        source.data_from_any(vals)
+
+        name = op.join(self.options.out_dir,
+                       'test_projection_tri_quad_source.vtk')
+        source.save_as_mesh(name)
+
+        mesh = Mesh.from_file('meshes/2d/square_quad.mesh',
+                              prefix_dir=sfepy.data_dir)
+        domain = Domain('domain', mesh)
+
+        omega = domain.create_region('Omega', 'all')
+
+
+        field = Field('bilinear', nm.float64, 'scalar', omega,
+                      space='H1', poly_space_base='lagrange', approx_order=1)
+
+        target = FieldVariable('ut', 'unknown', field, 1)
+
+        make_l2_projection(target, source)
+
+        name = op.join(self.options.out_dir,
+                       'test_projection_tri_quad_target.vtk')
+        target.save_as_mesh(name)
+
+        bbox = self.field.domain.get_mesh_bounding_box()
+        x = nm.linspace(bbox[0, 0] + 0.001, bbox[1, 0] - 0.001, 20)
+        y = nm.linspace(bbox[0, 1] + 0.001, bbox[1, 1] - 0.001, 20)
+
+        xx, yy = nm.meshgrid(x, y)
+        test_coors = nm.c_[xx.ravel(), yy.ravel()].copy()
+
+        vec1 = source.evaluate_at(test_coors)
+        vec2 = target.evaluate_at(test_coors)
+
+        ok = (nm.abs(vec1 - vec2) < 0.14).all()
+
+        return ok
