24be43927a32a00215c7e9720a27752e781e396e
Vladimir Lukes <lukes@kme.zcu.cz>
2011-03-03 08:30:48 +0100

    new addpoints, addlines, addsurfaces, addvolumes, leaveonlyphysicalsurfaces in geometry.py, 2D and 3D geometries

--- sfepy/geom/geometry.py
+++ sfepy/geom/geometry.py
@@ -1,3 +1,5 @@
+import numpy as nm
+
 class geometry(object):
     """The geometry is given by a sets of points (d0), lines (d1), surfaces
     (d2) and volumes (d3). A lines are constructed from 2 points, a surface from
@@ -19,7 +21,8 @@ class geometry(object):
 
     This would give you a list [..] of point() instances.
     """
-    def __init__(self):
+    def __init__(self, dim=3):
+        self.dim = dim
         self.d0={}
         self.d1={}
         self.d2={}
@@ -30,20 +33,36 @@ class geometry(object):
         "p=[x,y,z]"
         o=point(self,n,p)
         self.d0[o.getn()]=o
+    def addpoints(self,ps,off=1):
+        "ps=[p1, p2, ...]"
+        for i, p in enumerate(ps):
+            self.addpoint(i + off, p)
     def addline(self,n,l):
         "l=[p1,p2]"
         o=line(self,n,l)
         self.d1[o.getn()]=o
-    def addsurface(self,n,s):
+    def addlines(self,ls,off=1):
+        "ls=[l1, l2, ...]"
+        for i, l in enumerate(ls):
+            self.addline(i + off, l)
+    def addsurface(self,n,s, is_hole=False):
         "s=[l1,l2,l3,...]"
-        o=surface(self,n,s)
+        o=surface(self,n,s, is_hole)
         self.d2[o.getn()]=o
+    def addsurfaces(self,ss,off=1):
+        "s=[s1,s2,s3,...]"
+        for i, s in enumerate(ss):
+            self.addsurface(i + off, s)
     def addvolume(self,n,v):
         "v=[s1,s2,s3,...]"
         o=volume(self,n,v)
         self.d3[o.getn()]=o
+    def addvolumes(self,vs,off=1):
+        "v=[v1,v2,v3,...]"
+        for i, v in enumerate(vs):
+            self.addvolume(i + off, v)
     def addphysicalsurface(self,n,surfacelist):
-        "surfacelist=[v1,v2,v3,...]"
+        "surfacelist=[s1,s2,s3,...]"
         o=physicalsurface(self,n,surfacelist)
         self.phys2[o.getn()]=o
     def addphysicalvolume(self,n,volumelist):
@@ -55,19 +74,59 @@ class geometry(object):
             if snum in self.phys2[x].surfaces:
                 return x
         return 0
-    def printinfo(self):
+    def printinfo(self, verbose=False):
         print "General geometry information:"
-        print "  points:",len(self.d0)
-        print "  lines:",len(self.d1)
-        print "  surfaces:",len(self.d2)
-        print "  volumes:",len(self.d3)
+        print "  dimension:", self.dim
+        print "  points:", len(self.d0)
+        if verbose:
+            for k, v in self.d0.iteritems():
+                print "    %d - %s" % (k, v.getstr())
+        print "  lines:", len(self.d1)
+        if verbose:
+            for k, v in self.d1.iteritems():
+                print "    %d - " % k, v.points
+        print "  surfaces:", len(self.d2)
+        if verbose:
+            for k, v in self.d2.iteritems():
+                if v.is_hole:
+                    aux = '(hole)'
+                else:
+                    aux = ''
+                print "    %d%s - " % (k, aux), v.lines
+        print "  volumes:", len(self.d3)
+        if verbose:
+            for k, v in self.d3.iteritems():
+                print "    %d - " % k, v.surfaces
         print "Physical entities:"
-        print "  surfaces (boundary conditions):"
-        for d in self.phys2.values():
-            print "    %d: surface numbers %r"%(d.getn(),d.surfaces)
-        print "  volumes (regions):"
-        for d in self.phys3.values():
-            print "    %d: volume numbers %r"%(d.getn(),d.volumes)
+        if self.dim == 2:
+            print "  surfaces (regions):"
+            for d in self.phys2.values():
+                print "    %d: surface numbers %r"%(d.getn(),d.surfaces)
+        elif self.dim == 3:
+            print "  surfaces (boundary conditions):"
+            for d in self.phys2.values():
+                print "    %d: surface numbers %r"%(d.getn(),d.surfaces)
+            print "  volumes (regions):"
+            for d in self.phys3.values():
+                print "    %d: volume numbers %r"%(d.getn(),d.volumes)
+
+    def leaveonlyphysicalsurfaces(self):
+        points={}
+        lines={}
+        surfaces={}
+        volumes={}
+        for e in self.phys2:
+            for s in self.phys2[e].getsurfaces():
+                surfaces[s.getn()]=s
+                for l in s.getlines():
+                    lines[l.getn()]=l
+                    for p in l.getpoints():
+                        points[p.getn()]=p
+        self.d0=points
+        self.d1=lines
+        self.d2=surfaces
+        self.d3=volumes
+
     def leaveonlyphysicalvolumes(self):
         points={}
         lines={}
@@ -86,6 +145,38 @@ class geometry(object):
         self.d1=lines
         self.d2=surfaces
         self.d3=volumes
+
+    def splitlines(self, ls, n):
+        repl = {}
+        for il in ls:
+            l = self.d1[il]
+            pts = l.getpoints()
+            dp = pts[1] - pts[0]
+            t = nm.linspace(0, 1, n + 1)
+            points = [pts[0].n, pts[1].n]
+            for ii, it in enumerate(t[1:-1]):
+                pid = il * 1000 + ii
+                self.addpoint(pid, (pts[0] + dp * it).getxyz())
+                points.insert(-1, pid)
+
+            lines = []
+            for ii in range(n):
+                lid = il * 1000 + ii
+                self.addline(lid, [points[ii], points[ii + 1]])
+                lines.append(lid)
+
+            for s in self.d2.itervalues():
+                try:
+                    idx = s.lines.index(l.n)
+                except ValueError:
+                    continue
+
+                s.lines.pop(idx)
+                for ii, j in enumerate(lines):
+                    s.lines.insert(idx + ii, j)
+
+            repl[l.n] = lines
+            self.d1.pop(l.n)
 
 class geomobject(object):
     def getn(self):
@@ -109,7 +200,12 @@ class point(geomobject):
     def getxyz(self):
         return self.p
     def getstr(self):
-        return "%f, %f, %f"%self.getxyz()
+        if self.geom.dim == 2:
+            return "%f, %f" % tuple(self.getxyz())
+        elif self.geom.dim == 3:
+            return "%f, %f, %f" % tuple(self.getxyz())
+        else:
+            return None
 
 class line(geomobject):
     def __init__(self,g,n,l):
@@ -120,10 +216,11 @@ class line(geomobject):
         return [self.geom.d0[x] for x in self.points]
 
 class surface(geomobject):
-    def __init__(self,g,n,s):
+    def __init__(self,g,n,s, is_hole=False):
         self.geom=g
         self.n=n
         self.lines,self.holes=self.separate(s)
+        self.is_hole = is_hole
     def separate(self,s):
         #FIXME - this is just a quick hack to satisfy all the examples
         if len(s)<=4:
@@ -151,12 +248,19 @@ class surface(geomobject):
             else:
                 return self.geom.d1[-idx].getpoints()[1]
         r=[]
-        for hole in self.holes:
-            r.append([p(x) for x in hole])
+        if self.is_hole:
+            r.append(self.getinsidepoint())
+        else:
+            for hole in self.holes:
+                r.append([p(x) for x in hole])
         return r
+    def getcenterpoint(self):
+        pts=self.getpoints()
+        return sum(pts[1:], pts[0] * 0) / float(len(pts))
     def getinsidepoint(self):
-        pts=self.getpoints()[:3]
-        return (pts[0]+pts[1]+pts[2])/3
+        p0 = self.getpoints()[0]
+        pc = self.getcenterpoint()
+        return p0 + (pc - p0) * 0.001
 
 class volume(geomobject):
     def __init__(self,g,n,v):
@@ -169,7 +273,7 @@ class volume(geomobject):
         sfs=self.getsurfaces()[:3]
         pts=[s.getinsidepoint() for s in sfs]
         p0=sfs[0].getpoints()[0]
-        direct=(pts[0]+pts[1]+pts[2])/3-p0
+        direct=(pts[0]+pts[1]+pts[2]) / 3.0 - p0
         return p0+0.001*direct
 
 class physicalsurface(geomobject):
