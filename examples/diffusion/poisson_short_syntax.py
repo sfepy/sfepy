b3465f7aacf15c02ebe22c910c356017cac18db3
Robert Cimrman <cimrman3@ntc.zcu.cz>
2011-05-26 13:50:38 +0200

    new examples/diffusion/poisson_short_syntax.py

---
+++ examples/diffusion/poisson_short_syntax.py
@@ -0,0 +1,51 @@
+"""
+The same example as poisson.py, but using the short syntax of keywords.
+"""
+from sfepy import data_dir
+
+filename_mesh = data_dir + '/meshes/3d/cylinder.mesh'
+
+materials = {
+    'coef' : ({'val' : 1.0},),
+}
+
+regions = {
+    'Omega' : ('all', {}), # or 'elements of group 6'
+    'Gamma_Left' : ('nodes in (x < 0.00001)', {}),
+    'Gamma_Right' : ('nodes in (x > 0.099999)', {}),
+}
+
+fields = {
+    'temperature' : ('real', 1, 'Omega', 1),
+}
+
+variables = {
+    't' : ('unknown field', 'temperature', 0),
+    's' : ('test field',    'temperature', 't'),
+}
+
+ebcs = {
+    't1' : ('Gamma_Left', {'t.0' : 2.0}),
+    't2' : ('Gamma_Right', {'t.0' : -2.0}),
+}
+
+integrals = {
+    'i1' : ('v', 'gauss_o2_d3'),
+}
+
+equations = {
+    'Temperature' : """dw_laplace.i1.Omega( coef.val, s, t ) = 0"""
+}
+
+solvers = {
+    'ls' : ('ls.scipy_direct', {}),
+    'newton' : ('nls.newton',
+                {'i_max'      : 1,
+                 'eps_a'      : 1e-10,
+    }),
+}
+
+options = {
+    'nls' : 'newton',
+    'ls' : 'ls',
+}
