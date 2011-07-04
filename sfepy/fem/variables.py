0b27e32559682d9eba7de62a5307eab7f2c9a9f8
Robert Cimrman <cimrman3@ntc.zcu.cz>
2011-07-04 10:16:48 +0200

    easier_terms: update FieldVariable.evaluate() for complex values

--- sfepy/fem/variables.py
+++ sfepy/fem/variables.py
@@ -1765,37 +1765,81 @@ class FieldVariable(Variable):
             aux = self.get_data_shape(ig, integral, shape_kind, region.name)
             n_el, n_qp, dim, n_en, n_comp = aux
 
-            if mode == 'val':
-                function = terms.dq_state_in_qp
+            if self.dtype == nm.float64:
+                if mode == 'val':
+                    function = terms.dq_state_in_qp
 
-                out = nm.empty((n_el, n_qp, n_comp, 1), dtype=self.dtype)
-                function(out, vec, 0, geo.bf, conn)
+                    out = nm.empty((n_el, n_qp, n_comp, 1), dtype=self.dtype)
+                    function(out, vec, 0, geo.bf, conn)
 
-            elif mode == 'grad':
-                function = terms.dq_grad
+                elif mode == 'grad':
+                    function = terms.dq_grad
 
-                out = nm.empty((n_el, n_qp, dim, n_comp), dtype=self.dtype)
-                function(out, vec, 0, geo, conn)
+                    out = nm.empty((n_el, n_qp, dim, n_comp), dtype=self.dtype)
+                    function(out, vec, 0, geo, conn)
 
-            elif mode == 'div':
-                assert_(n_comp == dim)
-                function = terms.dq_div_vector
+                elif mode == 'div':
+                    assert_(n_comp == dim)
+                    function = terms.dq_div_vector
 
-                out = nm.empty((n_el, n_qp, 1, 1), dtype=self.dtype)
-                function(out, vec, 0, geo, conn)
+                    out = nm.empty((n_el, n_qp, 1, 1), dtype=self.dtype)
+                    function(out, vec, 0, geo, conn)
 
-            elif mode == 'cauchy_strain':
-                assert_(n_comp == dim)
-                function = terms.dq_cauchy_strain
+                elif mode == 'cauchy_strain':
+                    assert_(n_comp == dim)
+                    function = terms.dq_cauchy_strain
 
-                sym = (dim + 1) * dim / 2
-                out = nm.empty((n_el, n_qp, sym, 1), dtype=self.dtype)
-                function(out, vec, 0, geo, conn)
+                    sym = (dim + 1) * dim / 2
+                    out = nm.empty((n_el, n_qp, sym, 1), dtype=self.dtype)
+                    function(out, vec, 0, geo, conn)
+
+                else:
+                    raise ValueError('unsupported variable evaluation mode! (%s)'
+                                     % mode)
 
             else:
-                raise ValueError('unsupported variable evaluation mode! (%s)'
-                                 % mode)
+                if mode == 'val':
+                    function = terms.dq_state_in_qp
 
+                    rout = nm.empty((n_el, n_qp, n_comp, 1), dtype=nm.float64)
+                    iout = nm.empty((n_el, n_qp, n_comp, 1), dtype=nm.float64)
+                    function(rout, vec.real.copy(), 0, geo.bf, conn)
+                    function(iout, vec.imag.copy(), 0, geo.bf, conn)
+                    out = rout + 1j * iout
+
+                elif mode == 'grad':
+                    function = terms.dq_grad
+
+                    rout = nm.empty((n_el, n_qp, dim, n_comp), dtype=nm.float64)
+                    iout = nm.empty((n_el, n_qp, dim, n_comp), dtype=nm.float64)
+                    function(rout, vec.real.copy(), 0, geo, conn)
+                    function(iout, vec.imag.copy(), 0, geo, conn)
+                    out = rout + 1j * iout
+
+                elif mode == 'div':
+                    assert_(n_comp == dim)
+                    function = terms.dq_div_vector
+
+                    rout = nm.empty((n_el, n_qp, 1, 1), dtype=nm.float64)
+                    iout = nm.empty((n_el, n_qp, 1, 1), dtype=nm.float64)
+                    function(rout, vec.real.copy(), 0, geo, conn)
+                    function(iout, vec.imag.copy(), 0, geo, conn)
+                    out = rout + 1j * iout
+
+                elif mode == 'cauchy_strain':
+                    assert_(n_comp == dim)
+                    function = terms.dq_cauchy_strain
+
+                    sym = (dim + 1) * dim / 2
+                    rout = nm.empty((n_el, n_qp, sym, 1), dtype=nm.float64)
+                    iout = nm.empty((n_el, n_qp, sym, 1), dtype=nm.float64)
+                    function(rout, vec.real.copy(), 0, geo, conn)
+                    function(iout, vec.imag.copy(), 0, geo, conn)
+                    out = rout + 1j * iout
+
+                else:
+                    raise ValueError('unsupported variable evaluation mode! (%s)'
+                                     % mode)
             cache[key] = out
 
         return out
