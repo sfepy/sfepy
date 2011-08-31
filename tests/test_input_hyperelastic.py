c6ae0c8c476a5812ef15ce62a7af8a010c486891
Robert Cimrman <cimrman3@ntc.zcu.cz>
2011-08-31 14:33:45 +0200

    remove tests/test_input_hyperelastic.py
    - test duplicated by test_hyperelastic_tlul.py

--- tests/test_input_hyperelastic.py
+++
@@ -1,7 +0,0 @@
-input_name = '../examples/large_deformation/hyperelastic.py'
-output_name_trunk = 'test_hyperelastic'
-
-from testsBasic import TestInputEvolutionary
-
-class Test( TestInputEvolutionary ):
-    pass
