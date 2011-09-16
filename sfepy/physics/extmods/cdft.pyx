c77399c3942bf7c0f72e70e2a42cfb94dc40fd22
Robert Cimrman <cimrman3@ntc.zcu.cz>
2011-09-16 13:20:59 +0200

    cython: use less cryptic types

--- sfepy/physics/extmods/cdft.pyx
+++ sfepy/physics/extmods/cdft.pyx
@@ -4,22 +4,21 @@ cimport cython
 import numpy as np
 cimport numpy as np
 
-DTYPE = np.float64
-ctypedef np.float64_t DTYPE_t
+ctypedef np.float64_t float64
 
 cdef extern from "dft.h":
     double vxc(double n, int mode, int relat)
 
 @cython.boundscheck(False)
-def get_vxc(np.ndarray[DTYPE_t, mode='c', ndim=4] n_qp not None,
+def get_vxc(np.ndarray[float64, mode='c', ndim=4] n_qp not None,
             int mode=0, int relat=0):
     cdef unsigned int ii
     cdef int num = n_qp.shape[0]
     cdef int nq = n_qp.shape[1]
-    cdef np.ndarray[DTYPE_t, ndim=4] out = np.zeros((num, nq, 1, 1),
-                                                    dtype=DTYPE)
-    cdef DTYPE_t *pout = &out[0, 0, 0, 0]
-    cdef DTYPE_t *pn_qp = &n_qp[0, 0, 0, 0]
+    cdef np.ndarray[float64, ndim=4] out = np.zeros((num, nq, 1, 1),
+                                                    dtype=np.float64)
+    cdef float64 *pout = &out[0, 0, 0, 0]
+    cdef float64 *pn_qp = &n_qp[0, 0, 0, 0]
 
     assert (n_qp.shape[2] == 1) and (n_qp.shape[3] == 1)
 
