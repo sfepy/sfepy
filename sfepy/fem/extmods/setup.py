7054c9019176984abf8605e669892454b0d59d1b
Robert Cimrman <cimrman3@ntc.zcu.cz>
2011-09-20 13:46:51 +0200

    cython: update setup.py for bases.pyx

--- sfepy/fem/extmods/setup.py
+++ sfepy/fem/extmods/setup.py
@@ -58,6 +58,14 @@ def configuration(parent_package='', top_path=None):
                          include_dirs=[auto_dir],
                          define_macros=defines)
 
+    src = ['bases.pyx']
+    config.add_extension('bases',
+                         sources=src,
+                         extra_compile_args=site_config.compile_flags(),
+                         extra_link_args=site_config.link_flags(),
+                         include_dirs=[auto_dir],
+                         define_macros=defines)
+
     return config
 
 if __name__ == '__main__':
