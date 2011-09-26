590c97008eeb25d06e6df748ec768980cc51b9da
Robert Cimrman <cimrman3@ntc.zcu.cz>
2011-09-26 15:54:14 +0200

    cython: new _fmfield.pyx, _fmfield.pxd - FMField-related wrappers
    - new array2fmfield*()

---
+++ sfepy/fem/extmods/_fmfield.pyx
@@ -0,0 +1,50 @@
+# -*- Mode: Python -*-
+cimport cython
+
+@cython.boundscheck(False)
+cdef array2fmfield4(FMField *out, np.ndarray[float64, mode='c', ndim=4] arr):
+    cdef int32 n_cell, n_lev, n_row, n_col
+    cdef int32 ii, stride
+
+    sh = arr.shape
+    n_cell, n_lev, n_row, n_col = sh[0], sh[1], sh[2], sh[3]
+
+    out.nAlloc = -1
+    fmf_pretend(out, n_cell, n_lev, n_row, n_col, &arr[0, 0, 0, 0])
+    out.stride = 1
+
+@cython.boundscheck(False)
+cdef array2fmfield3(FMField *out, np.ndarray[float64, mode='c', ndim=3] arr):
+    cdef int32 n_cell = 1, n_lev, n_row, n_col
+    cdef int32 ii, stride
+
+    sh = arr.shape
+    n_lev, n_row, n_col = sh[0], sh[1], sh[2]
+
+    out.nAlloc = -1
+    fmf_pretend(out, n_cell, n_lev, n_row, n_col, &arr[0, 0, 0])
+    out.stride = 1
+
+@cython.boundscheck(False)
+cdef array2fmfield2(FMField *out, np.ndarray[float64, mode='c', ndim=2] arr):
+    cdef int32 n_cell = 1, n_lev = 1, n_row, n_col
+    cdef int32 ii, stride
+
+    sh = arr.shape
+    n_row, n_col = sh[0], sh[1]
+
+    out.nAlloc = -1
+    fmf_pretend(out, n_cell, n_lev, n_row, n_col, &arr[0, 0])
+    out.stride = 1
+
+@cython.boundscheck(False)
+cdef array2fmfield1(FMField *out, np.ndarray[float64, mode='c', ndim=1] arr):
+    cdef int32 n_cell = 1, n_lev = 1, n_row = 1, n_col
+    cdef int32 ii, stride
+
+    sh = arr.shape
+    n_col = sh[0]
+
+    out.nAlloc = -1
+    fmf_pretend(out, n_cell, n_lev, n_row, n_col, &arr[0])
+    out.stride = 1
