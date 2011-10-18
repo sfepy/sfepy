62c452fa262d716f61516daae062099279ee56db
Robert Cimrman <cimrman3@ntc.zcu.cz>
2011-10-18 15:42:35 +0200

    add test for refine_3_8()
    - new refine(), test_refine_hexa()
    - rename test_refine() -> test_refine_tetra()

--- tests/test_domain.py
+++ tests/test_domain.py
@@ -1,15 +1,23 @@
 import os.path as op
 
 from sfepy.base.testing import TestCommon
+from sfepy import data_dir
+from sfepy.fem import Mesh, Domain
+
+def refine(domain, out_dir, level=3):
+    for ii in range(3):
+        domain = domain.refine()
+        filename = op.join(out_dir, 'refine_' + domain.mesh.name + '.mesh')
+        domain.mesh.write(filename, io='auto')
+
+    return domain
 
 class Test(TestCommon):
 
     @staticmethod
     def from_conf(conf, options):
-        from sfepy import data_dir
-        from sfepy.fem import Mesh, Domain
-
-        mesh = Mesh('mesh', data_dir + '/meshes/various_formats/small3d.mesh')
+        mesh = Mesh('mesh_tetra',
+                    data_dir + '/meshes/various_formats/small3d.mesh')
         domain = Domain('domain', mesh)
 
         return Test(conf=conf, options=options, domain=domain)
@@ -29,13 +37,16 @@ class Test(TestCommon):
 
         return ok
 
-    def test_refine(self):
-        domain = self.domain
+    def test_refine_tetra(self):
+        refine(self.domain, self.options.out_dir)
 
-        for ii in range(3):
-            domain = domain.refine()
-            filename = op.join(self.options.out_dir,
-                               'refine_' + domain.mesh.name + '.mesh')
-            domain.mesh.write(filename, io='auto')
+        return True
+
+    def test_refine_hexa(self):
+        mesh = Mesh('mesh_hexa',
+                    data_dir + '/meshes/various_formats/abaqus_hex.inp')
+        domain = Domain('domain', mesh)
+
+        refine(domain, self.options.out_dir)
 
         return True
