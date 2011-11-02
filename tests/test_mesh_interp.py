4442bccb3c2fa45c8681a0f63b32206089872937
Robert Cimrman <cimrman3@ntc.zcu.cz>
2011-11-02 17:10:29 +0100

    test mesh interpolation invariance (FieldVariable.evaluate_at())
    - new gen_datas(), test_invariance(), test_invariance_qp()

--- tests/test_mesh_interp.py
+++ tests/test_mesh_interp.py
@@ -13,7 +13,26 @@ variables = {
 def in_dir(adir):
     return lambda x: op.join(adir, x)
 
-def do_interpolation(m2, m1, data, field_name):
+def gen_datas(meshes):
+    datas = {}
+
+    for key, mesh in meshes.iteritems():
+        bbox = mesh.get_bounding_box()
+        nx = bbox[1,0] - bbox[0,0]
+        centre = 0.5 * bbox.sum(axis=0)
+        mesh.coors -= centre
+
+        data = nm.sin(4.0 * nm.pi * mesh.coors[:,0:1] / nx)
+        datas['scalar_' + key] = data
+
+        data = nm.zeros_like(mesh.coors)
+        data[:,0] = 0.05 * nx * nm.sin(4.0 * nm.pi * mesh.coors[:,0] / nx)
+        data[:,2] = 0.05 * nx * nm.cos(4.0 * nm.pi * mesh.coors[:,0] / nx)
+        datas['vector_' + key] = data
+
+    return datas
+
+def do_interpolation(m2, m1, data, field_name, force=False):
     """Interpolate data from m1 to m2. """
     from sfepy.fem import Domain, Field, Variables
 
@@ -46,12 +65,18 @@ def do_interpolation(m2, m1, data, field_name):
     vv2 = Variables.from_conf(transform_variables(variables), ff2)
     u2 = vv2['u']
 
-    # Performs interpolation, if other field differs from self.field
-    # or, in particular, is defined on a different mesh.
-    u2.set_from_other(u1, strategy='interpolation', close_limit=0.5)
+    if not force:
+        # Performs interpolation, if other field differs from self.field
+        # or, in particular, is defined on a different mesh.
+        u2.set_from_other(u1, strategy='interpolation', close_limit=0.5)
+
+    else:
+        coors = u2.field.get_coor()
+        vals = u1.evaluate_at(coors, close_limit=0.5)
+        u2.data_from_any(vals)
 
     return u1, u2
- 
+
 class Test(TestCommon):
 
     @staticmethod
@@ -71,21 +96,7 @@ class Test(TestCommon):
             'si' : Mesh('original mesh', data_dir + '/meshes/3d/cylinder.mesh'),
         }
 
-        datas = {}
-
-        for key, mesh in meshes.iteritems():
-            bbox = mesh.get_bounding_box()
-            nx = bbox[1,0] - bbox[0,0]
-            centre = 0.5 * bbox.sum(axis=0)
-            mesh.coors -= centre
-            
-            data = nm.sin(4.0 * nm.pi * mesh.coors[:,0:1] / nx)
-            datas['scalar_' + key] = data
-
-            data = nm.zeros_like(mesh.coors)
-            data[:,0] = 0.05 * nx * nm.sin(4.0 * nm.pi * mesh.coors[:,0] / nx)
-            data[:,2] = 0.05 * nx * nm.cos(4.0 * nm.pi * mesh.coors[:,0] / nx)
-            datas['vector_' + key] = data
+        datas = gen_datas(meshes)
 
         for field_name in ['scalar_si', 'vector_si', 'scalar_tp', 'vector_tp']:
             m1 = meshes[field_name[-2:]]
@@ -107,7 +118,7 @@ class Test(TestCommon):
 
                 u2.save_as_mesh(fname('test_mesh_interp_%s_u2.%03d.vtk'
                                       % (field_name, ia)))
-       
+
         return True
 
     def test_interpolation_two_meshes(self):
@@ -160,3 +171,72 @@ class Test(TestCommon):
         u2.save_as_mesh(fname('test_mesh_interp_cube_scalar.vtk'))
 
         return True
+
+    def test_invariance(self):
+        from sfepy import data_dir
+        from sfepy.fem import Mesh
+
+        meshes = {
+            'tp' : Mesh('original mesh', data_dir + '/meshes/3d/block.mesh'),
+            'si' : Mesh('original mesh', data_dir + '/meshes/3d/cylinder.mesh'),
+        }
+        datas = gen_datas(meshes)
+
+        ok = True
+        for field_name in ['scalar_si', 'vector_si', 'scalar_tp', 'vector_tp']:
+            m1 = meshes[field_name[-2:]]
+
+            data = datas[field_name]
+            u1, u2 = do_interpolation(m1, m1, data, field_name, force=True)
+
+            self.report('max. difference:', nm.abs(u1() - u2()).max())
+            _ok = nm.allclose(u1(), u2(), rtol=0.0, atol=1e-12)
+            self.report('invariance for %s field: %s' % (field_name, _ok))
+
+            ok = ok and _ok
+
+        return ok
+
+    def test_invariance_qp(self):
+        from sfepy import data_dir
+        from sfepy.fem import Mesh, Domain, Field, Variables, Integral
+        from sfepy.terms import Term
+        from sfepy.fem.mappings import get_physical_qps
+
+        mesh = Mesh('source mesh', data_dir + '/meshes/3d/block.mesh')
+
+        bbox = mesh.get_bounding_box()
+        dd = bbox[1,:] - bbox[0,:]
+        data = nm.sin(4.0 * nm.pi * mesh.coors[:,0:1] / dd[0]) \
+               * nm.cos(4.0 * nm.pi * mesh.coors[:,1:2] / dd[1])
+
+        variables = {
+            'u'       : ('unknown field', 'scalar_tp', 0),
+            'v'       : ('test field',    'scalar_tp', 'u'),
+        }
+
+        domain = Domain('domain', mesh)
+        omega = domain.create_region('Omega', 'all')
+        field = Field('scalar_tp', nm.float64, 1, omega, approx_order=1)
+        ff = {field.name : field}
+
+        vv = Variables.from_conf(transform_variables(variables), ff)
+        u = vv['u']
+        u.set_from_mesh_vertices(data)
+
+        integral = Integral('i', order=2)
+        term = Term.new('di_volume_integrate(u)', integral, omega, u=u)
+        term.setup()
+        val1, _ = term.evaluate(mode='qp')
+        val1 = val1.ravel()
+
+        qps = get_physical_qps(omega, integral)
+        coors = qps.get_merged_values()
+
+        val2 = u.evaluate_at(coors).ravel()
+
+        self.report('max. difference:', nm.abs(val1 - val2).max())
+        ok = nm.allclose(val1, val2, rtol=0.0, atol=1e-12)
+        self.report('invariance in qp: %s' % ok)
+
+        return ok
