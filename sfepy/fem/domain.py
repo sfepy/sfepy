5e3df9ff8921d2d768d6a1e6e2a6edf5972229a2
Robert Cimrman <cimrman3@ntc.zcu.cz>
2011-11-16 18:44:03 +0100

    update Domain.refine() for 2D meshes

--- sfepy/fem/domain.py
+++ sfepy/fem/domain.py
@@ -8,7 +8,7 @@ from sfepy.fem.facets import Facets
 from geometry_element import GeometryElement
 from region import Region, get_dependency_graph, sort_by_dependency, get_parents
 from sfepy.fem.parseReg import create_bnf, visit_stack, ParseException
-from sfepy.fem.refine import refine_3_4, refine_3_8
+from sfepy.fem.refine import refine_2_3, refine_2_4, refine_3_4, refine_3_8
 from sfepy.fem.fe_surface import FESurface
 import fea
 
@@ -555,28 +555,33 @@ class Domain( Struct ):
 
         Notes
         -----
-        Works only for 3D meshes with single element type! Does not
+        Works only for meshes with single element type! Does not
         preserve node groups!
         """
-        msg = 'refine() works only for 3D meshes with single element type!'
-        if self.mesh.dim != 3:
-            raise NotImplementedError(msg)
 
         names = set()
         for group in self.groups.itervalues():
             names.add(group.gel.name)
 
         if len(names) != 1:
+            msg = 'refine() works only for meshes with single element type!'
             raise NotImplementedError(msg)
 
         el_type = names.pop()
-        if el_type == '3_4':
+        if el_type == '2_3':
+            mesh = refine_2_3(self.mesh, self.ed)
+
+        elif el_type == '2_4':
+            mesh = refine_2_4(self.mesh, self.ed)
+
+        elif el_type == '3_4':
             mesh = refine_3_4(self.mesh, self.ed)
 
         elif el_type == '3_8':
             mesh = refine_3_8(self.mesh, self.ed, self.fa)
 
         else:
+            msg = 'unsupported element type! (%s)' % el_type
             raise NotImplementedError(msg)
 
         domain = Domain(self.name + '_r', mesh)
