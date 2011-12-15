400a605900bb8de193ee9e30e2d10a7fc76fa7ce
Robert Cimrman <cimrman3@ntc.zcu.cz>
2011-12-15 15:09:05 +0100

    speed-up assemble.pyx by using more pointer arithmetics

--- sfepy/fem/extmods/assemble.pyx
+++ sfepy/fem/extmods/assemble.pyx
@@ -18,17 +18,22 @@ def assemble_vector(np.ndarray[float64, mode='c', ndim=1] vec not None,
     cdef int ii, iel, ir, irg
     cdef int num = iels.shape[0]
     cdef int n_ep = conn.shape[1]
-    cdef int32 *pconn
+    # Allow both row or column vectors.
+    cdef int cell_size = vec_in_els.shape[2] * vec_in_els.shape[3]
+    cdef int32 *pconn0, *pconn
     cdef float64 *val = &vec[0]
-    cdef float64 *vec_in_el
+    cdef float64 *vec_in_el0, *vec_in_el
 
     assert num == vec_in_els.shape[0]
+
+    pconn0 = &conn[0, 0]
+    vec_in_el0 = &vec_in_els[0, 0, 0, 0]
 
     for ii in range(0, num):
         iel = iels[ii]
 
-        pconn = &conn[iel, 0]
-        vec_in_el = &vec_in_els[ii, 0, 0, 0]
+        pconn = pconn0 + iel * n_ep
+        vec_in_el = vec_in_el0 + ii * cell_size
 
         for ir in range(0, n_ep):
             irg = pconn[ir]
@@ -47,17 +52,22 @@ def assemble_vector_complex(np.ndarray[complex128, mode='c', ndim=1]
     cdef int ii, iel, ir, irg
     cdef int num = iels.shape[0]
     cdef int n_ep = conn.shape[1]
-    cdef int32 *pconn
+    # Allow both row or column vectors.
+    cdef int cell_size = vec_in_els.shape[2] * vec_in_els.shape[3]
+    cdef int32 *pconn0, *pconn
     cdef complex128 *val = &vec[0]
-    cdef complex128 *vec_in_el
+    cdef complex128 *vec_in_el0, *vec_in_el
 
     assert num == vec_in_els.shape[0]
+
+    pconn0 = &conn[0, 0]
+    vec_in_el0 = &vec_in_els[0, 0, 0, 0]
 
     for ii in range(0, num):
         iel = iels[ii]
 
-        pconn = &conn[iel, 0]
-        vec_in_el = &vec_in_els[ii, 0, 0, 0]
+        pconn = pconn0 + iel * n_ep
+        vec_in_el = vec_in_el0 + ii * cell_size
 
         for ir in range(0, n_ep):
             irg = pconn[ir]
@@ -78,18 +88,25 @@ def assemble_matrix(np.ndarray[float64, mode='c', ndim=1] mtx not None,
     cdef int num = iels.shape[0]
     cdef int n_epr = row_conn.shape[1]
     cdef int n_epc = col_conn.shape[1]
-    cdef int32 *prow_conn, *pcol_conn
+    cdef int cell_size = mtx_in_els.shape[2] * mtx_in_els.shape[3]
+    cdef int32 *prow_conn0, *pcol_conn0, *prow_conn, *pcol_conn
+    cdef int32 *_prows = &prows[0]
+    cdef int32 *_cols = &cols[0]
     cdef float64 *val = &mtx[0]
-    cdef float64 *mtx_in_el
+    cdef float64 *mtx_in_el0, *mtx_in_el
 
     assert num == mtx_in_els.shape[0]
+
+    prow_conn0 = &row_conn[0, 0]
+    pcol_conn0 = &col_conn[0, 0]
+    mtx_in_el0 = &mtx_in_els[0, 0, 0, 0]
 
     for ii in range(0, num):
         iel = iels[ii]
 
-        prow_conn = &row_conn[iel, 0]
-        pcol_conn = &col_conn[iel, 0]
-        mtx_in_el = &mtx_in_els[ii, 0, 0, 0]
+        prow_conn = prow_conn0 + iel * n_epr
+        pcol_conn = pcol_conn0 + iel * n_epc
+        mtx_in_el = mtx_in_el0 + ii * cell_size
 
         for ir in range(0, n_epr):
             irg = prow_conn[ir]
@@ -102,8 +119,8 @@ def assemble_matrix(np.ndarray[float64, mode='c', ndim=1] mtx not None,
                 iloc = n_epc * ir + ic
 
                 found = 0
-                for ik in range(prows[irg], prows[irg + 1]):
-                    if cols[ik] == icg:
+                for ik in range(_prows[irg], _prows[irg + 1]):
+                    if _cols[ik] == icg:
                         val[ik] += sign * mtx_in_el[iloc]
                         break
 
@@ -128,18 +145,25 @@ def assemble_matrix_complex(np.ndarray[complex128, mode='c', ndim=1]
     cdef int num = iels.shape[0]
     cdef int n_epr = row_conn.shape[1]
     cdef int n_epc = col_conn.shape[1]
-    cdef int32 *prow_conn, *pcol_conn
+    cdef int cell_size = mtx_in_els.shape[2] * mtx_in_els.shape[3]
+    cdef int32 *prow_conn0, *pcol_conn0, *prow_conn, *pcol_conn
+    cdef int32 *_prows = &prows[0]
+    cdef int32 *_cols = &cols[0]
     cdef complex128 *val = &mtx[0]
-    cdef complex128 *mtx_in_el
+    cdef complex128 *mtx_in_el0, *mtx_in_el
 
     assert num == mtx_in_els.shape[0]
+
+    prow_conn0 = &row_conn[0, 0]
+    pcol_conn0 = &col_conn[0, 0]
+    mtx_in_el0 = &mtx_in_els[0, 0, 0, 0]
 
     for ii in range(0, num):
         iel = iels[ii]
 
-        prow_conn = &row_conn[iel, 0]
-        pcol_conn = &col_conn[iel, 0]
-        mtx_in_el = &mtx_in_els[ii, 0, 0, 0]
+        prow_conn = prow_conn0 + iel * n_epr
+        pcol_conn = pcol_conn0 + iel * n_epc
+        mtx_in_el = mtx_in_el0 + ii * cell_size
 
         for ir in range(0, n_epr):
             irg = prow_conn[ir]
@@ -152,8 +176,8 @@ def assemble_matrix_complex(np.ndarray[complex128, mode='c', ndim=1]
                 iloc = n_epc * ir + ic
 
                 found = 0
-                for ik in range(prows[irg], prows[irg + 1]):
-                    if cols[ik] == icg:
+                for ik in range(_prows[irg], _prows[irg + 1]):
+                    if _cols[ik] == icg:
                         val[ik] += sign * mtx_in_el[iloc]
                         break
 
