5e5c532751c6b825e4d1e32d3ddbdd802c32a120
Robert Cimrman <cimrman3@ntc.zcu.cz>
2011-12-22 12:33:56 +0100

    fix implicit function declaration warnings in generated terms.c

--- sfepy/terms/extmods/terms.h
+++ sfepy/terms/extmods/terms.h
@@ -11,6 +11,20 @@ BEGIN_C_DECLS
 #include "fmfield.h"
 #include "geommech.h"
 
+#include "termsAcoustic.h"
+#include "termsAdjointNavierStokes.h"
+#include "termsBasic.h"
+#include "termsBiot.h"
+#include "termsElectric.h"
+#include "termsHyperElasticity.h"
+#include "termsLaplace.h"
+#include "termsLinElasticity.h"
+#include "termsMass.h"
+#include "termsNavierStokes.h"
+#include "termsPiezo.h"
+#include "termsSurface.h"
+#include "termsVolume.h"
+
 END_C_DECLS
 
 #endif /* Header */
