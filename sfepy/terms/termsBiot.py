f4e561cd1d7ba4ae03f0957aa38ff72720cc9c9b
Robert Cimrman <cimrman3@ntc.zcu.cz>
2012-02-02 11:42:48 +0100

    update BiotTHTerm, BiotETHTerm for new term evaluation

--- sfepy/terms/termsBiot.py
+++ sfepy/terms/termsBiot.py
@@ -1,8 +1,7 @@
 import numpy as nm
 
-from sfepy.base.base import use_method_with_name
 from sfepy.terms.terms import Term, terms
-from sfepy.terms.terms_base import CouplingVectorScalar, CouplingVectorScalarTH
+from sfepy.terms.terms_th import THTerm, ETHTerm
 from sfepy.terms.termsLinElasticity import CauchyStrainTerm
 
 class BiotTerm(Term):
@@ -155,72 +154,7 @@ class BiotStressQTerm(Term):
 
             yield stress, chunk, 0
 
-
-class BiotGradTH( CouplingVectorScalarTH ):
-
-    def get_fargs_grad( self, diff_var = None, chunk_size = None, **kwargs ):
-        ts, mats, virtual, state = self.get_args( **kwargs )
-        apr, vgr = self.get_approximation(virtual)
-        apc, vgc = self.get_approximation(state)
-
-        self.set_data_shape( apr, apc )
-        shape, mode = self.get_shape_grad( diff_var, chunk_size )
-
-        if (ts.step == 0) and (mode == 0):
-            raise StopIteration
-
-        bf = apc.get_base('v', 0, self.integral)
-        n_el, n_qp = self.data_shape_r[:2]
-
-        if mode == 1:
-            aux = nm.array( [0], ndmin = 4, dtype = nm.float64 )
-            mat = mats[0]
-            mat = nm.tile(mat, (n_el, n_qp, 1, 1))
-            return (ts.dt, aux, bf, mat, vgr), shape, mode
-
-        else:
-            cache = self.get_cache( 'state_in_volume_qp', 0 )
-            def iter_kernel():
-                for ii, mat in enumerate( mats ):
-                    vec_qp = cache('state', self, ii,
-                                   state=state, get_vector=self.get_vector)
-                    mat = nm.tile(mat, (n_el, n_qp, 1, 1))
-                    yield ii, (ts.dt, vec_qp, bf, mat, vgr)
-            return iter_kernel, shape, mode
-
-class BiotDivTH( CouplingVectorScalarTH ):
-
-    def get_fargs_div( self, diff_var = None, chunk_size = None, **kwargs ):
-        ts, mats, state, virtual = self.get_args( **kwargs )
-        apr, vgr = self.get_approximation(virtual)
-        apc, vgc = self.get_approximation(state)
-
-        self.set_data_shape( apr, apc )
-        shape, mode = self.get_shape_div( diff_var, chunk_size )
-
-        if (ts.step == 0) and (mode == 0):
-            raise StopIteration
-
-        bf = apr.get_base('v', 0, self.integral)
-        n_el, n_qp = self.data_shape_r[:2]
-
-        if mode == 1:
-            aux = nm.array( [0], ndmin = 4, dtype = nm.float64 )
-            mat = mats[0]
-            mat = nm.tile(mat, (n_el, n_qp, 1, 1))
-            return (ts.dt, aux, bf, mat, vgc), shape, mode
-
-        else:
-            cache = self.get_cache( 'cauchy_strain', 0 )
-            def iter_kernel():
-                for ii, mat in enumerate( mats ):
-                    strain = cache('strain', self, ii,
-                                   state=state, get_vector=self.get_vector)
-                    mat = nm.tile(mat, (n_el, n_qp, 1, 1))
-                    yield ii, (ts.dt, strain, bf, mat, vgc)
-            return iter_kernel, shape, mode
-
-class BiotTHTerm( BiotGradTH, BiotDivTH, Term ):
+class BiotTHTerm(BiotTerm, THTerm):
     r"""
     :Description:
     Fading memory Biot term. Can use derivatives.
@@ -251,85 +185,40 @@ class BiotTHTerm( BiotGradTH, BiotDivTH, Term ):
                  ('ts', 'material', 'state', 'virtual'))
     modes = ('grad', 'div')
 
-    def set_arg_types( self ):
-        """Dynamically inherits from either BiotGradTH or
-        BiotDivTH."""
+    def get_fargs(self, ts, mats, vvar, svar,
+                  mode=None, term_mode=None, diff_var=None, **kwargs):
         if self.mode == 'grad':
-            self.function = terms.dw_biot_grad
-            use_method_with_name( self, self.get_fargs_grad, 'get_fargs' )
-            self.use_caches = {'state_in_volume_qp' : [['state',
-                                                        {'state' : (-1,-1)}]]}
-        elif self.mode == 'div':
-            self.function = terms.dw_biot_div
-            use_method_with_name( self, self.get_fargs_div, 'get_fargs' )
-            self.use_caches = {'cauchy_strain' : [['state',
-                                                   {'strain' : (-1,-1)}]]}
-        else:
-            raise NotImplementedError
-
-class BiotGradETH( CouplingVectorScalar ):
-
-    def get_fargs_grad( self, diff_var = None, chunk_size = None, **kwargs ):
-        ts, mat0, mat1, virtual, state = self.get_args( **kwargs )
-        apr, vgr = self.get_approximation(virtual)
-        apc, vgc = self.get_approximation(state)
-
-        self.set_data_shape( apr, apc )
-        shape, mode = self.get_shape_grad( diff_var, chunk_size )
-
-        bf = apc.get_base('v', 0, self.integral)
-        if diff_var is None:
-            cache = self.get_cache( 'state_in_volume_qp', 0 )
-            vec_qp = cache('state', self, 0,
-                           state=state, get_vector=self.get_vector)
-
-            cache = self.get_cache('exp_history', 0)
-            increment = cache('increment', self, 0,
-                              decay=mat1, values=vec_qp)
-            history = cache('history', self, 0)
-
-            fargs = (ts.dt, history + increment, bf, mat0, vgr)
-            if ts.step == 0: # Just init the history in step 0.
-                raise StopIteration
+            qp_var, qp_name = svar, 'val'
 
         else:
-            aux = nm.array( [0], ndmin = 4, dtype = nm.float64 )
-            fargs = (ts.dt, aux, bf, mat0, vgr)
+            qp_var, qp_name = vvar, 'cauchy_strain'
 
-        return fargs, shape, mode
+        n_el, n_qp, dim, n_en, n_c = self.get_data_shape(svar)
 
-class BiotDivETH( CouplingVectorScalar ):
+        if mode == 'weak':
+            vvg, _ = self.get_mapping(vvar)
+            svg, _ = self.get_mapping(svar)
 
-    def get_fargs_div( self, diff_var = None, chunk_size = None, **kwargs ):
-        ts, mat0, mat1, state, virtual = self.get_args( **kwargs )
-        apr, vgr = self.get_approximation(virtual)
-        apc, vgc = self.get_approximation(state)
+            if diff_var is None:
+                def iter_kernel():
+                    for ii, mat in enumerate(mats):
+                        val_qp = self.get(qp_var, qp_name, step=-ii)
+                        mat = nm.tile(mat, (n_el, n_qp, 1, 1))
+                        yield ii, (ts.dt, val_qp, svg.bf, mat, vvg, 0)
+                fargs = iter_kernel
 
-        self.set_data_shape( apr, apc )
-        shape, mode = self.get_shape_div( diff_var, chunk_size )
+            else:
+                val_qp = nm.array([0], ndmin=4, dtype=nm.float64)
+                mat = nm.tile(mats[0], (n_el, n_qp, 1, 1))
+                fargs = ts.dt, val_qp, svg.bf, mat, vvg, 1
 
-        bf = apr.get_base('v', 0, self.integral)
-        if diff_var is None:
-            cache = self.get_cache( 'cauchy_strain', 0 )
-            strain = cache('strain', self, 0,
-                           state=state, get_vector=self.get_vector)
-
-            cache = self.get_cache('exp_history', 0)
-            increment = cache('increment', self, 0,
-                              decay=mat1, values=strain)
-            history = cache('history', self, 0)
-
-            fargs = (ts.dt, history + increment, bf, mat0, vgc)
-            if ts.step == 0: # Just init the history in step 0.
-                raise StopIteration
+            return fargs
 
         else:
-            aux = nm.array( [0], ndmin = 4, dtype = nm.float64 )
-            fargs = (ts.dt, aux, bf, mat0, vgc)
+            raise ValueError('unsupported evaluation mode in %s! (%s)'
+                             % (self.name, mode))
 
-        return fargs, shape, mode
-
-class BiotETHTerm( BiotGradETH, BiotDivETH, Term ):
+class BiotETHTerm(BiotTerm, ETHTerm):
     r"""
     :Description:
     This term has the same definition as dw_biot_th, but assumes an
@@ -344,7 +233,7 @@ class BiotETHTerm( BiotGradETH, BiotDivETH, Term ):
         \int_{\Omega} \left [\int_0^t
         \alpha_{ij}(t-\tau) e_{kl}(\ul{u}(\tau)) \difd{\tau} \right] q
         \end{array}
-    
+
     :Arguments 1:
         ts         : :class:`TimeStepper` instance,
         material_0 : :math:`\alpha_{ij}(0)`,
@@ -363,20 +252,34 @@ class BiotETHTerm( BiotGradETH, BiotDivETH, Term ):
     arg_types = (('ts', 'material_0', 'material_1', 'virtual', 'state'),
                  ('ts', 'material_0', 'material_1', 'state', 'virtual'))
     modes = ('grad', 'div')
-    use_caches = {'exp_history' : [['material_0', 'material_1', 'state']]}
 
-    def set_arg_types( self ):
-        """Dynamically inherits from either BiotGradETH or
-        BiotDivETH."""
+    def get_fargs(self, ts, mat0, mat1, vvar, svar,
+                  mode=None, term_mode=None, diff_var=None, **kwargs):
         if self.mode == 'grad':
-            self.function = terms.dw_biot_grad
-            use_method_with_name( self, self.get_fargs_grad, 'get_fargs' )
-            use_caches = {'state_in_volume_qp' : [['state']]}
-        elif self.mode == 'div':
-            self.function = terms.dw_biot_div
-            use_method_with_name( self, self.get_fargs_div, 'get_fargs' )
-            use_caches = {'cauchy_strain' : [['state']]}
-        else:
-            raise NotImplementedError
+            qp_var, qp_name, iv = svar, 'val', 4
 
-        self.use_caches.update(use_caches)
+        else:
+            qp_var, qp_name, iv = vvar, 'cauchy_strain', 3
+
+        if mode == 'weak':
+            vvg, _, key = self.get_mapping(vvar, return_key=True)
+            svg, _ = self.get_mapping(svar)
+
+            if diff_var is None:
+                val_qp = self.get(qp_var, qp_name)
+
+                key += tuple(self.arg_names[ii] for ii in [1, 2, iv])
+                data = self.get_eth_data(key, qp_var, mat1, val_qp)
+
+                val = data.history + data.values
+                fargs = (ts.dt, val, svg.bf, mat0, vvg, 0)
+
+            else:
+                val_qp = nm.array([0], ndmin=4, dtype=nm.float64)
+                fargs = (ts.dt, val_qp, svg.bf, mat0, vvg, 1)
+
+            return fargs
+
+        else:
+            raise ValueError('unsupported evaluation mode in %s! (%s)'
+                             % (self.name, mode))
