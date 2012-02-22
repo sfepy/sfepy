331cdc513b4c49b6aecf8f135e64c3f806230b9c
Robert Cimrman <cimrman3@ntc.zcu.cz>
2012-02-22 15:50:09 +0100

    improve testing of material parameter conversion functions
    - rename test_tensors() -> test_elastic_constants()
    - new test_conversion_functions(), test_stiffness_tensors()

--- tests/test_matcoefs.py
+++ tests/test_matcoefs.py
@@ -6,7 +6,7 @@ class Test(TestCommon):
     def from_conf(conf, options):
         return Test(conf=conf, options=options)
 
-    def test_tensors(self):
+    def test_elastic_constants(self):
         import numpy as nm
         from sfepy.mechanics.matcoefs import ElasticConstants
 
@@ -41,5 +41,100 @@ class Test(TestCommon):
                     self.report('    got:', ec_vals)
 
                 ok = ok and _ok
+
+        return ok
+
+    def test_conversion_functions(self):
+        import numpy as nm
+
+        import sfepy.mechanics.matcoefs as mc
+
+        ok = True
+
+        lam = 1.0
+        mu = 1.5
+
+        ec = mc.ElasticConstants(lam=lam, mu=mu)
+        young, poisson, bulk = ec.get(['young', 'poisson', 'bulk'])
+
+        lam = nm.array([lam] * 3)
+        mu = nm.array([mu] * 3)
+        young = nm.array([young] * 3)
+        poisson = nm.array([poisson] * 3)
+
+        _lam, _mu = mc.youngpoisson_to_lame(young, poisson)
+        _ok = (nm.allclose(lam, _lam, rtol=0.0, atol=1e-14) and
+               nm.allclose(mu, _mu, rtol=0.0, atol=1e-14))
+        self.report('youngpoisson_to_lame():', _ok)
+        if not _ok:
+            self.report('correct:', lam, mu)
+            self.report('    got:', _lam, _mu)
+        ok = ok and _ok
+
+        _bulk = mc.bulk_modulus_youngpoisson(young, poisson)
+        _ok = nm.allclose(bulk, _bulk, rtol=0.0, atol=1e-14)
+        self.report('bulk_modulus_youngpoisson():', _ok)
+        if not _ok:
+            self.report('correct:', bulk)
+            self.report('    got:', _bulk)
+        ok = ok and _ok
+
+        _bulk = mc.bulk_modulus_lame(lam, mu)
+        _ok = nm.allclose(bulk, _bulk, rtol=0.0, atol=1e-14)
+        self.report('bulk_modulus_lame():', _ok)
+        if not _ok:
+            self.report('correct:', bulk)
+            self.report('    got:', _bulk)
+        ok = ok and _ok
+
+        return ok
+
+    def test_stiffness_tensors(self):
+        import numpy as nm
+
+        from sfepy.base.base import assert_
+        import sfepy.mechanics.matcoefs as mc
+
+        ok = True
+
+        lam = 1.0
+        mu = 4.0
+
+        lam = nm.array([lam] * 3)
+        mu = nm.array([mu] * 3)
+
+        d = nm.array([[ 9.,  1.,  1.,  0.,  0.,  0.],
+                      [ 1.,  9.,  1.,  0.,  0.,  0.],
+                      [ 1.,  1.,  9.,  0.,  0.,  0.],
+                      [ 0.,  0.,  0.,  4.,  0.,  0.],
+                      [ 0.,  0.,  0.,  0.,  4.,  0.],
+                      [ 0.,  0.,  0.,  0.,  0.,  4.]])
+
+        _ds = mc.stiffness_tensor_lame(3, lam, mu)
+        assert_(_ds.shape == (3, 6, 6))
+
+        _ok = True
+        for _d in _ds:
+            __ok = nm.allclose(_d, d, rtol=0.0, atol=1e-14)
+            _ok = _ok and __ok
+        self.report('stiffness_tensor_lame():', _ok)
+        ok = ok and _ok
+
+        d = nm.array([[ 6., -2., -2.,  0.,  0.,  0.],
+                      [-2.,  6., -2.,  0.,  0.,  0.],
+                      [-2., -2.,  6.,  0.,  0.,  0.],
+                      [ 0.,  0.,  0.,  4.,  0.,  0.],
+                      [ 0.,  0.,  0.,  0.,  4.,  0.],
+                      [ 0.,  0.,  0.,  0.,  0.,  4.]])
+
+        _ds = mc.stiffness_tensor_lame_mixed(3, lam, mu)
+        assert_(_ds.shape == (3, 6, 6))
+
+        _ok = True
+        for _d in _ds:
+            __ok = nm.allclose(_d, d, rtol=0.0, atol=1e-14)
+            _ok = _ok and __ok
+        self.report('stiffness_tensor_lame_mixed():', _ok)
+        ok = ok and _ok
 
         return ok
