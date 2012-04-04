ea481b195953b64558a64c4c67cf1fe95a4af467
Robert Cimrman <cimrman3@ntc.zcu.cz>
2012-04-04 14:50:55 +0200

    band_gaps_he: update phononic examples for AcousticBandGapsApp
    - new examples/phononic/band_gaps_conf.py
    - update examples/phononic/band_gaps.py, examples/phononic/band_gaps_rigid.py

--- examples/phononic/band_gaps.py
+++ examples/phononic/band_gaps.py
@@ -1,204 +1,108 @@
-# c: 25.09.2007, r: 16.12.2008
-import os
-import numpy as nm
+"""
+Acoustic band gaps in a strongly heterogeneous elastic body, detected using
+homogenization techniques.
 
+A reference periodic cell contains two domains: the stiff matrix :math:`Y_m`
+and the soft (but heavy) inclusion :math:`Y_c`.
+"""
 from sfepy import data_dir
-from sfepy.fem import MeshIO
-from sfepy.homogenization.utils import get_volume
+from sfepy.base.base import Struct
+from sfepy.base.ioutils import InDir
+from sfepy.homogenization.coefficients import Coefficients
 
-import coef_conf_elastic as cconf
-from parametric import vary_incident_wave_dir
+from band_gaps_conf import BandGapsConf, get_pars, clip_sqrt, normalize
 
-filename_mesh = data_dir + '/meshes/2d/special/circle_in_square.mesh'
-## filename_mesh = data_dir + '/meshes/2d/special/circle_in_square_small.mesh'
-## filename_mesh = data_dir + '/meshes/3d/special/cube_sphere.mesh'
-## filename_mesh = data_dir + '/meshes/3d/special/cube_cylinder.mesh'
+clip_sqrt, normalize # Make pyflakes happy...
 
-cwd = os.path.split( os.path.join( os.getcwd(), __file__ ) )[0]
+incwd = InDir(__file__)
 
-homogeneous = False
-fig_suffix = '.pdf'
+filename = data_dir + '/meshes/2d/special/circle_in_square.mesh'
 
-conf_dir = os.path.dirname(__file__)
-io = MeshIO.any_from_filename(filename_mesh,
-                              prefix_dir=conf_dir)
-bbox, dim = io.read_bounding_box(ret_dim = True)
+output_dir = incwd('output/band_gaps')
 
-if homogeneous:
-    matrix_region = 'Y'
-else:
-    matrix_region = 'Y1'
+# aluminium, in 1e+10 Pa
+D_m = get_pars(2, 5.898, 2.681)
+density_m = 0.2799 # in 1e4 kg/m3
+
+# epoxy, in 1e+10 Pa
+D_c = get_pars(2, 0.1798, 0.148)
+density_c = 0.1142 # in 1e4 kg/m3
+
+mat_pars = Coefficients(D_m=D_m, density_m=density_m,
+                        D_c=D_c, density_c=density_c)
+
+region_selects = Struct(matrix=('elements of group 1', {}),
+                        inclusion=('elements of group 2', {}))
+
+corrs_save_names = {'evp' : 'evp', 'corrs_rs' : 'corrs_rs'}
 
 options = {
-    'save_eig_vectors' : (10, 0),
-    'eig_range' : (0, 30), # -> freq_range = eigs[slice(*eig_range)][[0, -1]]
-    'freq_margins' : (10, 10), # % of freq_range
-
-    'feps' : 1e-12, # frequency
-    'zeps' : 1e-12, # zero finding
-    'teps' : 1e-2, # eigenmomentum threshold
-    'teps_rel' : True, # eigenmomentum threshold is relative w.r.t. largest one
-    'freq_step' : 0.0001, # % of freq_range
-#    'eig_vector_transform' : ('select_in_plane', 'z', 1e-1),
     'plot_transform_angle' : None,
     'plot_transform_wave' : ('clip_sqrt', (0, 30)),
     'plot_transform' : ('normalize', (-2, 2)),
 
-    'output_dir' : os.path.join( cwd, 'output/' ),
+    'fig_name' : 'band_gaps',
+    'fig_name_angle' : 'band_gaps_angle',
+    'fig_name_wave' : 'band_gaps_wave',
+    'fig_suffix' : '.pdf',
 
-    'fig_name' : os.path.join( cwd, 'output', 'band_gaps.pdf' ),
-    'fig_name_angle' : os.path.join( cwd, 'output', 'band_gaps_angle.pdf' ),
-    'fig_name_wave' : os.path.join( cwd, 'output', 'band_gaps_wave.pdf' ),
-    
-#    'method' : 'eig.sgscipy',
+    'coefs_filename' : 'coefs.txt',
 
-    'eigenmomentum' : {'var' : 'u',
-                       'regions' : ['Y2'],
-                       'term' : '%.12e * ev_volume_integrate.2.%s( u )'},
-
-    # Used to compute average density.
-    'region_to_material' : {'Y1' : 'matrix',
-                            'Y2' : 'inclusion',},
-    'tensor_names' : {'elastic' : 'D',},
-    'volume' : lambda problem, region_name: get_volume(problem,
-                                                       'displacement_Y',
-                                                       region_name,
-                                                       quad_order=1),
-    'eig_problem' : 'simple',
-
-    'dispersion' : 'simple',
     'incident_wave_dir' : [1.0, 1.0],
-    'dispersion_conf' : {
-        'input' : cconf.define_input(filename_mesh,
-                                     matrix_region, bbox),
-        'module' : cconf,
-    },
-
-    'homogeneous' : homogeneous,
-    'fig_suffix' : fig_suffix,
-#    'parametric_hook' : 'vary_incident_wave_dir',
 
     'plot_options' : {
         'show' : True,
         'legend' : True,
     },
+    'plot_labels' : {
+        'band_gaps' : {
+            'resonance' : r'$\lambda^r$',
+            'masked' : r'masked $\lambda^r$',
+            'eig_min' : r'min eig($M$)',
+            'eig_max' : r'max eig($M$)',
+            'x_axis' : r'$\sqrt{\lambda}$, $\omega$',
+            'y_axis' : r'eigenvalues of mass matrix $M$',
+        },
+    },
     'plot_rsc' : {
-        'eig_min' : {'linewidth' : 0.5, 'color' : 'b', 'linestyle' : '--'},
-        'eig_max' : {'linewidth' : 0.5, 'color' : 'b', 'linestyle' : '-'},
-        'params' : {'axes.labelsize': 'large',
+        'params' : {'axes.labelsize': 'x-large',
                     'text.fontsize': 'large',
                     'legend.fontsize': 'large',
+                    'legend.loc': 1,
                     'xtick.labelsize': 'large',
                     'ytick.labelsize': 'large',
-                    'text.usetex': False},
+                    'text.usetex': True},
     },
 }
 
-regions = {
-    'Y' : ('all', {}),
-    'Y1' : ('elements of group 1', {}),
-    'Y2' : ('elements of group 2', {}),
-    'Y2_Surface': ('r.Y1 *n r.Y2', {'can_cells' : False}),
+evp_options = {
+    'eigensolver' : 'eig.sgscipy',
+    'save_eig_vectors' : (12, 0),
+    'scale_epsilon' : 1.0,
+    'elasticity_contrast' : 1.0,
 }
 
-def get_pars( lam, mu, dim, full = False ):
-    from sfepy.mechanics.matcoefs import stiffness_from_lame, TransformToPlane
-    
-    if full:
-        c = stiffness_from_lame(3, lam, mu)
-        if dim == 2:
-            tr = TransformToPlane()
-            try:
-                c = tr.tensor_plane_stress( c3 = c )
-            except:
-                sym = (dim + 1) * dim / 2
-                c = nm.zeros( (sym, sym), dtype = nm.float64 )
-        return c
-    else:
-        return lam, mu
-
-material_1 = {
-    'name' : 'matrix',
-
-    # aluminium, in 1e+10 Pa
-    'values' : {
-        'lam' : get_pars( 5.898, 2.681, dim )[0],
-        'mu' : get_pars( 5.898, 2.681, dim )[1],
-        'D' : get_pars( 5.898, 2.681, dim, full = True ),
-        'density' : 0.2799, # in 1e4 kg/m3
-    },
-    'flags' : {'special_constant' : True},
+eigenmomenta_options = {
+    # eigenmomentum threshold,
+    'threshold' : 1e-2,
+    # eigenmomentum threshold is relative w.r.t. largest one,
+    'threshold_is_relative' : True,
 }
 
-material_2 = {
-    'name' : 'inclusion',
+band_gaps_options = {
+    'eig_range' : (0, 30), # -> freq_range
+                           # = sqrt(eigs[slice(*eig_range)][[0, -1]])
+    'freq_margins' : (10, 10), # % of freq_range
+    'freq_eps' : 1e-12, # frequency
+    'zezo_eps' : 1e-12, # zero finding
+    'freq_step' : 0.0001, # % of freq_range
 
-    # epoxy, in 1e+10 Pa
-    'values' : {
-        'lam' : get_pars( 0.1798, 0.148, dim )[0],
-        'mu' : get_pars( 0.1798, 0.148, dim )[1],
-        'D' : get_pars( 0.1798, 0.148, dim, full = True ),
-        'density' : 0.1142, # in 1e4 kg/m3
-    },
-    'flags' : {'special_constant' : True},
+    'log_save_name' : 'band_gaps.log',
 }
 
-if homogeneous:
-    material_2['values'] = material_1['values']
+conf = BandGapsConf(filename, 1, region_selects, mat_pars, options,
+                    evp_options, eigenmomenta_options, band_gaps_options,
+                    corrs_save_names=corrs_save_names, incwd=incwd,
+                    output_dir=output_dir)
 
-field_0 = {
-    'name' : 'displacement_Y',
-    'dtype' : nm.float64,
-    'shape' : dim,
-    'region' : 'Y',
-    'approx_order' : 1,
-}
-
-field_1 = {
-    'name' : 'displacement_Y2',
-    'dtype' : nm.float64,
-    'shape' : dim,
-    'region' : 'Y2',
-    'approx_order' : 1,
-}
-
-variables = {
-    'u' : ('unknown field', 'displacement_Y2', 0),
-    'v' : ('test field', 'displacement_Y2', 'u'),
-}
-
-ebc_1 = {
-    'name' : 'ZeroSurface',
-    'region' : 'Y2_Surface',
-    'dofs' : {'u.all' : 0.0},
-}
-
-equations = {
-    'lhs' : """dw_lin_elastic_iso.2.Y2( inclusion.lam, inclusion.mu, v, u )""",
-    'rhs' : """dw_volume_dot.2.Y2( inclusion.density, v, u )""",
-}
-
-def clip( data, plot_range ):
-    return nm.clip( data, *plot_range )
-
-def clip_sqrt( data, plot_range ):
-    return nm.clip( nm.sqrt( data ), *plot_range )
-
-def normalize( data, plot_range ):
-    aux = nm.arctan( data )
-    return clip( aux, plot_range )
-
-##
-# 02.10.2007, c
-def select_in_plane( vec, shape, normal_direction, eps ):
-    n_nod, dim = shape
-    dir_vecs = {2 : {'x': 0, 'y' : 1, 'z' : 1},
-               3 : {'x': 0, 'y' : 1, 'z' : 2}}
-    ident = nm.eye( dim, dtype = nm.float64 )
-    dir_vec = ident[:,dir_vecs[dim][normal_direction]]
-
-    proj = nm.dot( nm.reshape( vec, (n_nod, dim) ), dir_vec )
-    if nm.any( nm.abs( proj ) > eps ):
-        return nm.zeros_like( vec ), True
-    else:
-        return vec, False
+define = lambda: conf.conf.to_dict()
--- examples/phononic/band_gaps_rigid.py
+++ examples/phononic/band_gaps_rigid.py
@@ -1,242 +1,103 @@
-# c: 14.12.2007, r: 03.11.2008
-import os
+"""
+Acoustic band gaps in a strongly heterogeneous elastic body with a rigid
+inclusion, detected using homogenization techniques.
+
+A reference periodic cell contains three domains: the stiff matrix :math:`Y_m`
+and the soft inclusion :math:`Y_c` enclosing the rigid heavy sub-inclusion
+:math:`Y_r`.
+"""
 import numpy as nm
 
 from sfepy import data_dir
-from sfepy.base.base import output, pause, debug, Struct
-from sfepy.fem import MeshIO
-from sfepy.homogenization.utils import get_volume
+from sfepy.base.base import Struct
+from sfepy.base.ioutils import InDir
+from sfepy.fem import extend_cell_data
+from sfepy.linalg import norm_l2_along_axis
+from sfepy.homogenization.coefficients import Coefficients
 
-from gen_mesh import gen_concentric
+from band_gaps_conf import BandGapsRigidConf, get_pars, normalize
 
-is_3D = False
-generate_2D = False
-fig_suffix = '.pdf'
+normalize # Make pyflakes happy...
 
-if is_3D:
-    filename_mesh = data_dir + '/meshes/3d/special/cube_sphere.mesh'
+incwd = InDir(__file__)
 
-    out_groups = [1]
-    in_groups = [2]
-    diameters_g = None
-    tepss_g = nm.logspace( -3, -0.5, 11 )
-    default_y3_diameter = 0.1
-    diameters_g = nm.linspace( 0.075, 0.26, 11 )
+dim = 2
+
+if dim == 3:
+    filename = data_dir + '/meshes/3d/special/cube_sphere.mesh'
+
 else:
-    # This mesh is generated, see below.
-    filename_mesh = data_dir + '/meshes/2d/special//circle_in_square.vtk'
+    filename = data_dir + '/meshes/2d/special/circle_in_square.mesh'
 
-    out_groups = [1]
-    if generate_2D:
-        in_groups, diameters_g = gen_concentric( 'tmp/mesh.geo',
-                                                 1., 0.2, 0.08, 0.1, 0.6, 7 )
-        diameters_g = nm.array( diameters_g[:-2] ) + 0.001
-    else:
-        os.system("cp %s tmp/mesh.geo" % filename_mesh.replace('.vtk', '.geo'))
-        in_groups = [2]
-        diameters_g = None
-    tepss_g = nm.logspace( -3, -1, 11 )
-    default_y3_diameter = 0.25
-    os.system("gmsh -2 tmp/mesh.geo -format mesh")
-    os.system("script/mesh_to_vtk.py tmp/mesh.mesh %s" % filename_mesh)
-    #pause()
 
-cwd = os.path.split( os.path.join( os.getcwd(), __file__ ) )[0]
+output_dir = incwd('output/band_gaps_rigid')
+
+# Rigid inclusion diameter.
+yr_diameter = 0.125
+
+# aluminium, in 1e+10 Pa
+D_m = get_pars(dim, 5.898, 2.681)
+density_m = 0.2799 # in 1e4 kg/m3
+
+# epoxy, in 1e+10 Pa
+D_c = get_pars(dim, 0.1798, 0.148)
+density_c = 0.1142 # in 1e4 kg/m3
+
+# lead, in 1e+10 Pa, does not matter
+D_r = get_pars(dim, 4.074, 0.5556)
+density_r = 1.1340 # in 1e4 kg/m3
+
+mat_pars = Coefficients(D_m=D_m, density_m=density_m,
+                        D_c=D_c, density_c=density_c,
+                        D_r=D_r, density_r=density_r)
+
+region_selects = Struct(matrix=('elements of group 1', {}),
+                        inclusion=('elements of group 2', {}))
+
+corrs_save_names = {'evp' : 'evp'}
+
+evp_options = {
+    'eigensolver' : 'eig.sgscipy',
+    'save_eig_vectors' : (12, 0),
+    'scale_epsilon' : 1.0,
+    'elasticity_contrast' : 1.0,
+}
+
+eigenmomenta_options = {
+    # eigenmomentum threshold,
+    'threshold' : 1e-1,
+    # eigenmomentum threshold is relative w.r.t. largest one,
+    'threshold_is_relative' : True,
+}
+
+band_gaps_options = {
+    'fixed_freq_range' : (0., 35.), # overrides eig_range!
+
+    'freq_eps' : 1e-12, # frequency
+    'zezo_eps' : 1e-12, # zero finding
+    'freq_step' : 0.01, # % of freq_range
+
+    'log_save_name' : 'band_gaps.log',
+}
 
 options = {
-    'save_eig_vectors' : (10, 0),
-    # Either:
-    'eig_range' : (0, 10), # -> freq_range = eigs[slice(*eig_range)][[0, -1]]
-    # Or (this has precedence if not None):
-    'fixed_eig_range' : (0., 50.),
-
-    'freq_margins' : (10, 10), # % of freq_range
-
-    'feps' : 1e-4, # frequency
-    'zeps' : 1e-10, # zero finding
-    'teps' : 1e-1, # eigenmomentum threshold
-    'teps_rel' : True, # eigenmomentum threshold is relative w.r.t. largest one
-    'freq_step' : 0.02, # in percent of freq_range
-#    'eig_vector_transform' : ('select_in_plane', 'z', 1e-1),
-#    'plot_transform' : ('clip', (-20, 20)),
-    'plot_transform' : ('normalize', (-2, 2)),
-#    'plot_transform' : None,
-
-    #############################################
-#    'parametric_hook' : 'vary_y3_size',
-#    'parametric_hook' : 'vary_teps',
     'post_process_hook' : 'post_process',
-    'output_dir' : os.path.join( cwd, 'output/' ),
-    #############################################
 
-    'eigenmomentum' : {'var' : 'u',
-                       'regions' : ['Y2', 'Y3'],
-                       'term' : '%.12e * ev_volume_integrate.2.%s( u )'},
-    # Used to compute average density.
-    'region_to_material' : {'Y1' : 'matrix',
-                            'Y2' : 'inclusion',
-                            'Y3' : 'rigid',},
-    'tensor_names' : {'elastic' : 'lame',},
-    'volume' : lambda problem, region_name: get_volume(problem,
-                                                       'displacement_Y',
-                                                       region_name,
-                                                       quad_order=1),
-    'eig_problem' : 'simple',
+    'plot_transform' : ('normalize', (-2, 2)),
 
-    'fig_name' : os.path.join(cwd, 'output', 'band_gaps_sym_025' + fig_suffix),
+    'fig_name' : 'band_gaps',
+    'fig_suffix' : '.pdf',
+
+    'coefs_filename' : 'coefs.txt',
+
     'plot_options' : {
         'show' : True, # Show figure.
-        'legend' : False, # Show legend.
+        'legend' : True, # Show legend.
     },
-    'plot_rsc' : { # Resources for all plots.
-        'resonance' : {'linewidth' : 0.5, 'color' : 'k', 'linestyle' : '-' },
-        'masked' : {'linewidth' : 0.2, 'color' : 'k', 'linestyle' : ':' },
-        'x_axis' : {'linewidth' : 1, 'color' : 'k', 'linestyle' : '-' },
-        'eig_min' : {'linewidth' : 1, 'color' : 'k', 'linestyle' : '--' },
-        'eig_max' : {'linewidth' : 1, 'color' : 'k', 'linestyle' : '-' },
-        'strong_gap' : {'linewidth' : 0, 'facecolor' : (1, 1, 0.5) },
-        'weak_gap' : {'linewidth' : 0, 'facecolor' : (1, 1, 1) },
-        'propagation' : {'linewidth' : 0, 'facecolor' : (0.5, 1, 0.5) },
-##         'strong_gap' : {'linewidth' : 0, 'facecolor' : (0.6, 0.6, 0.6) },
-##         'weak_gap' : {'linewidth' : 0, 'facecolor' : (0.8, 0.8, 0.8) },
-##         'propagation' : {'linewidth' : 0, 'facecolor' : (1, 1, 1) },
-        'params' : {'axes.labelsize': 'x-large',
-                    'text.fontsize': 'large',
-                    'legend.fontsize': 'large',
-                    'xtick.labelsize': 'large',
-                    'ytick.labelsize': 'large',
-                    'text.usetex': False},
-    },
-    
 }
 
-functions = {
-    'select_y3_circ' : (lambda coors, domain=None, diameter=None:
-                        select_y3_circ(coors, diameter=default_y3_diameter),),
-}
-
-regions = {
-    'Y' : ('all', {}),
-    'Y1' : (' +e '.join( ('elements of group %d' % ig)
-                         for ig in out_groups ), {}),
-    'Y23' : (' +e '.join( ('elements of group %d' % ig)
-                          for ig in in_groups ), {}),
-    'Y3' : ('nodes by select_y3_circ', {}),
-    'Y2' : ('r.Y23 -e r.Y3', {}),
-    'Y23_Surface': ('r.Y1 *n r.Y23', {'can_cells' : False}),
-}
-
-material_1 = {
-    'name' : 'matrix',
-
-    # aluminium
-    'values' : {
-        'lam' : 5.898,
-        'mu' : 2.681,
-        'density' : 0.2799, # in 1e4 kg/m3
-    },
-    'flags' : {'special_constant' : True},
-}
-
-material_2 = {
-    'name' : 'inclusion',
-
-    # epoxy, in 1e+10 Pa
-    'values' : {
-        'lam' : 0.1798,
-        'mu' : 0.148,
-        'density' : 0.1142, # in 1e4 kg/m3
-    },
-    'flags' : {'special_constant' : True},
-}
-
-material_3 = {
-    'name' : 'rigid',
-
-    # lead, in 1e+10 Pa, does not matter
-    'values' : {
-        'lam' : 4.074 ,
-        'mu' : 0.5556,
-        'density' : 1.1340, # in 1e4 kg/m3
-    },
-    'flags' : {'special_constant' : True},
-}
-
-conf_dir = os.path.dirname(__file__)
-dim = MeshIO.any_from_filename(filename_mesh,
-                               prefix_dir=conf_dir).read_dimension()
-
-field_0 = {
-    'name' : 'displacement_Y',
-    'dtype' : nm.float64,
-    'shape' : dim,
-    'region' : 'Y',
-    'approx_order' : 1,
-}
-
-field_1 = {
-    'name' : 'displacement_Y23',
-    'dtype' : nm.float64,
-    'shape' : dim,
-    'region' : 'Y23',
-    'approx_order' : 1,
-}
-
-variables = {
-    'u' : ('unknown field', 'displacement_Y23', 0),
-    'v' : ('test field', 'displacement_Y23', 'u'),
-}
-
-ebc_1 = {
-    'name' : 'ZeroSurface',
-    'region' : 'Y23_Surface',
-    'dofs' : {'u.all' : 0.0},
-}
-
-lcbc_1 = {
-    'name' : 'RigidBody',
-    'region' : 'Y3',
-    'dofs' : {'u.all' : 'rigid'},
-}
-
-##
-# Eigenproblem equations.
-# dw_lin_elastic_iso.i1.Y3( rigid.lame, v, u ) should have no effect!
-equations = {
-    'lhs' : """dw_lin_elastic_iso.2.Y2( inclusion.lam, inclusion.mu, v, u )
-             + dw_lin_elastic_iso.2.Y3( rigid.lam, rigid.mu, v, u )""",
-    'rhs' : """dw_volume_dot.2.Y2( inclusion.density, v, u )
-             + dw_volume_dot.2.Y3( rigid.density, v, u )""",
-}
-
-def clip( data, plot_range ):
-    return nm.clip( data, *plot_range )
-
-def normalize( data, plot_range ):
-    aux = nm.arctan( data )
-    return clip( aux, plot_range )
-
-##
-# 02.10.2007, c
-def select_in_plane( vec, shape, normal_direction, eps ):
-    n_nod, dim = shape
-    dir_vecs = {2 : {'x': 0, 'y' : 1, 'z' : 1},
-               3 : {'x': 0, 'y' : 1, 'z' : 2}}
-    ident = nm.eye( dim, dtype = nm.float64 )
-    dir_vec = ident[:,dir_vecs[dim][normal_direction]]
-
-    proj = nm.dot( nm.reshape( vec, (n_nod, dim) ), dir_vec )
-    if nm.any( nm.abs( proj ) > eps ):
-        return nm.zeros_like( vec ), True
-    else:
-        return vec, False
-
-def select_y3_circ(coors, diameter=None):
-    r = coors[:,0]**2 + coors[:,1]**2
-    if dim == 3:
-        r += coors[:,2]**2
-    r = nm.sqrt( r )
-
+def select_yr_circ(coors, diameter=None):
+    r = norm_l2_along_axis(coors)
     out = nm.where(r < diameter)[0]
 
     if out.shape[0] <= 3:
@@ -244,166 +105,28 @@ def select_y3_circ(coors, diameter=None):
 
     return out
 
-def extend_cell_data( data, pb, rname, val = None ):
-    n_el = pb.domain.shape.n_el
-    if data.shape[0] == n_el: return data
+def _select_yr_circ(coors, domain=None, diameter=None):
+    return select_yr_circ(coors, diameter=yr_diameter)
 
-    if val is None:
-        if data.shape[2] > 1: # Vector.
-            val = nm.amin( nm.abs( data ) )
-        else: # Scalar.
-            val = nm.amin( data )
-
-    edata = nm.empty( (n_el,) + data.shape[1:], dtype = nm.float64 )
-    edata.fill( val )
-    region = pb.domain.regions[rname]
-    offs = region.get_cell_offsets()
-    eoffs = pb.domain.get_cell_offsets()
-##     print offs
-##     print eoffs
-##     print pb.domain.mat_ids_to_i_gs
-##     pause()
-    for group in pb.domain.iter_groups():
-        ig = group.ig
-        ii = eoffs[ig]
-        if ig in region.igs:
-            n_cell = region.shape[ig].n_cell
-            ir = offs[ig]
-            edata[ii+region.cells[ig]] = data[ir:ir+n_cell]
-    return edata
-
-def post_process( out, problem, mtx_phi ):
+def post_process(out, problem, mtx_phi):
     var = problem.get_variables()['u']
 
     for key in out.keys():
-        ii = int( key[1:] )
+        ii = int(key[1:])
         vec = mtx_phi[:,ii].copy()
         var.data_from_any(vec)
 
-        strain = problem.evaluate('ev_cauchy_strain.2.Y23( u )', u=var,
+        strain = problem.evaluate('ev_cauchy_strain.i1.Y_c(u)', u=var,
                                   verbose=False, mode='el_avg')
-        strain = extend_cell_data( strain, problem, 'Y23' )
+        strain = extend_cell_data(strain, problem.domain, 'Y_c')
         out['strain%03d' % ii] = Struct(name='output_data',
                                         mode='cell', data=strain,
                                         dofs=None)
     return out
 
-def save_log( filename, bg, log_item ):
-    """Saves band gaps, valid flags, eigenfrequencies."""
+conf = BandGapsRigidConf(filename, 1, region_selects, mat_pars, options,
+                         evp_options, eigenmomenta_options, band_gaps_options,
+                         corrs_save_names=corrs_save_names, incwd=incwd,
+                         output_dir=output_dir, select_yr=_select_yr_circ)
 
-    fd = open( filename, 'w' )
-    freq_range = bg.freq_range_margins
-    fd.write( log_item )
-    fd.write( 'squared: %s\n' % False )
-    fd.write( 'n_zeroed: %d\n' % bg.n_zeroed )
-    fd.write( 'n_eigs: %d\n' % bg.n_eigs )
-    fd.write( 'f0 f1 flag_min f_min v_min flag_max f_max v_max'
-              ' kind\ndesc\n' )
-    format = "%f %f %d %f %f %d %f %f %s\n%s\n"
-
-    n_row = len( freq_range ) - 1
-    fd.write( '%d\n' % n_row )
-    for ir in xrange( n_row ):
-        f0, f1 = freq_range[[ir, ir+1]]
-        gmin, gmax = bg.gaps[ir]
-        fd.write( format % ((f0, f1) + tuple( gmin ) + tuple( gmax )
-                            + bg.kinds[ir]) )
-
-    fd.write( 'valid resonance\n' )
-    freq_range = bg.freq_range_initial
-    n_row = len( freq_range )
-    fd.write( '%d\n' % n_row )
-    valid_in_range = bg.valid[bg.eig_range]
-    for ir in xrange( n_row ):
-        fd.write( '%d %f\n' % (valid_in_range[ir], freq_range[ir] ) )
-    fd.close()
-
-def vary_teps( problem ):
-    """Vary eigenmomentum threshold."""
-    from sfepy.solvers.ts import get_print_info
-
-    output.prefix = 'vary_teps:'
-
-    if tepss_g is None:
-        tepss = nm.logspace( -3, -1, 11 )
-    else:
-        tepss = tepss_g
-    ofn_trunk, output_dir = problem.ofn_trunk, problem.output_dir
-    join = os.path.join
-
-    n_digit, aux, d_format = get_print_info( len( tepss ) + 1 )
-    
-    for ii, teps in enumerate( tepss ):
-        output( 'iteration %d: teps %.2e' % (ii, teps) )
-        opts = problem.conf.options
-
-        opts.teps = teps
-
-        opts.plot_options['show'] = False
-        opts.fig_name = join( output_dir,
-                              (('band_gaps_%s' % d_format)
-                               + '_teps_%3.2e' + fig_suffix) % (ii, teps) )
-        problem.ofn_trunk = ofn_trunk + '_' + (d_format % ii)
-
-        out = []
-        yield problem, out
-
-        evp, bg = out[-1]
-
-        filename = join( output_dir,
-                         ('band_gaps_%s.txt' % d_format) % ii )
-        log_item = '$10^q$: %f\n' % teps
-        save_log( filename, bg, log_item )
-
-        yield None
-
-def vary_y3_size( problem ):
-    """Vary size of Y3 inclusion."""
-    from sfepy.fem import ProblemDefinition
-    from sfepy.solvers.ts import get_print_info
-    
-    output.prefix = 'vary_y3_size:'
-
-    y3_diameters = [0.2, 0.25, 0.3, 0.35, 0.4]
-    if diameters_g is None:
-        y3_diameters = nm.linspace( 0.15, 0.45, 16 )
-    else:
-        y3_diameters = diameters_g
-#    y3_diameters = [0.45]
-    ofn_trunk, output_dir = problem.ofn_trunk, problem.output_dir
-    join = os.path.join
-
-    conf = problem.conf
-    cr = conf.get_raw( 'regions' )
-    n_digit, aux, d_format = get_print_info( len( y3_diameters ) + 1 )
-    for ii, diameter in enumerate( y3_diameters ):
-        output( 'iteration %d: diameter %3.2f' % (ii, diameter) )
-        opts = problem.conf.options
-
-        cr['Y3'] = ('nodes by select_y3_circ( x, y, z, %.5f )' % diameter, {})
-        conf.edit( 'regions', cr )
-        problem = ProblemDefinition.from_conf( conf )
-
-        problem.save_regions( join( output_dir, ('regions_' + d_format) % ii ),
-                              ['Y2', 'Y3'] )
-        for region in problem.domain.regions:
-            if not region.has_cells_if_can():
-                raise ValueError( 'region %s has no cells!' % region.name )
-
-        opts.plot_options['show'] = False
-        opts.fig_name = join( output_dir,
-                              (('band_gaps_%s' % d_format)
-                               + '_y3_%03.2f' + fig_suffix) % (ii, diameter) )
-        problem.ofn_trunk = ofn_trunk + '_' + (d_format % ii)
-        out = []
-        yield problem, out
-
-        evp, bg = out[-1]
-
-        filename = join( output_dir,
-                         ('band_gaps_%s.txt' % d_format) % ii )
-        log_item = '$r(Y_3)$: %f\n' % diameter
-        save_log( filename, bg, log_item )
-
-        yield None
-
+define = lambda: conf.conf.to_dict()
