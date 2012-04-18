b5159475bf414f9e4679f28ce7b9b3531183a6c1
Robert Cimrman <cimrman3@ntc.zcu.cz>
2012-04-18 15:24:35 +0200

    merge sfepy/solvers/generic.py with sfepy/applications/pde_solver_app.py
    - remove sfepy/solvers/generic.py
    - update solve_pde(), PDESolverApp.call()
    - rename solve_stationary_op() -> solve_stationary(),
      time_step_function() -> make_implicit_step(),
      solve_evolutionary_op() -> solve_evolutionary()

--- sfepy/applications/__init__.py
+++ sfepy/applications/__init__.py
@@ -1,2 +1,3 @@
 from application import Application
-from pde_solver_app import PDESolverApp, solve_pde
+from pde_solver_app import (PDESolverApp, solve_pde, solve_evolutionary,
+                            solve_stationary, assign_standard_hooks)
--- sfepy/applications/pde_solver_app.py
+++ sfepy/applications/pde_solver_app.py
@@ -1,14 +1,14 @@
 import os
+import numpy as nm
 
 from sfepy.base.base import output, dict_to_struct, Struct
 from sfepy.base.conf import ProblemConf, get_standard_keywords
 import sfepy.base.ioutils as io
 from sfepy.fem import ProblemDefinition
 from sfepy.fem.meshio import MeshIO
-from sfepy.solvers.generic import solve_direct
 from application import Application
 
-def solve_pde(conf_filename, options=None, **app_options):
+def solve_pde(conf, options=None, nls_status=None, **app_options):
     """
     Solve a system of partial differential equations (PDEs).
 
@@ -17,15 +17,19 @@ def solve_pde(conf_filename, options=None, **app_options):
 
     Parameters
     ----------
-    conf_filename : str
-        The name of the problem description file defining the PDEs.
+    conf : str or ProblemConf instance
+        Either the name of the problem description file defining the PDEs,
+        or directly the ProblemConf instance.
     options : options
         The command-line options.
+    nls_status : dict-like
+        The object for storing the nonlinear solver return status.
     app_options : kwargs
         The keyword arguments that can override application-specific options.
     """
-    required, other = get_standard_keywords()
-    conf = ProblemConf.from_file(conf_filename, required, other)
+    if not isinstance(conf, ProblemConf):
+        required, other = get_standard_keywords()
+        conf = ProblemConf.from_file(conf, required, other)
 
     opts = conf.options = dict_to_struct(app_options, flag=(1,)) + conf.options
 
@@ -46,7 +50,161 @@ def solve_pde(conf_filename, options=None, **app_options):
         parametric_hook = conf.get_function(opts.parametric_hook)
         app.parametrize(parametric_hook)
 
-    return app()
+    return app(nls_status=nls_status)
+
+def save_only(conf, save_names, problem=None):
+    """
+    Save information available prior to setting equations and
+    solving them.
+    """
+    if problem is None:
+        problem = ProblemDefinition.from_conf(conf, init_equations=False)
+
+    if save_names.regions is not None:
+        problem.save_regions(save_names.regions)
+
+    if save_names.regions_as_groups is not None:
+        problem.save_regions_as_groups(save_names.regions_as_groups)
+
+    if save_names.field_meshes is not None:
+        problem.save_field_meshes(save_names.field_meshes)
+
+    if save_names.ebc is not None:
+        problem.save_ebc(save_names.ebc)
+
+def solve_stationary(problem, save_results=True, ts=None,
+                     post_process_hook=None,
+                     nls_status=None):
+    """
+    Solve a stationary problem.
+    """
+    if ts is None:
+        try:
+            ts = problem.get_time_solver().ts
+        except ValueError:
+            pass
+
+    problem.time_update(ts)
+    state = problem.solve(nls_status=nls_status)
+
+    if save_results:
+        problem.save_state(problem.get_output_name(), state,
+                           post_process_hook=post_process_hook,
+                           file_per_var=None)
+
+    return state
+
+def prepare_matrix(problem, state):
+    """
+    Pre-assemble tangent system matrix.
+    """
+    problem.update_materials()
+
+    ev = problem.get_evaluator()
+    try:
+        mtx = ev.eval_tangent_matrix(state(), is_full=True)
+
+    except ValueError:
+        output('matrix evaluation failed, giving up...')
+        raise
+
+    return mtx
+
+def prepare_save_data(ts, conf):
+    try:
+        save_steps = conf.options.save_steps
+    except:
+        save_steps = -1
+
+    if save_steps == -1:
+        save_steps = ts.n_step
+
+    is_save = nm.linspace(0, ts.n_step - 1, save_steps).astype(nm.int32)
+    is_save = nm.unique(is_save)
+
+    return ts.suffix, is_save
+
+def make_implicit_step(ts, state0, problem, nls_status=None):
+    problem.time_update(ts)
+
+    if ts.step == 0:
+        state0.apply_ebc()
+        state = state0.copy(deep=True)
+
+        if not ts.is_quasistatic:
+            problem.init_time(ts)
+
+            ev = problem.get_evaluator()
+            try:
+                vec_r = ev.eval_residual(state(), is_full=True)
+            except ValueError:
+                output('initial residual evaluation failed, giving up...')
+                raise
+            else:
+                err = nm.linalg.norm(vec_r)
+                output('initial residual: %e' % err)
+
+        if problem.is_linear():
+            mtx = prepare_matrix(problem, state)
+
+        else:
+            mtx = None
+
+        # Initialize solvers (and possibly presolve the matrix).
+        presolve = mtx is not None
+        problem.init_solvers(nls_status=nls_status, mtx=mtx, presolve=presolve)
+
+        # Initialize variables with history.
+        state0.init_history()
+        if ts.is_quasistatic:
+            # Ordinary solve.
+            state = problem.solve(state0=state0)
+
+    else:
+        if (ts.step == 1) and ts.is_quasistatic and problem.is_linear():
+            mtx = prepare_matrix(problem, state0)
+            problem.init_solvers(nls_status=nls_status, mtx=mtx)
+
+        state = problem.solve(state0=state0)
+
+    return state
+
+def solve_evolutionary(problem, time_solver=None,
+                       save_results=True, return_history=False,
+                       step_hook=None, post_process_hook=None,
+                       nls_status=None):
+    """
+    Solve an evolutionary problem.
+
+    TODO:  return_history
+    """
+    if time_solver is None:
+        time_solver = problem.get_time_solver(step_fun=make_implicit_step,
+                                              step_args=(problem, nls_status))
+
+    suffix, is_save = prepare_save_data(time_solver.ts, problem.conf)
+
+    state0 = problem.create_state()
+    problem.setup_ic()
+    state0.apply_ic()
+
+    ii = 0
+    for ts, state in time_solver(state0):
+
+        if step_hook is not None:
+            step_hook(problem, ts, state)
+
+        if save_results and (is_save[ii] == ts.step):
+            filename = problem.get_output_name(suffix=suffix % ts.step)
+            problem.save_state(filename, state,
+                               post_process_hook=post_process_hook,
+                               file_per_var=None,
+                               ts=ts)
+            ii += 1
+
+        problem.advance(ts)
+
+    return state
 
 def assign_standard_hooks(obj, get, conf):
     """
@@ -149,15 +307,52 @@ class PDESolverApp(Application):
                              file_per_var=self.app_options.file_per_var,
                              linearization=self.app_options.linearization)
 
-    def call( self ):
-        out = solve_direct( self.conf, self.options,
-                            problem=self.problem,
-                            step_hook=self.step_hook,
-                            post_process_hook=self.post_process_hook,
-                            post_process_hook_final=self.post_process_hook_final,
-                            pre_process_hook=self.pre_process_hook)
+    def call(self, nls_status=None):
+        problem = self.problem
+        options = self.options
 
-        return out
+        if self.pre_process_hook is not None: # User pre_processing.
+            self.pre_process_hook(problem)
+
+        ofn_trunk = problem.ofn_trunk
+        self.save_names = Struct(ebc=ofn_trunk + '_ebc.vtk'
+                                 if options.save_ebc else None,
+
+                                 regions=ofn_trunk + '_region'
+                                 if options.save_regions else None,
+
+                                 regions_as_groups=ofn_trunk + '_regions'
+                                 if options.save_regions_as_groups else None,
+
+                                 field_meshes=ofn_trunk + '_field'
+                                 if options.save_field_meshes else None)
+
+        if any(self.save_names.to_dict().values()):
+            save_only(self.conf, self.save_names, problem=problem)
+
+        if options.solve_not:
+            return None, None, None
+
+        if hasattr(self.conf.options, 'ts'):
+            time_solver = problem.get_time_solver()
+            if time_solver.name == 'ts.simple': # Implicit time stepping.
+                time_solver.set_step_fun(make_implicit_step,
+                                         (problem, nls_status))
+
+            else: # Explicit time stepping.
+                pass
+
+            state = solve_evolutionary(problem, time_solver)
+
+        else: # Stationary problem.
+            state = solve_stationary(problem,
+                                     post_process_hook=self.post_process_hook,
+                                     nls_status=nls_status)
+
+        if self.post_process_hook_final is not None: # User postprocessing.
+            self.post_process_hook_final(problem, state)
+
+        return problem, state
 
     def save_dict(self, filename, data):
         """
