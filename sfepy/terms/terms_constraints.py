dec7011af3f5d8e2624ee52a0d036e6d92868a05
Robert Cimrman <cimrman3@ntc.zcu.cz>
2012-04-26 09:29:00 +0200

    add optional material argument to NonPenetrationTerm

--- sfepy/terms/terms_constraints.py
+++ sfepy/terms/terms_constraints.py
@@ -10,25 +10,29 @@ class NonPenetrationTerm(Term):
     :Definition:
 
     .. math::
+        \int_{\Gamma} c \lambda \ul{n} \cdot \ul{v} \mbox{ , }
+        \int_{\Gamma} c \hat\lambda \ul{n} \cdot \ul{u} \\
         \int_{\Gamma} \lambda \ul{n} \cdot \ul{v} \mbox{ , }
         \int_{\Gamma} \hat\lambda \ul{n} \cdot \ul{u}
 
     :Arguments 1:
+        - material : :math:`c` (optional)
         - virtual  : :math:`\ul{v}`
         - state    : :math:`\lambda`
 
     :Arguments 2:
+        - material : :math:`c` (optional)
         - state    : :math:`\ul{u}`
         - virtual  : :math:`\hat\lambda`
     """
     name = 'dw_non_penetration'
-    arg_types = (('virtual', 'state'),
-                 ('state', 'virtual'))
+    arg_types = (('opt_material', 'virtual', 'state'),
+                 ('opt_material', 'state', 'virtual'))
     modes = ('grad', 'div')
     integration = 'surface'
 
     @staticmethod
-    def function(out, val_qp, ebf, bf, sg, diff_var, mode):
+    def function(out, val_qp, ebf, bf, mat, sg, diff_var, mode):
         """
         `ebf` belongs to vector variable, `bf` to scalar variable.
         """
@@ -40,14 +44,14 @@ class NonPenetrationTerm(Term):
                 ebf_t = nm.tile(ebf.transpose((0, 2, 1)), (n_fa, 1, 1, 1))
 
                 nl = normals * val_qp
-                eftnl = dot_sequences(ebf_t, nl)
+                eftnl = mat * dot_sequences(ebf_t, nl)
                 status = sg.integrate(out, eftnl, 0)
 
             else:
                 bf_t = nm.tile(bf.transpose((0, 2, 1)), (n_fa, 1, 1, 1))
 
                 ntu = (normals * val_qp).sum(axis=-2)[...,None]
-                ftntu = (bf_t * ntu)
+                ftntu = mat * (bf_t * ntu)
 
                 status = sg.integrate(out, ftntu, 0)
 
@@ -55,7 +59,7 @@ class NonPenetrationTerm(Term):
             ebf_t = nm.tile(ebf.transpose((0, 2, 1)), (n_fa, 1, 1, 1))
             bf_ = nm.tile(bf, (n_fa, 1, 1, 1))
 
-            eftn = dot_sequences(ebf_t, normals)
+            eftn = mat * dot_sequences(ebf_t, normals)
             eftnf = eftn * bf_
 
             if mode == 'grad':
@@ -67,7 +71,7 @@ class NonPenetrationTerm(Term):
 
         return status
 
-    def get_fargs(self, vvar, svar,
+    def get_fargs(self, mat, vvar, svar,
                   mode=None, term_mode=None, diff_var=None, **kwargs):
         if self.mode == 'grad':
             qp_var = svar
@@ -81,10 +85,13 @@ class NonPenetrationTerm(Term):
         ssg, _ = self.get_mapping(svar)
         n_fa, n_qp, dim, n_fn, n_c = self.get_data_shape(vvar)
 
+        if mat is None:
+            mat = nm.ones((1, n_qp, 1, 1), dtype=nm.float64)
+
         # Expand base corresponding to \ul{u} for all dofs.
         bf = vsg.bf
         ebf = nm.zeros((bf.shape[0], dim, n_fn * dim), dtype=nm.float64)
         for ir in xrange(dim):
             ebf[:, ir, ir*n_fn:(ir+1)*n_fn] = bf[:, 0, :]
 
-        return val_qp, ebf, ssg.bf, vsg, diff_var, self.mode
+        return val_qp, ebf, ssg.bf, mat, vsg, diff_var, self.mode
