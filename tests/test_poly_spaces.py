c0a5077592785ace93cd15fead51e2450feca64c
Robert Cimrman <cimrman3@ntc.zcu.cz>
2012-08-01 18:00:29 +0200

    hierarchic: move code from _gen_common_data() to new _gen_common_data()

--- tests/test_poly_spaces.py
+++ tests/test_poly_spaces.py
@@ -19,6 +19,77 @@ rots = {
     '3_8' : None,
 }
 
+
+def _gen_common_data(order, gels, report):
+    import sfepy
+    from sfepy.base.base import Struct
+    from sfepy.linalg import combine
+    from sfepy.fem import Mesh, Domain, Field, FieldVariable, Integral
+    from sfepy.fem.global_interp import get_ref_coors
+
+    integral = Integral('i', order=order)
+
+    for geom, poly_space_base in combine([['2_4', '3_8'],
+                                          ['lagrange', 'lobatto']]):
+        report('gemoetry: %s, base: %s' % (geom, poly_space_base))
+
+        mesh0 = Mesh.from_file('meshes/elements/%s_2.mesh' % geom,
+                               prefix_dir=sfepy.data_dir)
+        gel = gels[geom]
+
+        perms = gel.get_conn_permutations()
+
+        qps, qp_weights = integral.get_qp(gel.surface_facet.name)
+        zz = nm.zeros_like(qps[:, :1])
+        qps = nm.hstack(([qps] + [zz]))
+
+        rot = rots[geom]
+        if rot is not None:
+            pass
+
+        shift = shifts[geom]
+        rcoors = nm.ascontiguousarray(qps
+                                      + shift[:1, :] - shift[1:, :])
+        ccoors = nm.ascontiguousarray(qps
+                                      + shift[:1, :] + shift[1:, :])
+
+        for ir, pr in enumerate(perms):
+            for ic, pc in enumerate(perms):
+                report('ir: %d, ic: %d' % (ir, ic))
+
+                mesh = mesh0.copy()
+                conn = mesh.conns[0]
+                conn[0, :] = conn[0, pr]
+                conn[1, :] = conn[1, pc]
+
+                cache = Struct(mesh=mesh)
+
+                domain = Domain('domain', mesh)
+                omega = domain.create_region('Omega', 'all')
+                region = domain.create_region('Facet', rsels[geom])
+                field = Field.from_args('f', nm.float64, shape=1,
+                                        region=omega, approx_order=order,
+                                        poly_space_base=poly_space_base)
+                var = FieldVariable('u', 'unknown', field, 1)
+                report('# dofs: %d' % var.n_dof)
+
+                vec = nm.empty(var.n_dof, dtype=var.dtype)
+
+                ap = field.aps[0]
+                ps = ap.interp.poly_spaces['v']
+
+                dofs = field.get_dofs_in_region_group(region, 0,
+                                                      merge=False)
+                edofs, fdofs = nm.unique(dofs[1]), nm.unique(dofs[2])
+
+                rrc, rcells, rstatus = get_ref_coors(field, rcoors,
+                                                     cache=cache)
+                crc, ccells, cstatus = get_ref_coors(field, ccoors,
+                                                     cache=cache)
+
+                yield (geom, poly_space_base, qp_weights, mesh, ir, ic,
+                       ap, ps, rrc, crc, vec, edofs, fdofs)
+
 class Test(TestCommon):
 
     @staticmethod
@@ -34,98 +105,38 @@ class Test(TestCommon):
         return Test(conf=conf, options=options, gels=gels)
 
     def test_continuity(self):
-        import sfepy
-        from sfepy.base.base import Struct
-        from sfepy.linalg import combine
-        from sfepy.fem import Mesh, Domain, Field, FieldVariable, Integral
-        from sfepy.fem.global_interp import get_ref_coors
-
         ok = True
         order = 5
 
-        integral = Integral('i', order=order)
-
         bads = []
-        for geom, poly_space_base in combine([['2_4', '3_8'],
-                                              ['lagrange', 'lobatto']]):
-            self.report('gemoetry: %s, base: %s' % (geom, poly_space_base))
 
-            mesh0 = Mesh.from_file('meshes/elements/%s_2.mesh' % geom,
-                                   prefix_dir=sfepy.data_dir)
-            gel = self.gels[geom]
+        for (geom, poly_space_base, qp_weights, mesh, ir, ic,
+             ap, ps, rrc, crc, vec,
+             edofs, fdofs) in _gen_common_data(order, self.gels, self.report):
 
-            perms = gel.get_conn_permutations()
+            if poly_space_base == 'lagrange':
+                rbf = ps.eval_base(rrc)
+                cbf = ps.eval_base(crc)
 
-            qps, _ = integral.get_qp(gel.surface_facet.name)
-            zz = nm.zeros_like(qps[:, :1])
-            qps = nm.hstack(([qps] + [zz]))
+            else:
+                rbf = ps.eval_base(rrc, ori=ap.ori[:1])
+                cbf = ps.eval_base(crc, ori=ap.ori[1:])
 
-            rot = rots[geom]
-            if rot is not None:
-                pass
+            for ip in nm.r_[edofs, fdofs]:
+                vec.fill(0.0)
+                vec[ip] = 1.0
 
-            shift = shifts[geom]
-            rcoors = nm.ascontiguousarray(qps
-                                          + shift[:1, :] - shift[1:, :])
-            ccoors = nm.ascontiguousarray(qps
-                                          + shift[:1, :] + shift[1:, :])
+                evec = vec[ap.econn]
 
-            for ir, pr in enumerate(perms):
-                for ic, pc in enumerate(perms):
-                    self.report('ir: %d, ic: %d' % (ir, ic))
+                rvals = nm.dot(rbf, evec[0])
+                cvals = nm.dot(cbf, evec[1])
 
-                    mesh = mesh0.copy()
-                    conn = mesh.conns[0]
-                    conn[0, :] = conn[0, pr]
-                    conn[1, :] = conn[1, pc]
+                _ok = nm.allclose(rvals, cvals, atol=1e-14, rtol=0.0)
+                self.report('dof %d: %s' % (ip, _ok))
+                if not _ok:
+                    bads.append([geom, poly_space_base, ir, ic, ip])
 
-                    cache = Struct(mesh=mesh)
-
-                    domain = Domain('domain', mesh)
-                    omega = domain.create_region('Omega', 'all')
-                    region = domain.create_region('Facet', rsels[geom])
-                    field = Field.from_args('f', nm.float64, shape=1,
-                                            region=omega, approx_order=order,
-                                            poly_space_base=poly_space_base)
-                    var = FieldVariable('u', 'unknown', field, 1)
-                    self.report('# dofs: %d' % var.n_dof)
-
-                    vec = nm.empty(var.n_dof, dtype=var.dtype)
-                    ap = field.aps[0]
-                    ps = ap.interp.poly_spaces['v']
-
-                    dofs = field.get_dofs_in_region_group(region, 0,
-                                                          merge=False)
-                    edofs, fdofs = nm.unique(dofs[1]), nm.unique(dofs[2])
-
-                    rrc, rcells, rstatus = get_ref_coors(field, rcoors,
-                                                         cache=cache)
-                    crc, ccells, cstatus = get_ref_coors(field, ccoors,
-                                                         cache=cache)
-
-                    if poly_space_base == 'lagrange':
-                        rbf = ps.eval_base(rrc)
-                        cbf = ps.eval_base(crc)
-
-                    else:
-                        rbf = ps.eval_base(rrc, ori=ap.ori[:1])
-                        cbf = ps.eval_base(crc, ori=ap.ori[1:])
-
-                    for ip in nm.r_[edofs, fdofs]:
-                        vec.fill(0.0)
-                        vec[ip] = 1.0
-
-                        evec = vec[ap.econn]
-
-                        rvals = nm.dot(rbf, evec[0])
-                        cvals = nm.dot(cbf, evec[1])
-
-                        _ok = nm.allclose(rvals, cvals, atol=1e-14, rtol=0.0)
-                        self.report('dof %d: %s' % (ip, _ok))
-                        if not _ok:
-                            bads.append([geom, poly_space_base, ir, ic, ip])
-
-                        ok = ok and _ok
+                ok = ok and _ok
 
         if not ok:
             self.report('continuity errors:\n', bads)
