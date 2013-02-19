8a512e53ac42ddc178326a135b6fe9a687fca77b
Robert Cimrman <cimrman3@ntc.zcu.cz>
2013-02-19 18:14:51 +0100

    allow dot in set names in create_bnf()

--- sfepy/fem/parseReg.py
+++ sfepy/fem/parseReg.py
@@ -115,7 +115,8 @@ def create_bnf(stack):
     _set = Literal('set')
     surface = Literal('surface')
 
-    ident = Word(alphas + '_', alphanums + "_")
+    ident = Word(alphas + '_.', alphanums + '_.')
+    set_name = Word(nums) | ident
 
     function = Word(alphas + '_', alphanums + '_')
     function = Group(function).setParseAction(join_tokens)
@@ -156,9 +157,9 @@ def create_bnf(stack):
               + inumber + rpar.suppress())
     ei2 = Group(element + delimitedList(etuple)).setParseAction(
         replace('E_EI2', keep=True))
-    noset = Group(nodes + _of + _set + (Word(nums) | ident)).setParseAction(
+    noset = Group(nodes + _of + _set + set_name).setParseAction(
         replace('E_NOSET', keep=True))
-    eoset = Group(elements + _of + _set + (Word(nums) | ident)).setParseAction(
+    eoset = Group(elements + _of + _set + set_name).setParseAction(
         replace('E_EOSET', keep=True))
 
     region_expression = Forward()
