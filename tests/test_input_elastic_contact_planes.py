cc5c2f5fccc0081b96cde65d85839278bdc8d574
Robert Cimrman <cimrman3@ntc.zcu.cz>
2013-04-22 13:33:19 +0200

    new tests/test_input_elastic_contact_planes.py

---
+++ tests/test_input_elastic_contact_planes.py
@@ -0,0 +1,6 @@
+input_name = '../examples/linear_elasticity/elastic_contact_planes.py'
+output_name = 'test_elastic_contact_planes.vtk'
+
+from testsBasic import TestInput
+class Test(TestInput):
+    pass
