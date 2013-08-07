844a6d1b0d6b00ee2379ffd3af231c1566aaf211
Robert Cimrman <cimrman3@ntc.zcu.cz>
2013-08-07 10:55:45 +0200

    new get_facet_dof_permutations(), permute_facets(), get_dof_orientation_maps()
    - updated and recombined code from removed Facets

--- sfepy/fem/facets.py
+++ sfepy/fem/facets.py
@@ -3,7 +3,9 @@ Helper functions related to mesh facets.
 """
 import numpy as nm
 
-from sfepy.linalg import permutations
+from sfepy.base.base import dict_to_array, assert_
+from sfepy.base.compat import in1d
+from sfepy.linalg import permutations, map_permutations
 
 _quad_ori_groups = {
     0 : 0,
@@ -72,3 +74,73 @@ def build_orientation_map(n_fp):
         ori_map[key] = [indx, isort]
 
     return ori_map, cmps, powers
+
+def get_dof_orientation_maps(n_fp, raw_ori_maps, nodes):
+    """
+    Given description of facet DOF nodes, return the corresponding
+    integer coordinates and orientation maps.
+
+    Notes
+    -----
+    Assumes single facet type in all groups.
+    """
+    if n_fp <= 3: # Simplex facet.
+        ori_maps = raw_ori_maps
+
+    else: # Tensor product facet.
+        ori_maps = {}
+        for ig, ori_map in raw_ori_maps.iteritems():
+            ori_maps[ig] = {}
+            for key in ori_map.iterkeys():
+                new_key = _quad_ori_groups[key]
+                ori_maps[ig][key] = [None, _quad_orientations[new_key]]
+
+    return ori_maps
+
+def permute_facets(facets, ori, ori_map):
+    """
+    Return a copy of `facets` array with vertices sorted lexicographically.
+    """
+    assert_((in1d(nm.unique(ori), ori_map.keys())).all())
+
+    permuted_facets = facets.copy()
+
+    for key, ori_map in ori_map.iteritems():
+        perm = ori_map[1]
+        ip = nm.where(ori == key)[0]
+        for ic0, ic1 in enumerate(perm):
+            permuted_facets[ip,ic0] = facets[ip,ic1]
+
+    return permuted_facets
+
+def get_facet_dof_permutations(n_fp, igs, nodes):
+    """
+    Prepare DOF permutation vector for each possible facet orientation.
+    """
+    ori_map, _, _ = build_orientation_map(n_fp)
+    raw_ori_maps = {}
+    for ig in igs:
+        raw_ori_maps[ig] = ori_map
+    ori_maps = get_dof_orientation_maps(n_fp, raw_ori_maps, nodes)
+
+    inod = nm.arange(n_fp, dtype=nm.int32)
+    int_coors = nodes[0][:, inod]
+
+    ori = nm.empty((int_coors.shape[0],), dtype=nm.int32)
+    dof_perms = {}
+    for ig, ori_map in ori_maps.iteritems():
+        dof_perms[ig] = {}
+        for key in ori_map.iterkeys():
+            ori.fill(key)
+            permuted_int_coors = permute_facets(int_coors, ori, ori_map)
+
+            perm = map_permutations(permuted_int_coors, int_coors,
+                                    check_same_items=True)
+
+            dof_perms[ig][key] = perm
+
+    adof_perms = {}
+    for ig, val in dof_perms.iteritems():
+        adof_perms[ig] = dict_to_array(val)
+
+    return adof_perms
