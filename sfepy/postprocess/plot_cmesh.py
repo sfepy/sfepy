f8b088f8540ca518db3b235fd3cc93800fcb880b
Robert Cimrman <cimrman3@ntc.zcu.cz>
2013-08-29 11:54:45 +0200

    new sfepy/postprocess/plot_cmesh.py: new plot_wireframe()

---
+++ sfepy/postprocess/plot_cmesh.py
@@ -0,0 +1,30 @@
+"""
+Functions to visualize the CMesh geometry and topology.
+"""
+import matplotlib.pyplot as plt
+
+from sfepy.postprocess.plot_dofs import _get_axes
+
+def plot_wireframe(ax, cmesh, show=False):
+    """
+    Plot a finite element mesh as a wireframe using edges connectivity.
+    """
+    coors = cmesh.coors
+    dim = cmesh.dim
+
+    edges = cmesh.get_conn(1, 0)
+
+    ax = _get_axes(ax, dim)
+
+    for edge_vertices in edges.indices.reshape((edges.num, 2)):
+        cc = coors[edge_vertices]
+        if dim == 3:
+            ax.plot(cc[:, 0], cc[:, 1], cc[:, 2], 'k')
+
+        else:
+            ax.plot(cc[:, 0], cc[:, 1], 'k')
+
+    if show:
+        plt.show()
+
+    return ax
