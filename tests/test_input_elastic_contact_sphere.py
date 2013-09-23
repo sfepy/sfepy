94487ca24519f7d82123cf74e67795e1e0301708
Robert Cimrman <cimrman3@ntc.zcu.cz>
2013-09-23 19:20:08 +0200

    new examples/linear_elasticity/elastic_contact_sphere.py + test

---
+++ tests/test_input_elastic_contact_sphere.py
@@ -0,0 +1,6 @@
+input_name = '../examples/linear_elasticity/elastic_contact_sphere.py'
+output_name = 'test_elastic_contact_sphere.vtk'
+
+from testsBasic import TestInput
+class Test(TestInput):
+    pass
