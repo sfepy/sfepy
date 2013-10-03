57aceb608fa8d252fc7add59380aa7e5ff34000b
Robert Cimrman <cimrman3@ntc.zcu.cz>
2013-10-03 10:41:59 +0200

    script/plot_mesh.py: fix help message

--- script/plot_mesh.py
+++ script/plot_mesh.py
@@ -8,7 +8,7 @@ from sfepy.base.base import output
 from sfepy.fem import Mesh, Domain
 import sfepy.postprocess.plot_cmesh as pc
 
-usage = '%prog [options] output_dir\n' + __doc__.rstrip()
+usage = '%prog [options] filename\n' + __doc__.rstrip()
 
 helps = {
 }
