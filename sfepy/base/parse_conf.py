fe1edda56a32b9f1f84a0fec7d90f89551e4d1d1
Matyas Novak <novakmat@fzu.cz>
2014-01-09 16:12:11 +0100

    update parsing lexical elements of problem configuration
    - new list_dict()

--- sfepy/base/parse_conf.py
+++ sfepy/base/parse_conf.py
@@ -10,30 +10,33 @@ word_free = Word(alphas8bit + '_-/.+**' + alphanums)
 word_strict = Word(alphas8bit + alphas, alphas8bit + alphanums + '_' )
 
 (lparen, rparen, lbrack, rbrack,
-lbrace, rbrace, colon) = map(Suppress, '()[]{}:')
+ lbrace, rbrace, colon, equal_sign) = map(Suppress, '()[]{}:=')
 
 integer = Combine(Optional(oneOf('+ -')) + Word(nums)).setName('integer')
 cvt_int = lambda toks: int(toks[0])
 integer.setParseAction(cvt_int)
 
-boolean = Keyword('False', caseless=True)
-cvt_bool = lambda toks: toks[0].lower == 'true'
-boolean.setParseAction(cvt_bool)
+boolean_true = Keyword('True', caseless=True)
+boolean_true.setParseAction(lambda x: True)
+boolean_false = Keyword('False', caseless=True)
+boolean_false.setParseAction(lambda x: False)
+
+boolean = boolean_true | boolean_false
 
 none = Keyword('None', caseless=True)
 
-cvt_none =  lambda toks: [None]
+cvt_none = lambda toks: [None]
 none.setParseAction(cvt_none)
 
-real = Combine(Optional(oneOf('+ -'))+ Word(nums)
+real = Combine(Optional(oneOf('+ -')) + Word(nums)
                + '.' + Optional(Word(nums))
                + Optional('e' + Optional(oneOf('+ -'))
                           + Word(nums))).setName('real')
 cvt_real = lambda toks: float(toks[0])
 real.setParseAction(cvt_real)
 
-array_index = Word(nums) + Optional(colon + integer
-				+ Optional(colon + integer))
+array_index = integer + Optional(colon + integer
+                                 + Optional(colon + integer))
 cvt_array_index = lambda toks: int(toks[0]) if len(toks) == 1 \
                   else slice(*toks)
 array_index.setParseAction(cvt_array_index)
@@ -49,12 +52,38 @@ def create_bnf(allow_tuple=False, free_word=False):
         return defs['dict'].inner
 
 def list_of(element, *elements):
+    """
+    Return lexical element that parses a list of items. The items can be a one
+    or several lexical elements. For example, result of ``list_of(real,
+    integer)`` parses list of real or integer numbers.
+    """
     for e in elements:
         element |= e
     lst = delimitedList(element)
     return lst + Optional(Suppress(','))
 
-def get_standard_type_defs(word):
+def get_standard_type_defs(word=word_free):
+    """
+    Return dict of the pyparsing base lexical elements.
+
+    The compound types (tuple, list, dict) can contain compound types or simple
+    types such as integers, floats and words.
+
+    Parameters
+    ----------
+    word : lexical element
+        A custom lexical element for word.
+
+    Returns
+    -------
+    defs : dict
+        The dictionary with the following items:
+
+        - tuple: (..., ..., ...)
+        - list: [..., ...., ...]
+        - dict: {...:..., ...:..., ....} or {...=..., ...=..., ....}
+        - list_item: any of preceding compound types or simple types
+    """
     tuple_str = Forward()
     list_str = Forward()
     dict_str = Forward()
@@ -64,7 +93,7 @@ def get_standard_type_defs(word):
     list_item = (none | boolean | real | integer | list_str | tuple_str
                  | dict_str
                  | quotedString.setParseAction(removeQuotes)
-                 | word )
+                 | word)
     list_item2 = list_item | Empty().setParseAction(lambda: [None])
 
     tuple_str.inner = list_of(list_item)
@@ -75,12 +104,45 @@ def get_standard_type_defs(word):
     list_str.inner.setParseAction(lambda toks: list(toks))
     list_str << (lbrack + list_str.inner + rbrack)
 
-    dict_entry = Group(list_item + colon + list_item2)
+    dict_entry = Group(list_item + (colon | equal_sign) + list_item2)
     dict_str.inner = list_of(dict_entry)
     dict_str.inner.setParseAction(cvt_dict)
     dict_str << (lbrace + Optional(dict_str.inner) + rbrace)
 
-    return {'tuple' : tuple_str,
+    defs = {'tuple' : tuple_str,
             'list' : list_str,
             'dict' : dict_str,
             'list_item' : list_item}
+
+    return defs
+
+def list_dict(word=word_free):
+    """
+    Return the pyparsing lexical element, that parses a string either as a list
+    or as a dictionary.
+
+    Parameters
+    ----------
+    word : lexical element
+        A custom lexical element for word.
+
+    Returns
+    -------
+    ld : lexical element
+        The returned lexical element parses a string in the form
+        ``..., ..., ...`` or ``key1:..., key2=..., key3: ...``
+        where ``...`` is a ``list_item`` from :func:`get_standard_type_defs()`
+        and interprets it as a list or a dictionary.
+    """
+    defs = get_standard_type_defs(word)
+    i = defs['list_item']
+    arg = i.copy()
+    arg.setParseAction(lambda t: (t[0],))
+    narg = word_strict + (colon | equal_sign) + i
+    narg.setParseAction(lambda t: (t[0], t[1]))
+
+    ld = Group(list_of(narg | arg))
+    ld.setParseAction(lambda t: ([x[0] for x in t[0] if len(x) == 1],
+                                 dict([x for x in t[0] if len(x) > 1]))
+                      )
+    return ld
