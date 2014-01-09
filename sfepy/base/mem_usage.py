498c4e1092e7321dc51e30cc622cc0ae3c004c94
Robert Cimrman <cimrman3@ntc.zcu.cz>
2014-01-09 19:00:19 +0100

    new sfepy/base/mem_usage.py - get_mem_usage(), print_mem_usage()

---
+++ sfepy/base/mem_usage.py
@@ -0,0 +1,142 @@
+"""
+Memory usage functions.
+"""
+import sys
+import collections
+
+import numpy as nm
+import scipy.sparse as sp
+
+from sfepy.base.base import basestr, Struct, Output
+
+def get_mem_usage(obj, usage=None, name=None, traversal_order=None, level=0):
+    """
+    Get lower bound of memory usage of an object.
+
+    Takes into account strings, numpy arrays and scipy CSR sparse matrices,
+    descends into sequences, mappings and objects.
+
+    Parameters
+    ----------
+    obj : any object
+        The object to be measured.
+    usage : dict
+        The dict with memory usage records, serving also as a cache of already
+        traversed objects.
+    name : str
+        The name to be given to the object in its record.
+    traversal_order : list, internal
+        The traversal order of the object.
+    level : int, internal
+        The recurrence level.
+
+    Returns
+    -------
+    usage : int
+        The object's lower bound of memory usage.
+    """
+    if usage is None:
+        usage = {}
+
+    if name is None:
+        name = getattr(obj, 'name', '-')
+
+    if traversal_order is None:
+        traversal_order = [0]
+
+    to = traversal_order
+
+    key = id(obj)
+    if key in usage:
+        usage[key].nrefs += 1
+        return 0
+
+    else:
+        record = usage.setdefault(key, Struct(name=name,
+                                              kind=type(obj).__name__,
+                                              usage=0, nrefs=1,
+                                              traversal_order=to[0],
+                                              level=level))
+        level += 1
+
+    if isinstance(obj, nm.ndarray):
+        record.usage = obj.nbytes
+
+    elif isinstance(obj, sp.csr_matrix):
+        record.usage = (get_mem_usage(obj.data, usage, name='data',
+                                      traversal_order=to, level=level)
+                        + get_mem_usage(obj.indices, usage, name='indices',
+                                        traversal_order=to, level=level)
+                        + get_mem_usage(obj.indptr, usage, name='indptr',
+                                        traversal_order=to, level=level))
+
+    elif isinstance(obj, basestr):
+        record.usage = len(obj)
+
+    elif isinstance(obj, Struct):
+        for subname, sub in obj.__dict__.iteritems():
+            to[0] += 1
+            record.usage += get_mem_usage(sub, usage,
+                                          name='attribute %s of %s'
+                                          % (subname, getattr(obj, 'name',
+                                                              record.kind)),
+                                          traversal_order=to, level=level)
+
+    elif isinstance(obj, collections.Mapping):
+        for subname, sub in obj.iteritems():
+            to[0] += 1
+            record.usage += get_mem_usage(sub, usage,
+                                          name='item %s of %s'
+                                          % (subname, record.kind),
+                                          traversal_order=to, level=level)
+
+    elif isinstance(obj, collections.Sequence):
+        for ii, sub in enumerate(obj):
+            to[0] += 1
+            record.usage += get_mem_usage(sub, usage,
+                                          name='item %d of %s'
+                                          % (ii, record.kind),
+                                          traversal_order=to, level=level)
+
+    else:
+        record.usage = sys.getsizeof(obj)
+
+    return record.usage
+
+def print_mem_usage(usage, order_by='usage', direction='up', print_key=False):
+    """
+    Print memory usage dictionary.
+
+    Parameters
+    ----------
+    usage : dict
+        The dict with memory usage records.
+    order_by : 'usage', 'name', 'kind', 'nrefs', 'traversal_order', or 'level'
+        The sorting field name.
+    direction : 'up' or 'down'
+        The sorting direction.
+    print_key : bool
+        If True, print also the record key (object's id).
+    """
+    keys = usage.keys()
+    order_vals = nm.array([record.get(order_by)
+                           for record in usage.itervalues()])
+
+    order = nm.argsort(order_vals)
+    if direction == 'down':
+        order = order[::-1]
+
+    output = Output('')
+    fmt = '%9s, %s, %s, %d %d %d' + ', %d' * print_key
+
+    for ii in order:
+        key = keys[ii]
+        record = usage[key]
+
+        if print_key:
+            output(fmt % (record.usage, record.name, record.kind, record.nrefs,
+                          record.traversal_order, record.level, key))
+
+        else:
+            output(fmt % (record.usage, record.name, record.kind, record.nrefs,
+                          record.traversal_order, record.level))
