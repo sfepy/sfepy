8cd5517496949dec51eb4554b4121ac9fbae7a8b
Robert Cimrman <cimrman3@ntc.zcu.cz>
2014-02-24 14:05:55 +0100

    new sfepy/discrete/common/setup.py, update sfepy/discrete/setup.py

--- sfepy/discrete/setup.py
+++ sfepy/discrete/setup.py
@@ -9,6 +9,7 @@ def configuration(parent_package='',top_path=None):
     config = Configuration('sfepy', parent_package, top_path)
 
     subdirs = [
+        'common',
         'fem',
     ]
     for subdir in subdirs:
