630848a00be01969e46f1121897289ee6951f67e
Robert Cimrman <cimrman3@ntc.zcu.cz>
2014-03-16 13:25:45 +0100

    clean up tests/test_volume.py

--- tests/test_volume.py
+++ tests/test_volume.py
@@ -1,4 +1,6 @@
-# 28.05.2009, c
+"""
+Test computing volumes by volume or surface integrals.
+"""
 from sfepy import data_dir
 
 filename_mesh = data_dir + '/meshes/3d/elbow.mesh'
@@ -18,31 +20,26 @@ regions = {
 }
 
 expressions = {
-    'volume_p' : 'd_volume.i.Omega( p )',
-    'volume_u' : 'd_volume.i.Omega( u )',
-    'surface_p' : 'd_volume_surface.i.Gamma( p )',
-    'surface_u' : 'd_volume_surface.i.Gamma( u )',
+    'volume_p' : 'd_volume.i.Omega(p)',
+    'volume_u' : 'd_volume.i.Omega(u)',
+    'surface_p' : 'd_volume_surface.i.Gamma(p)',
+    'surface_u' : 'd_volume_surface.i.Gamma(u)',
 }
 
 import numpy as nm
 from sfepy.base.testing import TestCommon
-from sfepy.base.base import debug, pause
 
-##
-# 10.07.2007, c
-class Test( TestCommon ):
-    tests = ['test_volume']
+class Test(TestCommon):
 
-    def from_conf( conf, options ):
+    @staticmethod
+    def from_conf(conf, options):
         from sfepy.discrete import Problem
 
         problem = Problem.from_conf(conf, init_equations=False)
-        test = Test( problem = problem,
-                     conf = conf, options = options )
+        test = Test(problem=problem, conf=conf, options=options)
         return test
-    from_conf = staticmethod( from_conf )
 
-    def test_volume( self ):
+    def test_volume(self):
         from sfepy.discrete import FieldVariable
 
         ok = True
@@ -65,11 +62,11 @@ class Test( TestCommon ):
         avg /= len(volumes)
 
         for key, val in volumes.items():
-            err = nm.abs( avg - val ) / nm.abs( avg )
+            err = nm.abs(avg - val) / nm.abs(avg)
             _ok = err < 1e-12
             self.report('"'"%s"'" - volume: %e' % (key, val))
-            self.report('"'"%s"'" - relative volume difference: %e -> %s'\
-                         % (key, err, _ok) )
+            self.report('"'"%s"'" - relative volume difference: %e -> %s'
+                        % (key, err, _ok))
             ok = ok and _ok
 
         return ok
