2c6ca8aec74957390d8051e00fbbecf453ed509f
Robert Cimrman <cimrman3@ntc.zcu.cz>
2014-08-20 13:28:33 +0200

    remove examples/diffusion/octahedron.py + test
    - no added value w.r.t. other diffusion examples

--- tests/test_input_octahedron.py
+++
@@ -1,7 +0,0 @@
-# c: 15.02.2008, r: 15.02.2008
-input_name = '../examples/diffusion/octahedron.py'
-output_name = 'test_octahedron.vtk'
-
-from tests_basic import TestInput
-class Test( TestInput ):
-    pass
