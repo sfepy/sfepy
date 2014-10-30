sfepy/sfepy

commit ec186b8c69b0719f483e11a864092a21a608a203

update scripts for new paths

script/gen_lobatto1d_c.py
@@ -203,7 +203,7 @@ def main():
 
         plt.show()
 
-    indir = InDir(os.path.join(top_dir, 'sfepy/fem/extmods/'))
+    indir = InDir(os.path.join(top_dir, 'sfepy.discrete.fem.extmods/'))
 
     fd = open(indir('lobatto1d_template.h'), 'r')
     template = fd.read()

script/blockgen.py
@@ -10,7 +10,7 @@ import numpy as nm
 
 from sfepy.base.base import output
 from sfepy.mesh.mesh_generators import gen_block_mesh
-from sfepy.fem.meshio import MeshIO
+from sfepy.discrete.fem.meshio import MeshIO
 
 usage = '%prog [options]\n' + __doc__.rstrip()
 

script/convert_mesh.py
@@ -14,8 +14,8 @@ sys.path.append('.')
 
 from optparse import OptionParser
 from sfepy.base.base import nm, output
-from sfepy.fem import Mesh, Domain
-from sfepy.fem.meshio import (output_writable_meshes, MeshIO,
+from sfepy.discrete.fem import Mesh, Domain
+from sfepy.discrete.fem.meshio import (output_writable_meshes, MeshIO,
                               supported_cell_types)
 
 usage = '%prog [options] filename_in filename_out\n' + __doc__.rstrip()

script/cylindergen.py
@@ -3,7 +3,7 @@ import sys
 sys.path.append( '.' )
 from optparse import OptionParser
 from sfepy.mesh.mesh_generators import gen_cylinder_mesh
-from sfepy.fem.meshio import MeshIO
+from sfepy.discrete.fem.meshio import MeshIO
 
 usage = """%prog [options]
 

script/gen_mesh_prev.py
@@ -13,7 +13,7 @@ import sys
 sys.path.append('.')
 import os
 import vtk
-from sfepy.fem import Mesh
+from sfepy.discrete.fem import Mesh
 
 def gen_shot(vtk_filename, png_filename):
     """

script/gen_term_table.py
@@ -7,8 +7,8 @@ import pyparsing as pp
 import numpy as nm
 
 sys.path.append( '.' )
-import sfepy.fem # Hack: fix circular dependency, as terms.pyx imports
-                 # from sfepy.fem
+import sfepy.discrete.fem # Hack: fix circular dependency, as terms.pyx imports
+                          # from sfepy.discrete.fem
 from sfepy.terms import term_table
 
 def set_section(sec):

script/plot_condition_numbers.py
@@ -10,7 +10,8 @@ import matplotlib.pyplot as plt
 
 from sfepy import data_dir
 from sfepy.base.base import output, assert_
-from sfepy.fem import Mesh, Domain, Field, FieldVariable, Material, Integral
+from sfepy.discrete import FieldVariable, Material, Integral
+from sfepy.discrete.fem import Mesh, Domain, Field
 from sfepy.terms import Term
 from sfepy.solvers import eig
 

script/plot_mesh.py
@@ -5,7 +5,7 @@ Plot mesh connectivities, facet orientations, global and local DOF ids etc.
 from optparse import OptionParser
 
 from sfepy.base.base import output
-from sfepy.fem import Mesh, Domain
+from sfepy.discrete.fem import Mesh, Domain
 import sfepy.postprocess.plot_cmesh as pc
 
 usage = '%prog [options] filename\n' + __doc__.rstrip()

commit 83864e8d71b67810e4db2c44aa494778cc91fea4

fix path in script/gen_lobatto1d_c.py
- bug introduced in ec186b8c69b0719f483e11a864092a21a608a203

script/gen_lobatto1d_c.py
@@ -203,7 +203,7 @@ def main():
 
         plt.show()
 
-    indir = InDir(os.path.join(top_dir, 'sfepy.discrete.fem.extmods/'))
+    indir = InDir(os.path.join(top_dir, 'sfepy/discrete/fem/extmods/'))
 
     fd = open(indir('lobatto1d_template.h'), 'r')
     template = fd.read()