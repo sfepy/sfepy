894aff20cf4514c3b2d9732ae7bf39521b8b7629
Robert Cimrman <cimrman3@ntc.zcu.cz>
2014-11-10 15:06:44 +0100

    new tests/test_input_linear_elastic_probes.py

---
+++ tests/test_input_linear_elastic_probes.py
@@ -0,0 +1,6 @@
+input_name = '../examples/linear_elasticity/linear_elastic_probes.py'
+output_name = 'test_linear_elastic_probes.vtk'
+
+from tests_basic import TestInput
+class Test(TestInput):
+    pass
