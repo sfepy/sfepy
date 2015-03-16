bdf7c1ab0ce9fc97f5371f598f83e434d3a2786d
Robert Cimrman <cimrman3@ntc.zcu.cz>
2015-03-16 14:39:30 +0100

    clean up tests/test_linear_solvers.py

--- tests/test_linear_solvers.py
+++ tests/test_linear_solvers.py
@@ -42,17 +42,18 @@ integral_1 = {
 }
 
 equations = {
-    'Temperature' : """dw_laplace.i.Omega( coef.val, s, t ) = 0"""
+    'Temperature' : """dw_laplace.i.Omega(coef.val, s, t) = 0"""
 }
 
 solvers = {
     'd00' : ('ls.scipy_direct',
-             {'warn' : True,}
+             {'method' : 'umfpack',
+              'warn' : True,}
     ),
-##     'd01' : ('ls.scipy_direct',
-##              {'method' : 'superlu',
-##               'warn' : True,}
-##     ),
+    'd01' : ('ls.scipy_direct',
+             {'method' : 'superlu',
+              'warn' : True,}
+    ),
     'i00' : ('ls.pyamg',
              {'method' : 'ruge_stuben_solver',
               'accel' : 'cg',
@@ -100,61 +101,50 @@ options = {
 from sfepy.base.testing import TestCommon
 output_name = 'test_linear_solvers_%s.vtk'
 
-##
-# c: 02.05.2008
-class Test( TestCommon ):
+class Test(TestCommon):
     can_fail = ['ls.pyamg', 'ls.petsc']
 
-    ##
-    # c: 02.05.2008, r: 02.05.2008
-    def from_conf( conf, options ):
+    @staticmethod
+    def from_conf(conf, options):
         from sfepy.discrete import Problem
 
         problem = Problem.from_conf(conf)
         problem.time_update()
 
-        test = Test( problem = problem, 
-                     conf = conf, options = options )
+        test = Test(problem=problem, conf=conf, options=options)
         return test
-    from_conf = staticmethod( from_conf )
 
-    ##
-    # c: 02.05.2008, r: 02.05.2008
-    def _list_linear_solvers( self, confs ):
+    def _list_linear_solvers(self, confs):
         d = []
         for key, val in confs.iteritems():
-            if val.kind.find( 'ls.' ) == 0:
-                d.append( val )
-        d.sort( cmp = lambda a, b: cmp( a.name, b.name ) )
+            if val.kind.find('ls.') == 0:
+                d.append(val)
+        d.sort(cmp = lambda a, b: cmp(a.name, b.name))
 
         return d
 
-    ##
-    # c: 02.05.2008, r: 07.05.2008
-    def test_solvers( self ):
+    def test_solvers(self):
         from sfepy.base.base import IndexedStruct
         import os.path as op
 
-        solver_confs = self._list_linear_solvers( self.problem.solver_confs )
+        solver_confs = self._list_linear_solvers(self.problem.solver_confs)
 
         ok = True
         tt = []
         for solver_conf in solver_confs:
             method = solver_conf.get('method', '')
             precond = solver_conf.get('precond', '')
-            name = ' '.join( (solver_conf.name, solver_conf.kind,
-                              method, precond) ).rstrip()
-            self.report( name )
-            self.report( 'matrix size:', self.problem.mtx_a.shape )
-            self.report( '        nnz:', self.problem.mtx_a.nnz )
+            name = ' '.join((solver_conf.name, solver_conf.kind,
+                             method, precond)).rstrip()
+            self.report(name)
+            self.report('matrix size:', self.problem.mtx_a.shape)
+            self.report('        nnz:', self.problem.mtx_a.nnz)
             status = IndexedStruct()
             try:
-                self.problem.init_solvers( nls_status = status,
-                                          ls_conf = solver_conf )
+                self.problem.init_solvers(nls_status=status,
+                                          ls_conf=solver_conf)
                 state = self.problem.solve()
                 failed = status.condition != 0
-##                 self.problem.mtx_a.save( 'mtx_laplace_cube',
-##                                         format='%d %d %.12e\n' )
             except Exception, exc:
                 failed = True
                 status = None
@@ -163,24 +153,23 @@ class Test( TestCommon ):
 
             if status is not None:
                 for kv in status.time_stats.iteritems():
-                    self.report( '%10s: %7.2f [s]' % kv )
-                self.report( 'condition: %d, err0: %.3e, err: %.3e'\
-                             % (status.condition, status.err0, status.err) )
-                tt.append( [name, status.time_stats['solve'], status.err] )
+                    self.report('%10s: %7.2f [s]' % kv)
+                self.report('condition: %d, err0: %.3e, err: %.3e'
+                            % (status.condition, status.err0, status.err))
+                tt.append([name, status.time_stats['solve'], status.err])
 
                 aux = name.replace(' ', '_')
-                fname = op.join( self.options.out_dir,
-                                op.split( self.conf.output_name )[1] ) % aux
-                self.problem.save_state( fname, state )
+                fname = op.join(self.options.out_dir,
+                                op.split(self.conf.output_name)[1]) % aux
+                self.problem.save_state(fname, state)
             else:
-                self.report( 'solver failed:' )
-                self.report( exc )
-                tt.append( [name, 1e10, 1e10] )
+                self.report('solver failed:')
+                self.report(exc)
+                tt.append([name, 1e10, 1e10])
 
-
-        tt.sort( cmp = lambda a, b: cmp( a[1], b[1] ) )
-        self.report( 'solution times (rezidual norms):' )
+        tt.sort(cmp = lambda a, b: cmp(a[1], b[1]))
+        self.report('solution times (rezidual norms):')
         for row in tt:
-            self.report( '%.2f [s]' % row[1], '(%.3e)' % row[2], ':', row[0] )
+            self.report('%.2f [s]' % row[1], '(%.3e)' % row[2], ':', row[0])
 
         return ok
