1e2020894df32a9e90c9fbe52efa07a69c803416
Robert Cimrman <cimrman3@ntc.zcu.cz>
2015-08-10 15:31:46 +0200

    new IGField.create_mesh(), .create_eval_mesh(), .create_basis_context()

--- sfepy/discrete/iga/fields.py
+++ sfepy/discrete/iga/fields.py
@@ -362,6 +362,34 @@ class IGField(Field):
 
         return cmap, mapping
 
+    def create_mesh(self, extra_nodes=True):
+        """
+        Create a mesh corresponding to the field region. For IGA fields, this
+        is directly the topological mesh. The `extra_nodes` argument is
+        ignored.
+        """
+        return self.domain.mesh
+
+    def create_eval_mesh(self):
+        """
+        Create a mesh with the original NURBS connectivity for evaluating the
+        field. The mesh coordinates are the NURBS control points.
+        """
+        return self.domain.eval_mesh
+
+    def create_basis_context(self):
+        """
+        Create the context required for evaluating the field basis.
+        """
+        from sfepy.discrete.iga.extmods.igac import CNURBSContext
+
+        nurbs = self.nurbs
+
+        ctx = CNURBSContext(nurbs.cps, nurbs.weights, nurbs.degrees,
+                            nurbs.cs, nurbs.conn, i_max=100)
+
+        return ctx
+
     def create_output(self, dofs, var_name, dof_names=None,
                       key=None, **kwargs):
         """
