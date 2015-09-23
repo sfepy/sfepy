66f822d49f3657d37b38c9087d7f496224e529d4
Robert Cimrman <cimrman3@ntc.zcu.cz>
2015-09-23 08:47:10 +0200

    script/show_authors.py: merge by names, show commit counts

--- script/show_authors.py
+++ script/show_authors.py
@@ -9,14 +9,31 @@ def main():
 
     done = set()
     unique = []
+    counts = {}
     for line in reversed(out):
         line = line.strip()
-        if len(line) and not line in done:
-            done.add(line)
-            unique.append(line)
+        if not len(line): continue
+
+        name, email = line.split(' <')
+        name = name.strip()
+        email = '<' + email.strip()
+
+        if not name in done:
+            done.add(name)
+            unique.append(name)
+
+        record = counts.setdefault(name, [0, email])
+        record[0] += 1
+        if not email in record[1:]:
+            record.append(email)
+
+
+    print('List of contributors ordered by date of the first commit,'
+          ' with commit counts:')
 
     for line in unique:
-        print line
+        record = counts[line]
+        print('%6d %s %s' % (record[0], line, ', '.join(record[1:])))
 
 if __name__ == '__main__':
     main()
