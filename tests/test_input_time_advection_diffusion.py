c541fa68e4d58b37e09387652c8950ac2a176fa7
Robert Cimrman <cimrman3@ntc.zcu.cz>
2015-09-23 08:06:33 +0200

    new tests/test_input_time_advection_diffusion.py

---
+++ tests/test_input_time_advection_diffusion.py
@@ -0,0 +1,7 @@
+input_name = '../examples/diffusion/time_advection_diffusion.py'
+output_name_trunk = 'test_time_advection_diffusion'
+
+from tests_basic import TestInputEvolutionary
+
+class Test(TestInputEvolutionary):
+    pass
