6ba5aa094b9905555715e7734d6411056c34cf1a
Robert Cimrman <cimrman3@ntc.zcu.cz>
2016-04-25 21:26:18 +0200

    shell10x_cantilever_interactive.py: new --transform option
    - update make_domain(), solve_problem(), get_analytical_displacement(), main()

--- examples/linear_elasticity/shell10x_cantilever_interactive.py
+++ examples/linear_elasticity/shell10x_cantilever_interactive.py
@@ -16,10 +16,12 @@ from sfepy.terms import Term
 from sfepy.discrete.conditions import Conditions, EssentialBC
 from sfepy.solvers.ls import ScipyDirect
 from sfepy.solvers.nls import Newton
+from sfepy.linalg import make_axis_rotation_matrix
+from sfepy.mechanics.tensors import transform_data
 from sfepy.mesh.mesh_generators import gen_block_mesh
 import sfepy.mechanics.shell10x as sh
 
-def make_domain(dims, shape):
+def make_domain(dims, shape, transform=None):
     """
     Generate a 2D rectangle domain in 3D space, define regions.
     """
@@ -41,10 +43,23 @@ def make_domain(dims, shape):
     domain.create_region('Gamma1', 'vertices in (x < %.10f)' % xmin, 'facet')
     domain.create_region('Gamma2', 'vertices in (x > %.10f)' % xmax, 'facet')
 
+    if transform == 'bend':
+        bbox = mesh.get_bounding_box()
+        x0, x1 = bbox[:, 0]
+
+        angles = 0.5 *  nm.pi * (coors[:, 0] - x0) / (x1 - x0)
+        mtx = make_axis_rotation_matrix([0, -1, 0], angles[:, None, None])
+
+        coors = mesh.coors.copy()
+        coors[:, 0] = 0
+        coors[:, 2] = (x1 - x0)
+
+        mesh.coors[:] = transform_data(coors, mtx=mtx)
+
     return domain
 
-def solve_problem(shape, dims, young, poisson, force):
-    domain = make_domain(dims[:2], shape)
+def solve_problem(shape, dims, young, poisson, force, transform=None):
+    domain = make_domain(dims[:2], shape, transform=transform)
 
     omega = domain.regions['Omega']
     gamma1 = domain.regions['Gamma1']
@@ -56,7 +71,11 @@ def solve_problem(shape, dims, young, poisson, force):
     v = FieldVariable('v', 'test', field, primary_var_name='u')
 
     thickness = dims[2]
-    pload = [[0.0, 0.0, force / shape[1], 0.0, 0.0, 0.0]] * shape[1]
+    if transform is None:
+        pload = [[0.0, 0.0, force / shape[1], 0.0, 0.0, 0.0]] * shape[1]
+
+    elif transform == 'bend':
+        pload = [[force / shape[1], 0.0, 0.0, 0.0, 0.0, 0.0]] * shape[1]
 
     m = Material('m', D=sh.create_elastic_tensor(young=210e9, poisson=0.3),
                  values={'.drill' : 1e-7})
@@ -90,14 +109,20 @@ def solve_problem(shape, dims, young, poisson, force):
 
     return pb, state, u, gamma2
 
-def get_analytical_displacement(dims, young, force):
+def get_analytical_displacement(dims, young, force, transform=None):
     """
     Returns the analytical value of the max. displacement according to
     Euler-Bernoulli theory.
     """
     l, b, h = dims
-    moment = b * h**3 / 12.0
-    u = force * l**3 / (3 * young * moment)
+
+    if transform is None:
+        moment = b * h**3 / 12.0
+        u = force * l**3 / (3 * young * moment)
+
+    elif transform == 'bend':
+        u = - 3.0 * nm.pi * l**3 / (young * b * h**3)
+
     return u
 
 usage = """%prog [options]"""
@@ -106,6 +131,8 @@ helps = {
     'dimensions of the cantilever [default: %default]',
     'nx' :
     'the range for the numbers of cells in the x direction [default: %default]',
+    'transform' :
+    'the transformation of the domain coordinates [default: %default]',
     'young' : "the Young's modulus [default: %default]",
     'poisson' : "the Poisson's ratio [default: %default]",
     'force' : "the force load [default: %default]",
@@ -121,6 +148,10 @@ def main():
     parser.add_option('-n', '--nx', metavar='start,stop,step',
                       action='store', dest='nx',
                       default='2,203,8', help=helps['nx'])
+    parser.add_option('-t', '--transform', metavar='none,bend',
+                      action='store', dest='transform',
+                      choices=['none', 'bend'],
+                      default='none', help=helps['transform'])
     parser.add_option('--young', metavar='float', type=float,
                       action='store', dest='young',
                       default=210e9, help=helps['young'])
@@ -150,26 +181,40 @@ def main():
     output("  Young's modulus:", options.young)
     output("  Poisson's ratio:", options.poisson)
     output('  force:', options.force)
+    output('  transform:', options.transform)
 
-    u_exact = get_analytical_displacement(dims, young, force)
+    if options.transform == 'none':
+        options.transform = None
+
+    u_exact = get_analytical_displacement(dims, young, force,
+                                          transform=options.transform)
+
+    if options.transform is None:
+        ilog = 2
+        label = 'u_3'
+
+    elif options.transform == 'bend':
+        ilog = 0
+        label = 'u_0'
 
     log = []
     for nx in xrange(*nxs):
         shape = (nx, 2)
 
-        pb, state, u, gamma2 = solve_problem(shape, dims, young, poisson, force)
+        pb, state, u, gamma2 = solve_problem(shape, dims, young, poisson, force,
+                                             transform=options.transform)
 
         dofs = u.get_state_in_region(gamma2)
         output('DOFs along the loaded edge:')
         output('\n%s' % dofs)
 
-        log.append((nx - 1, dofs[0, 2]))
+        log.append((nx - 1, dofs[0, ilog]))
 
     pb.save_state('shell10x_cantilever.vtk', state)
 
     log = nm.array(log)
 
-    output('max. u3 displacements w.r.t. number of cells:')
+    output('max. %s displacements w.r.t. number of cells:' % label)
     output('\n%s' % log)
     output('analytical value:', u_exact)
 
@@ -182,19 +227,19 @@ def main():
         })
 
         fig, ax1 = plt.subplots()
-        fig.suptitle('max. $u_3$ displacement')
+        fig.suptitle('max. $%s$ displacement' % label)
 
-        ax1.plot(log[:, 0], log[:, 1], 'b', label=r'$u_3$')
+        ax1.plot(log[:, 0], log[:, 1], 'b', label=r'$%s$' % label)
         ax1.set_xlabel('# of cells')
-        ax1.set_ylabel(r'$u_3$')
+        ax1.set_ylabel(r'$%s$' % label)
         ax1.grid(which='both')
         ax1.hlines(u_exact, log[0, 0], log[-1, 0],
-                   'r', 'dotted', label=r'$u_3^{analytical}$')
+                   'r', 'dotted', label=r'$%s^{analytical}$' % label)
 
         ax2 = ax1.twinx()
         ax2.semilogy(log[:, 0], nm.abs(log[:, 1] - u_exact), 'g',
-                     label=r'$|u_3 - u_3^{analytical}|$')
-        ax2.set_ylabel(r'$|u_3 - u_3^{analytical}|$')
+                     label=r'$|%s - %s^{analytical}|$' % (label, label))
+        ax2.set_ylabel(r'$|%s - %s^{analytical}|$' % (label, label))
 
         lines1, labels1 = ax1.get_legend_handles_labels()
         lines2, labels2 = ax2.get_legend_handles_labels()
