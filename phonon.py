09a5ee242ea3f4e8e10b2514ad0aaf9a4f6b06ae
Jan Heczko <jan.heczko@gmail.com>
2016-09-02 14:46:39 +0200

    use argparse instead of optparse in phonon.py

--- phonon.py
+++ phonon.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python
 # 12.01.2007, c
 from __future__ import absolute_import
-from optparse import OptionParser
+from argparse import ArgumentParser
 
 import sfepy
 from sfepy.base.base import output
 from sfepy.base.conf import ProblemConf, get_standard_keywords
 from sfepy.homogenization.band_gaps_app import AcousticBandGapsApp
 from sfepy.base.plotutils import plt
-
-usage = """%prog [options] filename_in"""
 
 help = {
     'filename' :
@@ -25,24 +23,27 @@ help = {
 }
 
 def main():
-    parser = OptionParser(usage=usage, version="%prog " + sfepy.__version__)
-    parser.add_option("-o", "", metavar='filename',
-                      action="store", dest="output_filename_trunk",
-                      default=None, help=help['filename'])
-    parser.add_option("-b", "--band-gaps",
-                      action="store_true", dest="detect_band_gaps",
-                      default=False, help=help['detect_band_gaps'])
-    parser.add_option("-d", "--dispersion",
-                      action="store_true", dest="analyze_dispersion",
-                      default=False, help=help['analyze_dispersion'])
-    parser.add_option("-p", "--plot",
-                      action="store_true", dest="plot",
-                      default=False, help=help['plot'])
-    parser.add_option("--phase-velocity",
-                      action="store_true", dest="phase_velocity",
-                      default=False, help=help['phase_velocity'])
-
-    options, args = parser.parse_args()
+    parser = ArgumentParser()
+    parser.add_argument("--version", action="version",
+                        version="%(prog)s " + sfepy.__version__)
+    parser.add_argument("-o", metavar='filename',
+                        action="store", dest="output_filename_trunk",
+                        default=None, help=help['filename'])
+    parser.add_argument("-b", "--band-gaps",
+                        action="store_true", dest="detect_band_gaps",
+                        default=False, help=help['detect_band_gaps'])
+    parser.add_argument("-d", "--dispersion",
+                        action="store_true", dest="analyze_dispersion",
+                        default=False, help=help['analyze_dispersion'])
+    parser.add_argument("-p", "--plot",
+                        action="store_true", dest="plot",
+                        default=False, help=help['plot'])
+    parser.add_argument("--phase-velocity",
+                        action="store_true", dest="phase_velocity",
+                        default=False, help=help['phase_velocity'])
+    parser.add_argument("filename_in")
+    
+    options = parser.parse_args()
     if options.plot:
         if plt is None:
             output('matplotlib.pyplot cannot be imported, ignoring option -p!')
@@ -50,19 +51,13 @@ def main():
         elif options.analyze_dispersion == False:
             options.detect_band_gaps = True
 
-    if (len(args) == 1):
-        filename_in = args[0];
-    else:
-        parser.print_help(),
-        return
-
     required, other = get_standard_keywords()
     required.remove('equations')
     if not options.analyze_dispersion:
         required.remove('solver_[0-9]+|solvers')
     if options.phase_velocity:
         required.remove('ebc_[0-9]+|ebcs')
-    conf = ProblemConf.from_file(filename_in, required, other)
+    conf = ProblemConf.from_file(options.filename_in, required, other)
 
     app = AcousticBandGapsApp(conf, options, 'phonon:')
     opts = conf.options
