4ab778ab884ebe0fbe4c72d3b2ab83af0a3dc2fe
Robert Cimrman <cimrman3@ntc.zcu.cz>
2016-09-06 10:18:19 +0200

    update find_level_interface(), refine() for 3D

--- sfepy/discrete/fem/refine_hanging.py
+++ sfepy/discrete/fem/refine_hanging.py
@@ -1,6 +1,12 @@
 """
 Functions for a mesh refinement with hanging nodes.
+
+Notes
+-----
+Using LCBCs with hanging nodes is not supported.
 """
+from six.moves import range
+
 import numpy as nm
 
 from sfepy.base.base import assert_
@@ -40,13 +46,14 @@ def find_level_interface(domain, refine_flag):
     - interface cells:
       - cells[:, 0] - cells to refine
       - cells[:, 1] - their facet sharing neighbors
-      - cells[:, 2] - their facet sharing neighbors w.r.t. the locally refined
+      - cells[:, 2] - facet kind: 0 = face, 1 = edge
+      - cells[:, 3] - their facet sharing neighbors w.r.t. the locally refined
         mesh.
     """
     if not refine_flag.any():
         facets = nm.zeros((0, 3), dtype=nm.uint32)
-        cells = nm.zeros((0, 2), dtype=nm.uint32)
-        return facets, cells, None, None
+        cells = nm.zeros((0, 4), dtype=nm.uint32)
+        return facets, cells, 0, None, None
 
     def _get_refine(coors, domain=None):
         return nm.nonzero(refine_flag)[0]
@@ -63,30 +70,126 @@ def find_level_interface(domain, refine_flag):
     region1 = domain.create_region('refine', 'cells by get_refine',
                                    functions=functions, add_to_regions=False)
 
-    facets = nm.intersect1d(region0.facets, region1.facets)
-
     cmesh = domain.mesh.cmesh
     dim = cmesh.dim
-    cmesh.setup_connectivity(dim - 1, dim)
-    cells, offs = cmesh.get_incident(dim, facets, dim - 1,
-                                     ret_offsets=True)
-    assert_((nm.diff(offs) == 2).all())
 
-    ii = cmesh.get_local_ids(facets, dim - 1, cells, offs, dim)
+    if dim == 2:
+        oe = 0
 
-    ii = ii.reshape((-1, 2))
-    cells = cells.reshape((-1, 2))
+        facets = nm.intersect1d(region0.facets, region1.facets)
 
-    ii = nm.where(refine_flag[cells], ii[:, :1], ii[:, 1:])
-    cells = nm.where(refine_flag[cells], cells[:, :1], cells[:, 1:])
+        cmesh.setup_connectivity(dim - 1, dim)
+        cells, offs = cmesh.get_incident(dim, facets, dim - 1,
+                                         ret_offsets=True)
+        assert_((nm.diff(offs) == 2).all())
 
-    facets = nm.c_[facets, ii]
+        ii = cmesh.get_local_ids(facets, dim - 1, cells, offs, dim)
 
-    # Indices of non-refined cells in the level-1 mesh.
-    ii = nm.searchsorted(region0.cells, cells[:, 1])
-    cells = nm.c_[cells, ii]
+        ii = ii.reshape((-1, 2))
+        cells = cells.reshape((-1, 2))
 
-    return facets, cells, region0, region1
+        ii = nm.where(refine_flag[cells], ii[:, :1], ii[:, 1:])
+        cells = nm.where(refine_flag[cells], cells[:, :1], cells[:, 1:])
+
+        facets = nm.c_[facets, ii]
+
+        # Indices of non-refined cells in the level-1 mesh.
+        ii = nm.searchsorted(region0.cells, cells[:, 1])
+        cells = nm.c_[cells, nm.zeros_like(ii), ii]
+
+    else: # if dim == 3:
+        gel = domain.geom_els['3_8']
+        epf = gel.get_edges_per_face()
+
+        cmesh.setup_connectivity(dim, dim)
+        fc, coffs = cmesh.get_incident(dim, region1.cells, dim,
+                                       ret_offsets=True)
+        cc = nm.repeat(region1.cells, nm.diff(coffs))
+        aux = nm.c_[cc, fc]
+        """
+        nnn[:, 0] cells to refine, nnn[:, 1] non-refined neighbours, nnn[:, 2]
+        neighbour kind : 0 face, 1 edge.
+        """
+        nn = aux[refine_flag[fc] == 0]
+
+        cf = nn[:, 0].copy().astype(nm.uint32)
+        cc = nn[:, 1].copy().astype(nm.uint32)
+
+        vc, vco = cmesh.get_incident(0, cc, dim, ret_offsets=True)
+        vf, vfo = cmesh.get_incident(0, cf, dim, ret_offsets=True)
+        vc = vc.reshape((-1, 8))
+        vf = vf.reshape((-1, 8))
+
+        nnn = []
+        oe = 0
+        for ii in range(vc.shape[0]):
+            aux = set(vc[ii]).intersection(vf[ii])
+            nc = len(aux)
+            if nc == 1: continue # Skip vertex-only neighbours.
+            if nc == 4:
+                nnn.append((nn[ii, 0], nn[ii, 1], 0))
+                oe += 1
+
+            else:
+                nnn.append((nn[ii, 0], nn[ii, 1], 1))
+        nnn = nm.array(nnn)
+
+        if nnn.shape[0] == 0:
+            facets = nm.zeros((0, 3), dtype=nm.uint32)
+            cells = nm.zeros((0, 4), dtype=nm.uint32)
+
+            return facets, cells, 0, region0, region1
+
+        ii = nm.argsort(nnn[:, 2])
+        nnn = nnn[ii]
+        cf = cf[ii]
+        cc = cc[ii]
+
+        ec, eco = cmesh.get_incident(1, cc, dim, ret_offsets=True)
+        ef, efo = cmesh.get_incident(1, cf, dim, ret_offsets=True)
+        ec = ec.reshape((-1, 12))
+        ef = ef.reshape((-1, 12))
+
+        fc, fco = cmesh.get_incident(2, cc, dim, ret_offsets=True)
+        ff, ffo = cmesh.get_incident(2, cf, dim, ret_offsets=True)
+        fc = fc.reshape((-1, 6))
+        ff = ff.reshape((-1, 6))
+
+        emask = nm.zeros((domain.shape.n_el, 12), dtype=nm.bool)
+
+        ffs = []
+        for ii in range(oe):
+            facet = nm.intersect1d(fc[ii], ff[ii])[0]
+            i1 = nm.where(ff[ii] == facet)[0][0]
+            i0 = nm.where(fc[ii] == facet)[0][0]
+            ffs.append((facet, i1, i0))
+
+            emask[nnn[ii, 0], epf[i1]] = True
+
+        for ii in range(oe, nnn.shape[0]):
+            facet = nm.intersect1d(ec[ii], ef[ii])[0]
+            i1 = nm.where(ef[ii] == facet)[0][0]
+            i0 = nm.where(ec[ii] == facet)[0][0]
+            ffs.append((facet, i1, i0))
+
+        ffs = nm.array(ffs)
+
+        ie = nm.where(nnn[:, 2] == 1)[0]
+        ennn = nnn[ie]
+        effs = ffs[ie]
+        omit = ie[emask[ennn[:, 0], effs[:, 1]]]
+
+        valid = nm.ones(nnn.shape[0], dtype=nm.bool)
+        valid[omit] = False
+
+        cells = nnn[valid]
+        facets = ffs[valid]
+
+        # Indices of non-refined cells in the level-1 mesh.
+        ii = nm.searchsorted(region0.cells, cells[:, 1])
+        cells = nm.c_[cells, ii]
+
+    return facets, cells, oe, region0, region1
 
 def refine_region(domain0, region0, region1):
     """
@@ -187,7 +290,10 @@ def force_facet_orientations(domain, gsubs, coris, dim):
         foris[ic] = coris[ii]
 
 def refine(domain0, refine, gsubs=None):
-    facets, cells, region0, region1 = find_level_interface(domain0, refine)
+    desc = domain0.mesh.descs[0]
+    assert_(desc in ['2_4', '3_8'])
+
+    facets, cells, oe, region0, region1 = find_level_interface(domain0, refine)
 
     print nm.c_[facets, cells]
 
@@ -207,28 +313,66 @@ def refine(domain0, refine, gsubs=None):
 
         print cells[:, 2]
         print conn0[cells[:, 1]]
-        print conn1[cells[:, 2]]
-        assert_((conn0[cells[:, 1]] == conn1[cells[:, 2]]).all())
+        print conn1[cells[:, 3]]
+        assert_((conn0[cells[:, 1]] == conn1[cells[:, 3]]).all())
 
     desc = domain0.mesh.descs[0]
     if desc == '2_4':
-        gsubs1 = find_facet_substitutions(facets, cells, sub_cells,
-                                          refine_edges_2_4)
+        cmesh0 = domain0.mesh.cmesh
+        oris = cmesh0.edge_oris.reshape((-1, 4))[region1.cells]
 
-    gsubs1 = find_facet_substitutions(facets, cells, sub_cells)
-    if gsubs is None:
-        gsubs = gsubs1 if len(gsubs1) else None
+        gsubs1, coris = find_facet_substitutions(facets, cells,
+                                                 sub_cells, oris,
+                                                 refine_edges_2_4)
+        force_facet_orientations(domain, gsubs1, coris, 1)
 
-    elif len(gsubs1):
-        mods = nm.zeros(domain.shape.n_el + 1, dtype=nm.int32)
-        mods[refine > 0] = -1
-        mods = nm.cumsum(mods)
+        if gsubs is None:
+            gsubs = gsubs1 if len(gsubs1) else None
 
-        print gsubs
-        gsubs[:, [0, 2, 4]] += mods[gsubs[:, [0, 2, 4]]]
-        print gsubs
+        elif len(gsubs1):
+            mods = nm.zeros(domain.shape.n_el + 1, dtype=nm.int32)
+            mods[refine > 0] = -1
+            mods = nm.cumsum(mods)
 
-        gsubs = nm.r_[gsubs, gsubs1]
+            gsubs[:, [0, 2, 4]] += mods[gsubs[:, [0, 2, 4]]]
+
+            gsubs = nm.r_[gsubs, gsubs1]
+
+    else:
+        cmesh0 = domain0.mesh.cmesh
+        foris = cmesh0.face_oris.reshape((-1, 6))[region1.cells]
+        eoris = cmesh0.edge_oris.reshape((-1, 12))[region1.cells]
+
+        gsubs1f, fcoris = find_facet_substitutions(facets[:oe], cells[:oe],
+                                                   sub_cells, foris,
+                                                   refine_faces_3_8)
+        force_facet_orientations(domain, gsubs1f, fcoris, 2)
+
+        gsubs1e, ecoris = find_facet_substitutions(facets[oe:], cells[oe:],
+                                                   sub_cells, eoris,
+                                                   refine_edges_3_8)
+        force_facet_orientations(domain, gsubs1e, ecoris, 1)
+
+        if gsubs is None:
+            gsubs = (gsubs1f if len(gsubs1f) else None,
+                     gsubs1e if len(gsubs1f) else None) # !!!
+
+        elif len(gsubs1):
+            gsubsf, gsubse = gsubs
+
+            mods = nm.zeros(domain.shape.n_el + 1, dtype=nm.int32)
+            mods[refine > 0] = -1
+            mods = nm.cumsum(mods)
+
+            gsubsf[:, [0, 2, 4, 6, 8]] += mods[gsubsf[:, [0, 2, 4, 6, 8]]]
+            gsubsf = nm.r_[gsubsf, gsubs1f]
+
+            gsubse[:, [0, 2, 4]] += mods[gsubse[:, [0, 2, 4]]]
+            gsubse = nm.r_[gsubse, gsubs1e]
+
+            gsubs = (gsubs1f, gsubs1e)
+
+        if gsubs == (None, None): gsubs = None
 
     return domain, gsubs
 
