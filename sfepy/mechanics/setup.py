1cbb844241352194ffe258435fe6c788c30cec41
Robert Cimrman <cimrman3@ntc.zcu.cz>
2017-05-10 19:19:00 +0200

    new sfepy/mechanics/extmods/__init__.py

--- sfepy/mechanics/setup.py
+++ sfepy/mechanics/setup.py
@@ -7,7 +7,7 @@ def configuration(parent_package='', top_path=None):
     auto_name = op.split(op.dirname(__file__))[-1]
     config = Configuration(auto_name, parent_package, top_path)
 
-#    config.add_data_dir('tests')
+    config.add_subpackage('extmods')
 
     return config
 
