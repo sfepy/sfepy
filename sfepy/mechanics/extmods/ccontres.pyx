a674945d55996590eb66d86f40aba3445388112a
Robert Cimrman <cimrman3@ntc.zcu.cz>
2017-05-11 14:37:23 +0200

    new init_global_search() cython function

--- sfepy/mechanics/extmods/ccontres.pyx
+++ sfepy/mechanics/extmods/ccontres.pyx
@@ -78,3 +78,50 @@ def get_AABB(
              n, nsn, nes, nen, neq)
 
     return AABBmin, AABBmax
+
+def init_global_search(
+    np.ndarray[int32, mode='c', ndim=1] N not None,
+    np.ndarray[float64, mode='c', ndim=1] AABBmin not None,
+    np.ndarray[float64, mode='c', ndim=1] AABBmax not None,
+    np.ndarray[float64, mode='fortran', ndim=2] X not None,
+    ):
+    """
+    The linked list initialization. The head array contains, at the position
+    Ic, the index of the first point that belongs to the cell Ic, the second
+    point index is then next[head[Ic]], the third point index is
+    next[next[head[Ic]]] etc. - the next array points from the i-th point in
+    each cell to the (i+1)-th point, until -1 is reached.
+    """
+    cdef int i, lower, upper, Ic
+    cdef np.ndarray[int32, mode='c', ndim=1] I
+    cdef np.ndarray j
+    cdef np.ndarray[int32, mode='c', ndim=1] head
+    cdef np.ndarray[int32, mode='c', ndim=1] next
+
+    dim = X.shape[1]
+    nnod = X.shape[0]
+
+    head = np.empty((np.prod(N),), dtype=np.int32);
+    head[:] = -1
+
+    next = np.empty((nnod,), dtype=np.int32);
+    next[:] = -1
+
+    # Loop over points to find a cell each point is in.
+    for i in range(nnod):
+        lower = np.sum(X[i] < AABBmin)
+        higher = np.sum(X[i] > AABBmax)
+
+        if lower + higher: continue
+
+        I = np.floor(N * (X[i] - AABBmin) / (AABBmax-AABBmin)).astype(np.int32)
+
+        j = I == N
+        I[j] = N[j] - 1
+
+        Ic = np.ravel_multi_index(I, N)
+
+        next[i] = head[Ic]
+        head[Ic] = i
+
+    return head, next
