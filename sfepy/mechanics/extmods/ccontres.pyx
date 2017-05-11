9cfbaa6d12f31972aebf65cf653b39623587895c
Robert Cimrman <cimrman3@ntc.zcu.cz>
2017-05-11 15:05:09 +0200

    clean up

--- sfepy/mechanics/extmods/ccontres.pyx
+++ sfepy/mechanics/extmods/ccontres.pyx
@@ -8,19 +8,19 @@ from sfepy.discrete.common.extmods.types cimport int32, uint32, float64
 
 cdef extern from 'contres.h':
      cdef void _getLongestEdgeAndGPs \
-     'getLongestEdgeAndGPs'(float64* longestEdge, float64* GPs,
-                            int n, int nsd, int ngp, int neq, int nsn,
-                            int nes, int nen,
-                            uint32* elementID, uint32* segmentID,
-                            int32* ISN, int32* IEN, float64* H, float64* X)
+          'getLongestEdgeAndGPs'(float64* longestEdge, float64* GPs,
+                                 int n, int nsd, int ngp, int neq, int nsn,
+                                 int nes, int nen,
+                                 uint32* elementID, uint32* segmentID,
+                                 int32* ISN, int32* IEN, float64* H, float64* X)
      cdef void _getAABB \
-     'getAABB'(float64* AABBmin, float64* AABBmax, int nsd, int nnod,
-               float64* X, float64 longestEdge, int32* IEN, int32* ISN,
-               uint32* elementID, uint32* segmentID,
-               int n, int nsn, int nes, int nen, int neq)
+          'getAABB'(float64* AABBmin, float64* AABBmax, int nsd, int nnod,
+                    float64* X, float64 longestEdge, int32* IEN, int32* ISN,
+                    uint32* elementID, uint32* segmentID,
+                    int n, int nsn, int nes, int nen, int neq)
 
 def get_longest_edge_and_gps(
-    np.ndarray[float64, mode='fortran', ndim=2] GPS not None,
+    np.ndarray[float64, mode='fortran', ndim=2] GPs not None,
     int neq,
     np.ndarray[uint32, mode='c', ndim=1] elementID not None,
     np.ndarray[uint32, mode='c', ndim=1] segmentID not None,
@@ -39,14 +39,15 @@ def get_longest_edge_and_gps(
     nsn = ISN.shape[0]
     nes = ISN.shape[1]
     nen = IEN.shape[1]
-    _getLongestEdgeAndGPs(<float64 *> &tmp, &GPS[0, 0],
+
+    _getLongestEdgeAndGPs(<float64 *> &tmp, &GPs[0, 0],
                           n, nsd, ngp, neq, nsn, nes, nen,
                           &elementID[0], &segmentID[0], &ISN[0, 0], &IEN[0, 0],
                           &H[0, 0], &X[0, 0])
 
     longestEdge = tmp[0]
 
-    return longestEdge, GPS
+    return longestEdge, GPs
 
 def get_AABB(
     np.ndarray[float64, mode='fortran', ndim=2] X not None,
