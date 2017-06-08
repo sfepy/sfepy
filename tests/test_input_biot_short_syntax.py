98508f7e216688c01fbcd0599a4a1650d112bc26
Robert Cimrman <cimrman3@ntc.zcu.cz>
2017-06-08 13:08:08 +0200

    new examples/multi_physics/biot_short_syntax.py + test

---
+++ tests/test_input_biot_short_syntax.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+input_name = '../examples/multi_physics/biot_short_syntax.py'
+output_name = 'test_biot_short_syntax.vtk'
+
+from tests_basic import TestInput
+
+class Test(TestInput):
+    pass
