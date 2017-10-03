c84a90d89bbd6e40ad1ce64fdfd46bdf58415e83
Robert Cimrman <cimrman3@ntc.zcu.cz>
2017-10-03 22:08:44 +0200

    script/plot_quadratures.py: new --boundary option
    - update _get_bqp(), plot_quadrature()

--- script/plot_quadratures.py
+++ script/plot_quadratures.py
@@ -15,6 +15,8 @@ helps = {
     ' [default: %(default)s]',
     'order' :
     'quadrature order [default: %(default)s]',
+    'boundary' :
+    'plot boundary quadrature points',
     'min_radius' :
     'min. radius of points corresponding to the min. weight'
     ' [default:  %(default)s]',
@@ -34,6 +36,9 @@ def main():
     parser.add_argument('-n', '--order', metavar='order', type=int,
                         action='store', dest='order',
                         default=2, help=helps['order'])
+    parser.add_argument('-b', '--boundary',
+                        action='store_true', dest='boundary',
+                        default=False, help=helps['boundary'])
     parser.add_argument('-r', '--min-radius', metavar='float', type=float,
                         action='store', dest='min_radius',
                         default=10, help=helps['min_radius'])
@@ -45,7 +50,7 @@ def main():
                         default=False, help=helps['show_colorbar'])
     options = parser.parse_args()
 
-    pq.plot_quadrature(None, options.geometry, options.order,
+    pq.plot_quadrature(None, options.geometry, options.order, options.boundary,
                        options.min_radius, options.max_radius,
                        options.show_colorbar)
     pq.plt.show()
--- sfepy/postprocess/plot_quadrature.py
+++ sfepy/postprocess/plot_quadrature.py
@@ -1,6 +1,7 @@
 """
 Functions to visualize quadrature points in reference elements.
 """
+import numpy as nm
 import matplotlib.pyplot as plt
 
 from sfepy.base.base import output
@@ -22,6 +23,31 @@ def _get_qp(geometry, order):
     output('max. weight:', weights.max())
 
     return GeometryElement(geometry), coors, weights
+
+def _get_bqp(geometry, order):
+    from sfepy.discrete import Integral
+    from sfepy.discrete.fem.geometry_element import GeometryElement
+    from sfepy.discrete.fem import Mesh, FEDomain, Field
+
+    gel = GeometryElement(geometry)
+
+    mesh = Mesh.from_data('aux', gel.coors, None,
+                          [gel.conn[None, :]], [[0]], [geometry])
+    domain = FEDomain('domain', mesh)
+    omega = domain.create_region('Omega', 'all')
+    surf =  domain.create_region('Surf', 'vertices of surface', 'facet')
+    field = Field.from_args('f', nm.float64, shape=1,
+                            region=omega, approx_order=1)
+    field.setup_surface_data(surf)
+
+    integral = Integral('aux', order=order)
+    field.create_bqp('Surf', integral)
+
+    sd = field.surface_data['Surf']
+    qp = field.qp_coors[(integral.order, sd.bkey)]
+
+    return (gel, qp.vals.reshape((-1, mesh.dim)),
+            nm.tile(qp.weights, qp.vals.shape[0]))
 
 def plot_weighted_points(ax, coors, weights, min_radius=10, max_radius=50,
                          show_colorbar=False):
@@ -48,7 +74,8 @@ def plot_weighted_points(ax, coors, weights, min_radius=10, max_radius=50,
 
     return ax
 
-def plot_quadrature(ax, geometry, order, min_radius=10, max_radius=50,
+def plot_quadrature(ax, geometry, order, boundary=False,
+                    min_radius=10, max_radius=50,
                     show_colorbar=False):
     """
     Plot quadrature points for the given geometry and integration order.
@@ -56,7 +83,12 @@ def plot_quadrature(ax, geometry, order, min_radius=10, max_radius=50,
     The points are plotted as circles/spheres with radii given by quadrature
     weights - the weights are mapped to [`min_radius`, `max_radius`] interval.
     """
-    gel, coors, weights = _get_qp(geometry, order)
+    if not boundary:
+        gel, coors, weights = _get_qp(geometry, order)
+
+    else:
+        gel, coors, weights = _get_bqp(geometry, order)
+
     dim = coors.shape[1]
 
     ax = _get_axes(ax, dim)
