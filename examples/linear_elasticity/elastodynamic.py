9141acf114025b41a73504bef706ebdceb90c340
Robert Cimrman <cimrman3@ntc.zcu.cz>
2018-01-15 13:51:28 +0100

    update elastodynamic.py example for iron plate impact problem, generate mesh

--- examples/linear_elasticity/elastodynamic.py
+++ examples/linear_elasticity/elastodynamic.py
@@ -4,20 +4,64 @@ from __future__ import absolute_import
 
 import numpy as nm
 
-from sfepy import data_dir
-from sfepy.mechanics.matcoefs import stiffness_from_lame
+import sfepy.mechanics.matcoefs as mc
+from sfepy.discrete.fem.meshio import UserMeshIO
+from sfepy.mesh.mesh_generators import gen_block_mesh
 
-filename_mesh = data_dir + '/meshes/3d/cylinder.mesh'
+# Material parameters.
+E = 200e9
+nu = 0.3
+rho = 7800.0
+
+lam, mu = mc.lame_from_youngpoisson(E, nu)
+cl = nm.sqrt((lam + 2.0 * mu) / rho)
+cs = nm.sqrt(mu / rho)
+
+# Initial velocity.
+v0 = 1
+
+# Mesh dimensions.
+d = 2.5e-3
+L = 4 * d
+dims = [L, 2 * d, 2 * d]
+
+# Mesh resolution: increase to improve accuracy.
+shape = [21, 6, 6]
+
+# Element size.
+H = L / (shape[0] - 1)
+
+# Time-stepping parameters.
+dt = H / cl
+t1 = 0.9 * L / cl
+
+def mesh_hook(mesh, mode):
+    """
+    Generate the block mesh.
+    """
+    if mode == 'read':
+        mesh = gen_block_mesh(dims, shape, [0.5 * L, d, d], name='user_block',
+                              verbose=False)
+        return mesh
+
+    elif mode == 'write':
+        pass
+
+filename_mesh = UserMeshIO(mesh_hook)
 
 regions = {
     'Omega' : 'all',
-    'Left' : ('vertices in (x < 0.001)', 'facet'),
-    'Right' : ('vertices in (x > 0.099)', 'facet'),
+    'Impact' : ('vertices in (x < 1e-12)', 'facet'),
+    'Symmetry-y' : ('vertices in (y < 1e-12)', 'facet'),
+    'Symmetry-z' : ('vertices in (z < 1e-12)', 'facet'),
 }
 
+# Iron.
 materials = {
-    'solid' : ({'D': stiffness_from_lame(dim=3, lam=1e1, mu=1e0),
-                'rho': 10.0},),
+    'solid' : ({
+            'D': mc.stiffness_from_youngpoisson(dim=3, young=E, poisson=nu),
+            'rho': rho,
+     },),
 }
 
 fields = {
@@ -40,9 +84,9 @@ variables = {
 }
 
 ebcs = {
-    'Fixed' : ('Left', {'u.all' : 0.0, 'du.all' : 0.0, 'ddu.all' : 0.0}),
-    'Displaced' : ('Right', {'u.0' : 0.01, 'u.[1,2]' : 0.0,
-                             'du.all' : 0.0, 'ddu.all' : 0.0}),
+    'Impact' : ('Impact', {'u.all' : 0.0, 'du.all' : 0.0, 'ddu.all' : 0.0}),
+    'Symmtery-y' : ('Symmetry-y', {'u.1' : 0.0, 'du.1' : 0.0, 'ddu.1' : 0.0}),
+    'Symmetry-z' : ('Symmetry-z', {'u.2' : 0.0, 'du.2' : 0.0, 'ddu.2' : 0.0}),
 }
 
 def get_ic(coor, ic, mode='u'):
@@ -51,7 +95,7 @@ def get_ic(coor, ic, mode='u'):
         val[:, 0] = 0.0
 
     elif mode == 'du':
-        val[:, 1] = 0.1
+        val[:, 0] = -1.0
 
     return val
 
@@ -76,9 +120,17 @@ equations = {
 
 solvers = {
     'ls' : ('ls.scipy_direct', {}),
+    'ls-i' : ('ls.petsc', {
+        'method' : 'cg',
+        'precond' : 'icc',
+        'i_max' : 150,
+        'eps_a' : 1e-32,
+        'eps_r' : 1e-8,
+        'verbose' : 2,
+    }),
     'newton' : ('nls.newton', {
         'i_max'      : 1,
-        'eps_a'      : 1e-10,
+        'eps_a'      : 1e-6,
     }),
     # 'tss' : ('ts.simple', {
     #     't0' : 0.0,
@@ -94,13 +146,15 @@ solvers = {
     # }),
     'tsn' : ('ts.newmark', {
         't0' : 0.0,
-        't1' : 1.0,
-        'dt' : 0.1,
+        't1' : t1,
+        'dt' : dt,
         'n_step' : None,
 
         'u' : 'u',
         'v' : 'du',
         'a' : 'ddu',
+
+        'verbose' : 1,
     }),
     # 'tsp' : ('ts.petsc', {
     #     'method' : 'rk',
@@ -109,4 +163,13 @@ solvers = {
     #     'dt' : 0.1,
     #     'n_step' : None,
     # }),
+}
+
+options = {
+    'ts' : 'ts',
+    'nls' : 'newton',
+    'ls' : 'ls-i',
+
+    'output_format' : 'h5',
+    'output_dir' : 'output/ed',
 }
