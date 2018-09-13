411a2abcd1393e0f9385b0a339519b91c95e2f8d
Tomáš Zítka <zitkat@students.zcu.cz>
2018-09-13 14:56:11 +0200

    Start implementing basis functions class, development test are in main so far.

--- sfepy/discrete/dg/dg_basis.py
+++ sfepy/discrete/dg/dg_basis.py
@@ -3,5 +3,25 @@ import numpy as nm
 class DGBasis:
 
     def __init__(self, degree):
-        self.degree = degree
-        self, degree
+        self.degree = degree + 1
+
+    def values(self, points):
+
+        if isinstance(points, (int, float)):
+            n = 1
+        else:
+            n = len(points)
+        values = nm.ones((self.degree, n))
+        for i in range(1, self.degree):
+            values[i] = points * values[i-1]
+        return values.T
+
+
+if __name__ == '__main__':
+    bs = DGBasis(5)
+    vals = bs.values(nm.linspace(0, 1))
+    from matplotlib import pylab as plt
+
+    plt.plot(nm.linspace(0, 1), vals)
+    plt.show()
+
