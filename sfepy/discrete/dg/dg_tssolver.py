dd1bf680402052871c1df805b816ac46bf438ece
Tomáš Zítka <zitkat@students.zcu.cz>
2018-10-02 13:51:27 +0200

    Add ic and bc treatement, proper assembly of the terms for higher orders of approximation, proper calculation of flux in FluxDGTerm

--- sfepy/discrete/dg/dg_tssolver.py
+++ sfepy/discrete/dg/dg_tssolver.py
@@ -10,30 +10,51 @@ import matplotlib.pyplot as plt
 
 class TSSolver:
 
-    def __init__(self, eq, ic):
+    def __init__(self, eq, ic, bc):
         self.equation = eq
         self.mesh = eq.mesh
         self.initial_cond = ic
+        self.boundary_cond = bc
 
     def solve(self, t0, tend, tsteps=10):
         print("Running testing solver: it does not solve anything, only tests shapes and types of data!")
         # TODO how to get number of cells from mesh?
-        A = nm.zeros((len(self.mesh.coors)-1, len(self.mesh.coors)-1), dtype=nm.float64)
-        b = nm.zeros((len(self.mesh.coors)-1, 1), dtype=nm.float64)
-        u = nm.ones((len(self.mesh.coors), 1), dtype=nm.float64)
-        u[5] = 2
+        A = nm.zeros((2, len(self.mesh.coors)-1, len(self.mesh.coors)-1), dtype=nm.float64)
+        b = nm.zeros((2, len(self.mesh.coors)-1, 1), dtype=nm.float64)
+        u = nm.ones((2, len(self.mesh.coors) + 1 , 1), dtype=nm.float64)
+
+        # ic
+        u[0, 1:-1] = self.initial_cond
+        u[1, 1:-1] = self.initial_cond
+
+        # bc
+        u[0, 0] = self.boundary_cond["left"]
+        u[0, -1] = self.boundary_cond["right"]
+        u[1, 0] = self.boundary_cond["left"]
+        u[1, -1] = self.boundary_cond["right"]
+
         self.equation.evaluate(dw_mode="matrix", asm_obj=A, diff_var="u")
         self.equation.evaluate(dw_mode="vector", asm_obj=b, diff_var=None, u=u)
 
-        print(A)
-        print(b)
+        # print(A)
+        # print(b)
 
-        plt.imshow(A)
-        plt.plot(b)
-        plt.show()
+        plt.figure("A[0]")
+        plt.imshow(A[0])
+        plt.figure("A[1]")
+        plt.imshow(A[1])
 
-        U = dot(nm.linalg.inv(A), b)
+        plt.figure("b")
+        plt.plot(b[0], label="b0")
+        plt.plot(b[1], label="b1")
+        plt.legend()
 
-        print(U)
-        plt.plot(U)
+        u[0, 1:-1] = dot(nm.linalg.inv(A[0]), b[0])
+        u[1, 1:-1] = dot(nm.linalg.inv(A[1]), b[1])
+
+        # print(u)
+        plt.figure("u")
+        plt.plot(u[0], label="u0")
+        plt.plot(u[1], label="u1")
+        plt.legend()
         plt.show()
--- sfepy/discrete/dg/example_dg_advection_1D.py
+++ sfepy/discrete/dg/example_dg_advection_1D.py
@@ -1,4 +1,5 @@
 import numpy as nm
+from my_utils.inits_consts import left_par_q
 
 
 # sfepy imports
@@ -10,7 +11,7 @@ from dg_terms import AdvFluxDGTerm, AdvIntDGTerm
 from dg_equation import Equation
 from dg_tssolver import TSSolver
 
-n_nod = 11
+n_nod = 101
 coors = nm.linspace(0.0, 1.0, n_nod).reshape((n_nod, 1))
 conn = nm.arange(n_nod, dtype=nm.int32).repeat(2)[1:-1].reshape((-1, 2))
 mat_ids = nm.zeros(n_nod - 1, dtype=nm.int32)
@@ -19,8 +20,13 @@ mesh = Mesh.from_data('advection_1d', coors, None,
                       [conn], [mat_ids], descs)
 
 a = 1.2
-IntT = AdvIntDGTerm(mesh, a)
+IntT = AdvIntDGTerm(mesh)
 FluxT = AdvFluxDGTerm(mesh, a)
 eq = Equation((IntT, FluxT))
-tss = TSSolver(eq, 0)
+# TODO sample initial condition properly
+ic = left_par_q((mesh.coors[:-1] + mesh.coors[1:])/2)
+bc = {"left" : 0,
+      "right" : 0}
+
+tss = TSSolver(eq, ic, bc)
 tss.solve(0, 1, 10)
