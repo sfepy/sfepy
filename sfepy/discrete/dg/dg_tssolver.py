f2c38be72c3c4a3ccfd11aeaccef92318313ceb8
Tomáš Zítka <zitkat@students.zcu.cz>
2018-10-17 21:17:05 +0200

    Add Courant number calculation and mesh info output.

--- sfepy/discrete/dg/dg_tssolver.py
+++ sfepy/discrete/dg/dg_tssolver.py
@@ -4,22 +4,46 @@ import numpy as nm
 from numpy import dot
 import matplotlib.pyplot as plt
 
+from numpy import newaxis as nax
 
 class TSSolver:
 
-    def __init__(self, eq, ic, bc):
+    def __init__(self, eq, ic, bc, basis):
         self.equation = eq
         self.mesh = eq.mesh
-        # TODO setup initial condition - integrate over elements and set coefs
-        self.initial_cond = ic
+        self.basis = basis
+        self.initial_cond = self.sampleIC(self.mesh, ic, self.intGauss2, self.basis)
         self.boundary_cond = bc
+
+    def sampleIC(self, mesh, ic, quad, basis):
+        # TODO create basis and check ic sampling
+        sic = nm.zeros((2, self.mesh.n_el, 1), dtype=nm.float64)
+
+        def psi0(x):
+            return 1
+
+        def psi1(x):
+            return 2 * (x - (self.mesh.coors[1:] + self.mesh.coors[:-1]) / 2) / (self.mesh.coors[1:] - self.mesh.coors[:-1])
+
+        sic[0, :] = quad(mesh, lambda t: ic(t)*psi0(t))/2
+        sic[1, :] = 3*quad(mesh, lambda t: ic(t)*psi1(t))/2
+        return sic
+
+    @staticmethod
+    def intGauss2(mesh, f):
+        # TODO check transformation to the reference element
+        x_1 = mesh.coors[:-1] + (mesh.coors[1:] - mesh.coors[:-1]) * (-nm.sqrt(1 / 2) + 1) / 2
+        x_2 = mesh.coors[:-1] + (mesh.coors[1:] - mesh.coors[:-1]) * (nm.sqrt(1 / 3) + 1) / 2
+
+        w = (mesh.coors[1:] - mesh.coors[:-1]) / 2
+
+        return f(x_1) + f(x_2)
 
     def solve(self, t0, tend, tsteps=10):
         print("Running testing solver: it does not solve anything, only tests shapes and types of data!")
-        # TODO how to get number of cells from mesh?
-        A = nm.zeros((2, len(self.mesh.coors)-1, len(self.mesh.coors)-1), dtype=nm.float64)
-        b = nm.zeros((2, len(self.mesh.coors)-1, 1), dtype=nm.float64)
-        u = nm.ones((2, len(self.mesh.coors) + 1 , 1), dtype=nm.float64)
+        A = nm.zeros((2, self.mesh.n_el, self.mesh.n_el), dtype=nm.float64)
+        b = nm.zeros((2, self.mesh.n_el-1, 1), dtype=nm.float64)
+        u = nm.zeros((2, self.mesh.n_nod + 1 , 1), dtype=nm.float64)
 
         # ic
         u[0, 1:-1] = self.initial_cond
@@ -57,83 +81,77 @@ class TSSolver:
         plt.legend()
         plt.show()
 
+
 class RK3Solver(TSSolver):
 
     def solve(self, t0, tend, tsteps=10):
-        # TODO implement three step Ruge-Kutta
-        dt = (tend - t0) / tsteps
+        dt = float(tend - t0) / tsteps
+        dx = nm.max(self.mesh.coors[1:] - self.mesh.coors[:-1])
+        dtdx = dt/dx
+        maxa = self.equation.terms[1].a
 
-        A = nm.zeros((2, len(self.mesh.coors) - 1, len(self.mesh.coors) - 1), dtype=nm.float64)
-        b = nm.zeros((2, len(self.mesh.coors) - 1, 1), dtype=nm.float64)
-        u = nm.ones((2, len(self.mesh.coors) + 1, tsteps), dtype=nm.float64)
-        u1 = nm.ones((2, len(self.mesh.coors) + 1), dtype=nm.float64)
-        u2 = nm.ones((2, len(self.mesh.coors) + 1), dtype=nm.float64)
-        u3 = nm.ones((2, len(self.mesh.coors) + 1), dtype=nm.float64)
+        print("Space divided into {0} cells, {1} steps, step size is {2}".format(self.mesh.n_el, len(self.mesh.coors), dx))
+        print("Time divided into {0} nodes, {1} steps, step size is {2}".format(tsteps - 1, tsteps, dt))
+        print("Courant number c = max(abs(u)) * dt/dx = {0}".format(maxa * dtdx))
 
-        # TODO check shapes and simplify bc treatment
+        A  = nm.zeros((2, self.mesh.n_el, self.mesh.n_el), dtype=nm.float64)
+        b  = nm.zeros((2, self.mesh.n_el, 1), dtype=nm.float64)
+        u  = nm.zeros((2, self.mesh.n_el + 2, tsteps, 1), dtype=nm.float64)
+        u1 = nm.zeros((2, self.mesh.n_el + 2, 1), dtype=nm.float64)
+        u2 = nm.zeros((2, self.mesh.n_el + 2, 1), dtype=nm.float64)
+
         # bc
-        u[0, 0, 0] = self.boundary_cond["left"]
-        u[0, -1, 0] = self.boundary_cond["right"]
-        u[1, 0, 0] = self.boundary_cond["left"]
-        u[1, -1, 0] = self.boundary_cond["right"]
+        u[:, 0, 0] = self.boundary_cond["left"]
+        u[:, -1, 0] = self.boundary_cond["right"]
 
         # ic
-        u[0, 1:-1, 0] = self.initial_cond[0, :]
-        u[1, 1:-1, 0] = self.initial_cond[1, :]
+        u[:, 1:-1, 0] = self.initial_cond
 
         for it in range(1, tsteps):
-            # 1st stage
+            # ----1st stage----
             # bcs
-            u1[0, 0] = self.boundary_cond["left"]
-            u1[0, -1] = self.boundary_cond["right"]
-            u1[1, 0] = self.boundary_cond["left"]
-            u1[1, -1] = self.boundary_cond["right"]
+            u1[:, 0] = self.boundary_cond["left"]
+            u1[:, -1] = self.boundary_cond["right"]
 
             # get RHS
+            A[:] = 0
+            b[:] = 0
             self.equation.evaluate(dw_mode="matrix", asm_obj=A, diff_var="u")
             self.equation.evaluate(dw_mode="vector", asm_obj=b, diff_var=None, u=u[:, :, it-1])
 
             # get update u1
-            u1[0, 1:-1] = u[0, 1:-1, it-1] + dt * dot(nm.linalg.inv(A[0]), b[0])
-            u1[1, 1:-1] = u[1, 1:-1, it-1] + dt * dot(nm.linalg.inv(A[1]), b[1])
+            u1[0, 1:-1] = u[0, 1:-1, it-1] + dt * b[0] / nm.diag(A[0])[:, nax]
+            u1[1, 1:-1] = u[1, 1:-1, it-1] + dt * b[1] / nm.diag(A[1])[:, nax]
 
-            # 2nd stage
+            # ----2nd stage----
             # bcs
-            u2[0, 0] = self.boundary_cond["left"]
-            u2[0, -1] = self.boundary_cond["right"]
-            u2[1, 0] = self.boundary_cond["left"]
-            u2[1, -1] = self.boundary_cond["right"]
+            u2[:, 0] = self.boundary_cond["left"]
+            u2[:, -1] = self.boundary_cond["right"]
 
             # get RHS
+            A[:] = 0
+            b[:] = 0
             self.equation.evaluate(dw_mode="matrix", asm_obj=A, diff_var="u")
             self.equation.evaluate(dw_mode="vector", asm_obj=b, diff_var=None, u=u1[:, :])
 
             # get update u2
-            u2[0, 1:-1] = 3 * u[0, 1:-1, it - 1]/4  \
-                            + u1[0, 1:-1] / 4 \
-                            + dt * dot(nm.linalg.inv(A[0]), b[0]) / 4
-            u2[1, 1:-1] = 3 * u[1, 1:-1, it - 1]/4  \
-                            + u1[1, 1:-1] / 4 \
-                            + dt * dot(nm.linalg.inv(A[1]), b[1]) / 4
+            u2[0, 1:-1] = (3 * u[0, 1:-1, it - 1] + u1[0, 1:-1]
+                            + dt * b[0] / nm.diag(A[0])[:, nax]) / 4
+            u2[1, 1:-1] = (3 * u[1, 1:-1, it - 1] + u1[1, 1:-1]
+                            + dt * b[1] / nm.diag(A[1])[:, nax]) / 4
 
-            # 3rd stage
-            # bcs
-            u3[0, 0] = self.boundary_cond["left"]
-            u3[0, -1] = self.boundary_cond["right"]
-            u3[1, 0] = self.boundary_cond["left"]
-            u3[1, -1] = self.boundary_cond["right"]
-
+            # ----3rd stage-----
             # get RHS
+            A[:] = 0
+            b[:] = 0
             self.equation.evaluate(dw_mode="matrix", asm_obj=A, diff_var="u")
             self.equation.evaluate(dw_mode="vector", asm_obj=b, diff_var=None, u=u2[:, :])
 
             # get update u3
-            u3[0, 1:-1] = u[0, 1:-1, it - 1] / 3 \
-                          + u2[0, 1:-1] / 3 \
-                          + dt * dot(nm.linalg.inv(A[0]), b[0]) / 3
-            u3[1, 1:-1] = u[1, 1:-1, it - 1] / 3 \
-                          + u2[1, 1:-1] / 3 \
-                          + dt * dot(nm.linalg.inv(A[1]), b[1]) / 3
+            u[0, 1:-1, it] = (u[0, 1:-1, it - 1] + 2 * u2[0, 1:-1]
+                              + 2*dt * b[0] / nm.diag(A[0])[:, nax]) / 3
+            u[1, 1:-1, it] = (u[1, 1:-1, it - 1] + 2 * u2[1, 1:-1]
+                              + 2*dt * b[1] / nm.diag(A[1])[:, nax]) / 3
 
-            u[:, :, it-1] = u3
+        return u, dt
 
