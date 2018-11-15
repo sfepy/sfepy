e290d4a8656ad781aa08d5f6dc0903e0aa73a49f
Robert Cimrman <cimrman3@ntc.zcu.cz>
2018-11-15 11:46:30 +0100

    update poisson_neumann.py example to compute boundary fluxes, change mesh

--- examples/diffusion/poisson_neumann.py
+++ examples/diffusion/poisson_neumann.py
@@ -1,31 +1,84 @@
 r"""
-Poisson equation with Neumann boundary conditions on a part of the boundary.
+The Poisson equation with Neumann boundary conditions on a part of the
+boundary.
 
-Find :math:`t` such that:
+Find :math:`T` such that:
 
 .. math::
-    \int_{\Omega} c \nabla s \cdot \nabla t
+    \int_{\Omega} K_{ij} \nabla_i s \nabla_j p T
     = \int_{\Gamma_N} s g
     \;, \quad \forall s \;,
 
-where :math:`g` is the given flux, :math:`g = \nabla T \cdot \ul{n}`. See the
+where :math:`g` is the given flux, :math:`g = \ul{n} \cdot K_{ij} \nabla_j
+\bar{T}`, and :math:`K_{ij} = c \delta_{ij}` (an isotropic medium). See the
 tutorial section :ref:`poisson-weak-form-tutorial` for a detailed explanation.
+
+The diffusion velocity and fluxes through various parts of the boundary are
+computed in the :func:`post_process()` function. On 'Gamma_N' (the Neumann
+condition boundary part), the flux/length should correspond to the given value
+:math:`g = -50`, while on 'Gamma_N0' the flux should be zero. Use the
+'refinement_level' option (see the usage examples below) to check the
+convergence of the numerical solution to those values. The total flux and the
+flux through 'Gamma_D' (the Dirichlet condition boundary part) are shown as
+well.
+
+Usage Examples
+--------------
+
+Run with the default settings (no refinement)::
+
+  python simple.py examples/diffusion/poisson_neumann.py
+
+Refine the mesh twice::
+
+  python simple.py examples/diffusion/poisson_neumann.py -O "'refinement_level' : 2"
 """
 from __future__ import absolute_import
+import numpy as nm
+
+from sfepy.base.base import output, Struct
 from sfepy import data_dir
 
-filename_mesh = data_dir + '/meshes/3d/cylinder.mesh'
+def post_process(out, pb, state, extend=False):
+    """
+    Calculate :math:`\nabla t` and compute boundary fluxes.
+    """
+    dv = pb.evaluate('ev_diffusion_velocity.i.Omega(m.K, t)', mode='el_avg',
+                     verbose=False)
+    out['dv'] = Struct(name='output_data', mode='cell',
+                       data=dv, dofs=None)
+
+    totals = nm.zeros(3)
+    for gamma in ['Gamma_N', 'Gamma_N0', 'Gamma_D']:
+
+        flux = pb.evaluate('d_surface_flux.i.%s(m.K, t)' % gamma,
+                           verbose=False)
+        area = pb.evaluate('d_surface.i.%s(t)' % gamma, verbose=False)
+
+        flux_data = (gamma, flux, area, flux / area)
+        totals += flux_data[1:]
+
+        output('%8s flux: % 8.3f length: % 8.3f flux/length: % 8.3f'
+               % flux_data)
+
+    totals[2] = totals[0] / totals[1]
+    output('   total flux: % 8.3f length: % 8.3f flux/length: % 8.3f'
+           % tuple(totals))
+
+    return out
+
+filename_mesh = data_dir + '/meshes/2d/cross-51-0.34.mesh'
 
 materials = {
     'flux' : ({'val' : -50.0},),
-    'coef' : ({'val' : 2.0},),
+    'm' : ({'K' : 2.7 * nm.eye(2)},),
 }
 
 regions = {
-    'Omega' : 'all', # or 'cells of group 6'
-    'Gamma_Left' : ('vertices in (x < 0.00001)', 'facet'),
-    'Gamma_Right' : ('vertices in (x > 0.099999)', 'facet'),
-    'Gamma_N' : ('vertices of surface -f (r.Gamma_Left +v r.Gamma_Right)',
+    'Omega' : 'all',
+    'Gamma_D' : ('vertices in (x < -0.4999)', 'facet'),
+    'Gamma_N0' : ('vertices in (y > 0.4999)', 'facet'),
+    'Gamma_N' : ('vertices of surface -s (r.Gamma_D +v r.Gamma_N0)',
                  'facet'),
 }
 
@@ -39,14 +92,17 @@ variables = {
 }
 
 ebcs = {
-    't1' : ('Gamma_Left', {'t.0' : 2.0}),
-    't2' : ('Gamma_Right', {'t.0' : -2.0}),
+    't1' : ('Gamma_D', {'t.0' : 5.3}),
+}
+
+integrals = {
+    'i' : 2
 }
 
 equations = {
     'Temperature' : """
-           dw_laplace.2.Omega(coef.val, s, t)
-         = dw_surface_integrate.2.Gamma_N(flux.val, s)
+           dw_diffusion.i.Omega(m.K, s, t)
+         = dw_surface_integrate.i.Gamma_N(flux.val, s)
     """
 }
 
@@ -61,4 +117,7 @@ solvers = {
 options = {
     'nls' : 'newton',
     'ls' : 'ls',
+
+    'refinement_level' : 0,
+    'post_process_hook' : 'post_process',
 }
