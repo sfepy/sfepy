fb8f700a895a37f0bda9b2929a7a5f701d155052
Tomáš Zítka <zitkat@students.zcu.cz>
2018-11-19 16:01:37 +0100

    Proper projection in set_dofs.
    Calculation of DOF should be fairly general now. It however uses nonstandard
    return values from polyspace._eval_base()

--- sfepy/discrete/dg/dg_basis.py
+++ sfepy/discrete/dg/dg_basis.py
@@ -37,14 +37,15 @@ class LegendrePolySpace(PolySpace):
 
     def _eval_base(self, coors, diff=0, ori=None,
                    suppress_errors=False, eps=1e-15):
+        # TODO this does not correspond to _eval_base of other basis
         if isinstance(coors, (int, float)):
-            n = 1
+            sh = (1,)
         else:
-            n = len(coors)
-        values = nm.ones((n, self.order + 1, 1))
-        values[:, 1] = coors
+            sh = nm.shape(coors)
+        values = nm.ones((self.order + 1,) + sh)
+        values[1, :] = coors
         for i in range(2, self.order + 1):
-            values[:, i] = ((2*i + 1) * coors * values[:, i-1] - i * values[:, i-2]) / (i + 1)
+            values[i, :] = ((2*i + 1) * coors * values[i-1, :] - i * values[i-2, :]) / (i + 1)
 
         return values
 
@@ -74,14 +75,14 @@ if __name__ == '__main__':
     bs = PrimitivePolySPace('primb', geometry, 5)
     vals = bs.eval_base(coors)
 
-    bs = LegendrePolySpace('legb', geometry, 5)
+    bs = LegendrePolySpace('legb', geometry, 2)
     Legvals = bs.eval_base(coors)
 
     # plt.figure("Primitive polyspace")
     # plt.plot(nm.linspace(-1, 1), vals[: ,: ,0])
 
     plt.figure("Legendre polyspace")
-    plt.plot(nm.linspace(-1, 1), Legvals[:, :, 0])
+    plt.plot(nm.linspace(-1, 1), Legvals[:, :, 0].T)
     plt.show()
     # geometry = Struct(n_vertex = 2,
     #              dim = 1,
