ddcdf33981ad0c060e18fd8763e53d169ceb4091
Tomáš Zítka <zitkat@students.zcu.cz>
2018-11-26 17:31:58 +0100

    Add custom time stepping solver DGTimeSteppingSolver and "step" solvers EulerStepSolver and RK3StepSolver inheriting from NonlinearSolver
    Custom TS solver ensures that nls is passes timestepper at each step;
    custom NL solvers solve serve as solution updaters, internally they solve only
    linear system.

--- sfepy/discrete/dg/example_dg_advection_1D.py
+++ sfepy/discrete/dg/example_dg_advection_1D.py
@@ -21,14 +21,14 @@ from sfepy.base.conf import ProblemConf
 # local import
 from dg_terms import AdvFluxDGTerm, AdvVolDGTerm
 # from dg_equation import Equation
-from dg_tssolver import TSSolver, RK3Solver, EUSolver
+from dg_tssolver import EulerStepSolver, DGTimeSteppingSolver, RK3StepSolver
 from dg_field import DGField
 
 from my_utils.inits_consts import left_par_q, gsmooth, const_u, ghump, superic
 from my_utils.visualizer import load_vtks, plot1D_DG_sol
 
-X1 = 0.
-XN1 = 1.
+X1 = -.5
+XN1 = .5
 n_nod = 100
 n_el = n_nod - 1
 coors = nm.linspace(X1, XN1, n_nod).reshape((n_nod, 1))
@@ -39,8 +39,9 @@ mesh = Mesh.from_data('advection_1d', coors, None,
                       [conn], [mat_ids], descs)
 
 t0 = 0
-t1 = 1
-tn = 10
+t1 = .08
+tn = 20
+speed = 1.0
 
 domain = FEDomain('domain', mesh)  # TODO DGDomain?
 # FEDomain contains default lagrange polyspace in its geometry
@@ -55,11 +56,11 @@ field = DGField('dgfu', nm.float64, 'scalar', omega, approx_order=1)
 # field = Field.from_args('fu', nm.float64, 'scalar', omega, approx_order=1)  # TODO DGField
 u = FieldVariable('u', 'unknown', field, history=1)
 v = FieldVariable('v', 'test', field, primary_var_name='u')
-integral = Integral('i', order=2)
+integral = Integral('i', order=1)
 
 IntT = AdvVolDGTerm(integral, omega, u=u, v=v)
 
-a = Material('a', val=[1.0])
+a = Material('a', val=[speed])
 FluxT = AdvFluxDGTerm(integral, omega, u=u, v=v, a=a)
 
 eq = Equation('balance', IntT + FluxT)
@@ -70,7 +71,7 @@ right_fix_u = EssentialBC('right_fix_u', right, {'u.all' : 0.0})
 
 
 def ic_wrap(x, ic=None):
-    return superic(x)
+    return ghump(x)
 
 
 ic_fun = Function('ic_fun', ic_wrap)
@@ -85,8 +86,23 @@ pb.set_ics(Conditions([ics]))
 ls = ScipyDirect({})
 nls_status = IndexedStruct()
 nls = Newton({'is_linear' : True}, lin_solver=ls, status=nls_status)
-tss = SimpleTimeSteppingSolver({'t0' : t0, 't1' : t1, 'n_step' : tn},
+nls = EulerStepSolver({}, lin_solver=ls, status=nls_status)
+nls = RK3StepSolver({}, lin_solver=ls, status=nls_status)
+
+
+dt = float(t1 - t0) / tn
+dx = nm.max(mesh.coors[1:] - mesh.coors[:-1])
+dtdx = dt / dx
+maxa = nm.max(nm.abs(speed))
+print("Space divided into {0} cells, {1} steps, step size is {2}".format(mesh.n_el, len(mesh.coors), dx))
+print("Time divided into {0} nodes, {1} steps, step size is {2}".format(tn - 1, tn, dt))
+print("Courant number c = max(abs(u)) * dt/dx = {0}".format(maxa * dtdx))
+
+tss = DGTimeSteppingSolver({'t0' : t0, 't1' : t1, 'n_step': tn},
                                nls=nls, context=pb, verbose=True)
+
+# tss = SimpleTimeSteppingSolver({'t0' : t0, 't1' : t1, 'n_step' : tn},
+#                                nls=nls, context=pb, verbose=True)
 pb.set_solver(tss)
 
 # pb.time_update(tss.ts)
@@ -96,5 +112,5 @@ pb.solve()
 #--------
 #| Plot |
 #--------
-lmesh, u = load_vtks(".", "domain", tn, 1)
-plot1D_DG_sol(lmesh, t0, t1, tn, u, ic_wrap)
+lmesh, u = load_vtks(".", "domain", tn, order=1, tns=20)
+plot1D_DG_sol(lmesh, t0, t1, u, ic_wrap, dt=dt,  tn=20,)
