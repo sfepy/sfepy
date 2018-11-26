c9f72fc118a1153a2cdfbc5696c8bce6342e3d88
Robert Cimrman <cimrman3@ntc.zcu.cz>
2018-11-26 14:14:28 +0100

    new tests/test_input_{boron,hydrogen,oscillator,well}.py

---
+++ tests/test_input_oscillator.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+input_name = '../examples/quantum/oscillator.py'
+output_name = 'test_oscillator.vtk'
+
+from tests_basic import TestInput
+
+class Test(TestInput):
+    pass
