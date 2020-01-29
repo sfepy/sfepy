6cef5a4970a159c849d171f5454b0d422e2263a9
Lubos Kejzlar <kejzlar@civ.zcu.cz>
2020-01-29 13:14:05 +0100

    Fixed long lines...

--- sfepy/base/multiproc.py
+++ sfepy/base/multiproc.py
@@ -10,13 +10,16 @@ except ImportError:
 try:
     import sys
     #
-    # Multiprocessing_proc implementation is currently broken on platforms using 'spawn' method as default.
+    # Multiprocessing_proc implementation is currently broken on platforms
+    # using 'spawn' method as default.
     #
-    # ToDo: we really need a real fix (Linux fork() method seems to be insecure/deprecated) !
+    # ToDo: we really need a real fix (Linux fork() method seems to be
+    #       insecure/deprecated) !
     #
     if sys.platform.startswith('win'):
         use_multiprocessing_proc = False
-    elif sys.platform == 'darwin' and sys.version_info.major == 3 and sys.version_info.minor == 8:
+    elif sys.platform == 'darwin' and \
+        sys.version_info.major == 3 and sys.version_info.minor == 8:
         use_multiprocessing_proc = False
     else:
         from multiprocessing import cpu_count
