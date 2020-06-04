eda40bf66e8f135c604564c1504e3ec27dc0b1a4
Tomáš Zítka <zitkat@students.zcu.cz>
2020-06-04 19:50:21 +0200

    Add some DG tests based on examples.

---
+++ tests/test_dg_input_laplace.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+input_name = '../examples/dg/diffusion/example_dg_laplace2D.py'
+output_name = 'laplace_sol.msh'
+
+from tests_basic import TestInput
+class Test( TestInput ):
+    pass
---
+++ tests/test_dg_input_quarteroni.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+input_name = '../examples/dg/advection/example_dg_quarteroni1.py'
+output_name = 'quarteroni1_sol.msh'
+
+from tests_basic import TestInput
+class Test( TestInput ):
+    pass
