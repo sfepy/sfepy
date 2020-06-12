f9f4662d9c4f8246a9ac8bda1400146dd57d7b4e
Robert Cimrman <cimrman3@ntc.zcu.cz>
2020-06-12 18:10:10 +0200

    guard memory use in VolumeMapping.get_mapping()
    - the problem cannot be solved if cmap.bfg takes the whole memory
    - IGMapping not treated due to limited use (single patch support only)

--- sfepy/discrete/fem/mappings.py
+++ sfepy/discrete/fem/mappings.py
@@ -4,6 +4,7 @@ Finite element reference mappings.
 import numpy as nm
 
 from sfepy.base.base import get_default, output
+from sfepy.base.mem_usage import raise_if_too_large
 from sfepy.discrete.common.mappings import Mapping
 from sfepy.discrete.common.extmods.mappings import CMapping
 from sfepy.discrete.fem.poly_spaces import PolySpace
@@ -92,6 +93,9 @@ class VolumeMapping(FEMapping):
 
         ebf_g = poly_space.eval_base(qp_coors, diff=True, ori=ori,
                                      force_axis=True, transform=transform)
+        size = ebf_g.nbytes * self.n_el
+        raise_if_too_large(size, 2)
+
         flag = (ori is not None) or (ebf_g.shape[0] > 1)
 
         cmap = CMapping(self.n_el, qp_coors.shape[0], self.dim,
