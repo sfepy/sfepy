84a81818aacf95574639bcd95d609fe2e31e3a18
Robert Cimrman <cimrman3@ntc.zcu.cz>
2022-04-11 11:22:27 +0200

    update test_base.py for pytest, clean up

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,172 +1,153 @@
-from __future__ import absolute_import
 from sfepy.base.base import assert_
-from sfepy.base.testing import TestCommon
 
-##
-# 28.08.2007, c
-class Test( TestCommon ):
+def test_struct_add():
+    from sfepy.base.base import Struct
+    from copy import deepcopy
 
-    ##
-    # 28.08.2007, c
-    def from_conf( conf, options ):
-        return Test( conf = conf, options = options )
-    from_conf = staticmethod( from_conf )
+    a = Struct(f1=0,
+               f2=[1, 2, 3],
+               f3=Struct(ff='abc'),
+               f4=3.14)
+    a0 = deepcopy(a)
+    b = Struct(f1=5,
+               f2=[1],
+               f3=Struct(ff='', gg=123),
+               f5='new one')
+    c = a + b
 
-    ##
-    # 28.08.2007, c
-    def test_struct_add( self ):
-        from sfepy.base.base import Struct
-        from copy import deepcopy
+    assert_(c.f1 == 0)
+    assert_(c.f2 == [1, 2, 3])
+    assert_(c.f3.ff == 'abc')
+    assert_(c.f3.gg == 123)
+    assert_(c.f4 == 3.14)
+    assert_(c.f5 == 'new one')
 
-        a = Struct( f1 = 0,
-                    f2 = [1, 2, 3],
-                    f3 = Struct( ff = 'abc' ),
-                    f4 = 3.14 )
-        a0 = deepcopy( a )
-        b = Struct( f1 = 5,
-                    f2 = [1],
-                    f3 = Struct( ff = '', gg = 123 ),
-                    f5 = 'new one' )
-        c = a + b
+    assert_(a.f1 == a0.f1)
+    assert_(a.f2 == a0.f2)
+    assert_(a.f3.ff == a0.f3.ff)
+    assert_(a.f4 == a0.f4)
 
-        assert_( c.f1 == 0 )
-        assert_( c.f2 == [1, 2, 3] )
-        assert_( c.f3.ff == 'abc' )
-        assert_( c.f3.gg == 123 )
-        assert_( c.f4 == 3.14 )
-        assert_( c.f5 == 'new one' )
+    return True
 
-        assert_( a.f1 == a0.f1 )
-        assert_( a.f2 == a0.f2 )
-        assert_( a.f3.ff == a0.f3.ff )
-        assert_( a.f4 == a0.f4 )
+def test_struct_i_add():
+    from sfepy.base.base import Struct
 
-        return True
+    a = Struct(f1=0,
+               f2=[1, 2, 3],
+               f3=Struct(ff='abc'))
+    b = Struct(f1=5,
+               f2=[1],
+               f3=Struct(ff='', gg=123),
+               f4='new one')
+    a += b
 
-    ##
-    # 28.08.2007, c
-    def test_struct_i_add( self ):
-        from sfepy.base.base import Struct
+    assert_(a.f1 == 0)
+    assert_(a.f2 == [1, 2, 3])
+    assert_(a.f3.ff == 'abc')
+    assert_(a.f3.gg == 123)
+    assert_(a.f4 == 'new one')
 
-        a = Struct( f1 = 0,
-                    f2 = [1, 2, 3],
-                    f3 = Struct( ff = 'abc' ) )
-        b = Struct( f1 = 5,
-                    f2 = [1],
-                    f3 = Struct( ff = '', gg = 123 ),
-                    f4 = 'new one' )
-        a += b
+    return True
 
-        assert_( a.f1 == 0 )
-        assert_( a.f2 == [1, 2, 3] )
-        assert_( a.f3.ff == 'abc' )
-        assert_( a.f3.gg == 123 )
-        assert_( a.f4 == 'new one' )
+def test_container_add():
+    from sfepy.base.base import Struct, Container
 
-        return True
+    a = Struct(name='a')
+    b = Struct(name='b')
 
-    def test_container_add(self):
-        from sfepy.base.base import Struct, Container
+    c1 = Container()
 
-        a = Struct(name='a')
-        b = Struct(name='b')
+    c1 = c1 + c1
+    assert_(c1.names == [])
 
-        c1 = Container()
+    c1 += Container([a, b])
+    assert_(c1.names == ['a', 'b'])
 
-        c1 = c1 + c1
-        assert_(c1.names == [])
+    c2 = c1 + c1
+    assert_(c2.names == 2 * ['a', 'b'])
 
-        c1 += Container([a, b])
-        assert_(c1.names == ['a', 'b'])
+    c2 += c2
+    assert_(c2.names == 4 * ['a', 'b'])
 
-        c2 = c1 + c1
-        assert_(c2.names == 2 * ['a', 'b'])
+    return True
 
-        c2 += c2
-        assert_(c2.names == 4 * ['a', 'b'])
+def test_verbose_output():
+    from io import StringIO
 
-        return True
+    from sfepy.base.base import Output, goptions
 
-    def test_verbose_output(self):
-        try:
-            from StringIO import StringIO  # Python 2
-        except ImportError:
-            from io import StringIO        # Python 3
+    fd = StringIO()
 
-        from sfepy.base.base import Output, goptions
+    output = Output('test', filename=fd)
 
-        fd = StringIO()
+    output('test1')
+    goptions['verbose'] = False
+    output('test2')
+    goptions['verbose'] = 1
+    output('test3')
 
-        output = Output('test', filename=fd)
+    _ok1 = bool(goptions['verbose'])
+    _ok2 = fd.getvalue() == 'test test1\ntest test3\n'
 
-        output('test1')
-        goptions['verbose'] = False
-        output('test2')
-        goptions['verbose'] = 1
-        output('test3')
+    fd.close()
 
-        _ok1 = bool(goptions['verbose'])
-        _ok2 = fd.getvalue() == 'test test1\ntest test3\n'
+    ok = _ok1 and _ok2
 
-        fd.close()
+    return ok
 
-        ok = _ok1 and _ok2
+def test_resolve_deps():
+    from sfepy.base.resolve_deps import resolve
 
-        return ok
+    deps = {
+        'a' : ['a', 'b'],
+        'b' : ['a', 'b'],
+        'c' : ['b', 'c', 'd', 'e'],
+        'd' : ['c', 'e'],
+        'e' : ['d', 'e'],
+        'f' : ['e', 'f', 'g'],
+        'g' : ['g'],
+    }
+    order = resolve(deps)
+    ok1 = order == [['g'], ['a', 'b'], ['c', 'd', 'e'], ['f']]
 
-    def test_resolve_deps(self):
-        from sfepy.base.resolve_deps import resolve
+    deps = {
+        'a' : ['b'],
+        'b' : ['c'],
+        'c' : ['a'],
+    }
+    order = resolve(deps)
+    ok2 = order == [['a', 'b', 'c']]
 
-        deps = {
-            'a' : ['a', 'b'],
-            'b' : ['a', 'b'],
-            'c' : ['b', 'c', 'd', 'e'],
-            'd' : ['c', 'e'],
-            'e' : ['d', 'e'],
-            'f' : ['e', 'f', 'g'],
-            'g' : ['g'],
-        }
-        order = resolve(deps)
-        ok1 = order == [['g'], ['a', 'b'], ['c', 'd', 'e'], ['f']]
+    return ok1 and ok2
 
-        deps = {
-            'a' : ['b'],
-            'b' : ['c'],
-            'c' : ['a'],
-        }
-        order = resolve(deps)
-        ok2 = order == [['a', 'b', 'c']]
+def test_parse_conf():
+    from sfepy.base.parse_conf import list_dict
+    from sfepy.base.conf import dict_from_string as parse_dict
+    ld = list_dict()
 
-        return ok1 and ok2
+    def parse(x):
+        out = ld.parseString(x, True)[0]
+        return out
 
-    def test_parse_conf(self):
-        from sfepy.base.parse_conf import list_dict
-        from sfepy.base.conf import dict_from_string as parse_dict
-        ld = list_dict()
-
-        def parse(x):
-            out = ld.parseString(x, True)[0]
-            return out
-
-        assert_(parse("1,2") == ([1,2],{}))
-        assert_(parse("1,[2]") == ([1,[2]],{}))
-        assert_(parse("1,[2,4],c=3") == ([1,[2,4]],{'c':3}))
-        assert_(parse("1,(2),c:3,uu=7") == ([1,(2,)],{'c':3,'uu':7}))
-        assert_(parse("'long string ([\"',(2,5),c:3") ==
-                     (['long string (["',(2,5)],{'c':3}))
+    assert_(parse("1,2") == ([1,2],{}))
+    assert_(parse("1,[2]") == ([1,[2]],{}))
+    assert_(parse("1,[2,4],c=3") == ([1,[2,4]],{'c':3}))
+    assert_(parse("1,(2),c:3,uu=7") == ([1,(2,)],{'c':3,'uu':7}))
+    assert_(parse("'long string ([\"',(2,5),c:3") ==
+                 (['long string (["',(2,5)],{'c':3}))
 
 
-        assert_(parse_dict('') == {})
-        assert_(parse_dict('a:[]') == {'a':[]})
-        assert_(parse_dict('a:{}') == {'a':{}})
-        assert_(parse_dict('1:2,a:{},3:4') == {1:2,'a':{},3:4})
+    assert_(parse_dict('') == {})
+    assert_(parse_dict('a:[]') == {'a':[]})
+    assert_(parse_dict('a:{}') == {'a':{}})
+    assert_(parse_dict('1:2,a:{},3:4') == {1:2,'a':{},3:4})
 
-        assert_(parse('') == ([],{}))
-        assert_(parse('[ ]') == ([[]],{}))
-        assert_(parse('[]') == ([[]],{}))
-        assert_(parse('[[]]') == ([[[]]],{}))
-        assert_(parse('[[[]]]')==([[[[]]]],{}))
-        assert_(parse('a,{},[],None,True,False,"False"') ==
-                         (['a',{},[],None,True,False,"False"],{}))
+    assert_(parse('') == ([],{}))
+    assert_(parse('[ ]') == ([[]],{}))
+    assert_(parse('[]') == ([[]],{}))
+    assert_(parse('[[]]') == ([[[]]],{}))
+    assert_(parse('[[[]]]')==([[[[]]]],{}))
+    assert_(parse('a,{},[],None,True,False,"False"') ==
+                     (['a',{},[],None,True,False,"False"],{}))
 
-        return True
+    return True
