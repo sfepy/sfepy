cc401742058c34d57da304d0432944d7881fceef
Robert Cimrman <cimrman3@ntc.zcu.cz>
2022-04-20 22:34:36 +0200

    update test_quadratures.py for pytest

--- tests/test_quadratures.py
+++ tests/test_quadratures.py
@@ -1,14 +1,12 @@
-from __future__ import absolute_import
 import numpy as nm
-from six.moves import range
 
 try:
     import sympy as sm
 except ImportError:
     sm = None
 
-from sfepy.base.testing import TestCommon
 from sfepy.base.base import assert_, ordered_iteritems
+import sfepy.base.testing as tst
 
 def symarray(prefix, shape):
     """
@@ -50,113 +48,104 @@ def get_poly(order, dim, is_simplex=False):
 
     return xs, poly, limits, integral
 
-class Test(TestCommon):
+def test_weight_consistency():
+    """
+    Test if integral of 1 (= sum of weights) gives the domain volume.
+    """
+    from sfepy.discrete.quadratures import quadrature_tables
 
-    @staticmethod
-    def from_conf( conf, options ):
+    ok = True
+    for geometry, qps in ordered_iteritems(quadrature_tables):
+        tst.report('geometry:', geometry)
+        for order, qp in ordered_iteritems(qps):
+            diff = nm.abs(qp.weights.sum() - qp.volume)
+            _ok = diff < 1e-14
+            tst.report('order %d: %s (%.2e)' % (order, _ok, diff))
 
-        test = Test(conf=conf, options=options)
-        return test
+            ok = ok and _ok
 
-    def test_weight_consistency(self):
-        """
-        Test if integral of 1 (= sum of weights) gives the domain volume.
-        """
-        from sfepy.discrete.quadratures import quadrature_tables
+    assert_(ok)
 
-        ok = True
-        for geometry, qps in ordered_iteritems(quadrature_tables):
-            self.report('geometry:', geometry)
-            for order, qp in ordered_iteritems(qps):
-                diff = nm.abs(qp.weights.sum() - qp.volume)
-                _ok = diff < 1e-14
-                self.report('order %d: %s (%.2e)' % (order, _ok, diff))
+def _test_quadratures(quad, geometry, order):
+    dim = int(geometry[0])
+    n_v = int(geometry[2])
+    is_simplex = n_v == (dim + 1)
 
-                ok = ok and _ok
+    porder = order if is_simplex else dim * order
 
-        return ok
+    xs, poly, limits, integral = get_poly(porder, dim,
+                                          is_simplex=is_simplex)
 
-    def test_quadratures(self):
-        """
-        Test if the quadratures have orders they claim to have, using
-        symbolic integration by sympy.
-        """
-        from sfepy.discrete.quadratures import quadrature_tables, tp_geometries
-        from sfepy.discrete.integrals import Integral
+    tst.report('  polynomial:', poly)
+    tst.report('  limits:', limits)
+    tst.report('  integral:', integral)
 
-        if sm is None:
-            self.report('cannot import sympy, skipping')
-            return True
+    def fun(coors):
+        vals = nm.empty(coors.shape[0], dtype=nm.float64)
 
-        quad = Integral('test_integral')
+        subs = {}
+        for ir, cc in enumerate(coors):
+            for ic, x in enumerate(xs):
+                subs[x] = coors[ir,ic]
+            vals[ir] = float(poly.subs(subs))
 
-        ok = True
-        failed = []
-        for geometry, qps in ordered_iteritems(quadrature_tables):
-            self.report('geometry:', geometry)
+        return vals
 
-            if geometry == '0_1':
-                continue
+    val = quad.integrate(fun, order=order, geometry=geometry)
+    ok = nm.allclose(val, float(integral), rtol=0.0, atol=1e-11)
 
-            elif geometry in tp_geometries:
-                iter_qp = range(1, 11)
+    tst.report('  sym. == num.: %f == %f -> %s' %
+               (float(integral), val, ok))
 
-            elif geometry == '2_3':
-                iter_qp = range(1, 25)
+    return ok, integral, val
 
-            elif geometry == '3_4':
-                iter_qp = range(1, 12)
+def test_quadratures():
+    """
+    Test if the quadratures have orders they claim to have, using
+    symbolic integration by sympy.
+    """
+    from sfepy.discrete.quadratures import quadrature_tables, tp_geometries
+    from sfepy.discrete.integrals import Integral
 
-            else:
-                iter_qp = sorted(qps.keys())
+    if sm is None:
+        tst.report('cannot import sympy, skipping')
+        return
 
-            for order in iter_qp:
-                self.report('order:', order)
+    quad = Integral('test_integral')
 
-                aux = self._test_quadratures(quad, geometry, order)
-                _ok, integral, val = aux
+    ok = True
+    failed = []
+    for geometry, qps in ordered_iteritems(quadrature_tables):
+        tst.report('geometry:', geometry)
 
-                if not _ok:
-                    failed.append((geometry, order, float(integral), val))
+        if geometry == '0_1':
+            continue
 
-                ok = ok and _ok
+        elif geometry in tp_geometries:
+            iter_qp = range(1, 11)
 
-        if not ok:
-            self.report('failed:')
-            for aux in failed:
-                self.report(aux, '%.1e' % abs(aux[2] - aux[3]))
+        elif geometry == '2_3':
+            iter_qp = range(1, 25)
 
-        return ok
+        elif geometry == '3_4':
+            iter_qp = range(1, 12)
 
-    def _test_quadratures(self, quad, geometry, order):
-        dim = int(geometry[0])
-        n_v = int(geometry[2])
-        is_simplex = n_v == (dim + 1)
+        else:
+            iter_qp = sorted(qps.keys())
 
-        porder = order if is_simplex else dim * order
+        for order in iter_qp:
+            tst.report('order:', order)
 
-        xs, poly, limits, integral = get_poly(porder, dim,
-                                              is_simplex=is_simplex)
+            _ok, integral, val = _test_quadratures(quad, geometry, order)
 
-        self.report('  polynomial:', poly)
-        self.report('  limits:', limits)
-        self.report('  integral:', integral)
+            if not _ok:
+                failed.append((geometry, order, float(integral), val))
 
-        def fun(coors):
-            vals = nm.empty(coors.shape[0], dtype=nm.float64)
+            ok = ok and _ok
 
-            subs = {}
-            for ir, cc in enumerate(coors):
-                for ic, x in enumerate(xs):
-                    subs[x] = coors[ir,ic]
-                vals[ir] = float(poly.subs(subs))
+    if not ok:
+        tst.report('failed:')
+        for aux in failed:
+            tst.report(aux, '%.1e' % abs(aux[2] - aux[3]))
 
-            return vals
-
-        val = quad.integrate(fun, order=order, geometry=geometry)
-        ok = nm.allclose(val, float(integral), rtol=0.0, atol=1e-11)
-
-        self.report('  sym. == num.: %f == %f -> %s' %
-                    (float(integral), val, ok))
-
-        return ok, integral, val
+    assert_(ok)
