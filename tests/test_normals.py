6401758678d3a609df99abeb29340d7bb4df39b3
Robert Cimrman <cimrman3@ntc.zcu.cz>
2022-04-20 19:32:25 +0200

    update test_normals.py for pytest

--- tests/test_normals.py
+++ tests/test_normals.py
@@ -1,7 +1,6 @@
-from __future__ import absolute_import
 import numpy as nm
 
-from sfepy.base.testing import TestCommon
+import sfepy.base.testing as tst
 
 expected_normals = { # Need to be normalized!
     '2_3' : nm.array([[ 0, -1],
@@ -23,59 +22,53 @@ expected_normals = { # Need to be normalized!
                       [ 0,  1,  0]], dtype=nm.float64),
 }
 
-class Test(TestCommon):
+def test_normals():
+    """
+    Check orientations of surface normals on the reference elements.
+    """
+    import sfepy
+    from sfepy.discrete import Integral, PolySpace
+    from sfepy.discrete.fem import Mesh, FEDomain
+    from sfepy.discrete.fem.mappings import SurfaceMapping
+    from sfepy.linalg import normalize_vectors
 
-    @staticmethod
-    def from_conf(conf, options):
-        return Test(conf=conf, options=options)
+    ok = True
 
-    def test_normals(self):
-        """
-        Check orientations of surface normals on the reference elements.
-        """
-        import sfepy
-        from sfepy.discrete import Integral, PolySpace
-        from sfepy.discrete.fem import Mesh, FEDomain
-        from sfepy.discrete.fem.mappings import SurfaceMapping
-        from sfepy.linalg import normalize_vectors
+    for geom in ['2_3', '2_4', '3_4', '3_8']:
+        mesh = Mesh.from_file('meshes/elements/%s_1.mesh' % geom,
+                              prefix_dir=sfepy.data_dir)
+        domain = FEDomain('domain', mesh)
+        surface = domain.create_region('Surface', 'vertices of surface',
+                                       'facet')
+        domain.create_surface_group(surface)
 
-        ok = True
+        sd = domain.surface_groups[surface.name]
 
-        for geom in ['2_3', '2_4', '3_4', '3_8']:
-            mesh = Mesh.from_file('meshes/elements/%s_1.mesh' % geom,
-                                  prefix_dir=sfepy.data_dir)
-            domain = FEDomain('domain', mesh)
-            surface = domain.create_region('Surface', 'vertices of surface',
-                                           'facet')
-            domain.create_surface_group(surface)
+        coors = domain.get_mesh_coors()
+        gel = domain.geom_els[geom].surface_facet
+        ps = PolySpace.any_from_args('aux', gel, 1)
 
-            sd = domain.surface_groups[surface.name]
+        mapping = SurfaceMapping(coors, sd.get_connectivity(), ps)
 
-            coors = domain.get_mesh_coors()
-            gel = domain.geom_els[geom].surface_facet
-            ps = PolySpace.any_from_args('aux', gel, 1)
+        integral = Integral('i', order=1)
+        vals, weights = integral.get_qp(gel.name)
 
-            mapping = SurfaceMapping(coors, sd.get_connectivity(), ps)
+        # Evaluate just in the first quadrature point...
+        geo = mapping.get_mapping(vals[:1], weights[:1])
 
-            integral = Integral('i', order=1)
-            vals, weights = integral.get_qp(gel.name)
+        expected = expected_normals[geom].copy()
+        normalize_vectors(expected)
 
-            # Evaluate just in the first quadrature point...
-            geo = mapping.get_mapping(vals[:1], weights[:1])
+        _ok = nm.allclose(expected, geo.normal[:, 0, :, 0],
+                          rtol=0.0, atol=1e-14)
+        tst.report('%s: %s' % (geom, _ok))
 
-            expected = expected_normals[geom].copy()
-            normalize_vectors(expected)
+        if not _ok:
+            tst.report('expected:')
+            tst.report(expected)
+            tst.report('actual:')
+            tst.report(geo.normal[:, 0, :, 0])
 
-            _ok = nm.allclose(expected, geo.normal[:, 0, :, 0],
-                              rtol=0.0, atol=1e-14)
-            self.report('%s: %s' % (geom, _ok))
+        ok = ok and _ok
 
-            if not _ok:
-                self.report('expected:')
-                self.report(expected)
-                self.report('actual:')
-                self.report(geo.normal[:, 0, :, 0])
-
-            ok = ok and _ok
-
-        return ok
+    assert ok
