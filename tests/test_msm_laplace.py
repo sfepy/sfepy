7f88f57bd937e751387b8f593831eb23f61344d2
Robert Cimrman <cimrman3@ntc.zcu.cz>
2022-04-20 18:07:36 +0200

    update test_msm_laplace.py for pytest, clean up

--- tests/test_msm_laplace.py
+++ tests/test_msm_laplace.py
@@ -1,7 +1,9 @@
-# c: 07.05.2007, r: 25.06.2008
-from __future__ import absolute_import
+import numpy as nm
+import pytest
+
+from sfepy.base.base import assert_
 from sfepy import data_dir
-import six
+import sfepy.base.testing as tst
 
 filename_mesh = data_dir + '/meshes/2d/special/circle_in_square.mesh'
 
@@ -42,16 +44,17 @@ materials = {
 
 equations = {
     'Temperature' :
-    """dw_laplace.i.Omega( coef.val, s, t )
-       = - dw_volume_lvf.i.Omega( rhs.val, s )""",
+    """dw_laplace.i.Omega(coef.val, s, t)
+       = - dw_volume_lvf.i.Omega(rhs.val, s)""",
 }
 
 solutions = {
-    'sincos' : ('t', 'sin( 3.0 * x ) * cos( 4.0 * y )',
-                '-25.0 * %s * sin( 3.0 * x ) * cos( 4.0 * y )' % coef),
+    'sincos' : ('t', 'sin(3.0 * x) * cos(4.0 * y)',
+                '-25.0 * %s * sin(3.0 * x) * cos(4.0 * y)' % coef),
     'poly' : ('t', '(x**2) + (y**2)', '4.0 * %s' % coef),
-    'polysin' : ('t', '((x - 0.5)**3) * sin( 5.0 * y )',
-                 '%s * (6.0 * (x - 0.5) * sin( 5.0 * y ) - 25.0 * ((x - 0.5)**3) * sin( 5.0 * y ))' % coef),
+    'polysin' : ('t', '((x - 0.5)**3) * sin(5.0 * y)',
+                 '%s * (6.0 * (x - 0.5) * sin(5.0 * y)'
+                 ' - 25.0 * ((x - 0.5)**3) * sin(5.0 * y))' % coef),
 }
 
 solver_0 = {
@@ -67,25 +70,18 @@ solver_1 = {
     'eps_a'      : 1e-10,
 }
 
-import numpy as nm
-from sfepy.base.testing import TestCommon
-from sfepy.base.base import debug, pause, assert_
-output_name = 'test_msm_laplace_%s.vtk'
-
-##
-# c: 07.05.2007, r: 09.05.2008
 solution = ['']
 def ebc(ts, coor, **kwargs):
     expression = solution[0]
-    val = TestCommon.eval_coor_expression( expression, coor )
-    return nm.atleast_1d( val )
+    val = tst.eval_coor_expression(expression, coor)
+    return nm.atleast_1d(val)
 
 def rhs(ts, coor, mode=None, expression=None, **kwargs):
     if mode == 'qp':
         if expression is None:
             expression = '0.0 * x'
 
-        val = TestCommon.eval_coor_expression( expression, coor )
+        val = tst.eval_coor_expression(expression, coor)
         val.shape = (val.shape[0], 1, 1)
 
         return {'val' : val}
@@ -95,74 +91,65 @@ functions = {
     'rhs' : (rhs,),
 }
 
-##
-# c: 07.05.2008
-class Test( TestCommon ):
+@pytest.fixture(scope='module')
+def problem():
+    import sys
+    from sfepy.discrete import Problem
+    from sfepy.base.conf import ProblemConf
 
-    ##
-    # c: 07.05.2007, r: 07.05.2008
-    def from_conf( conf, options ):
-        from sfepy.discrete import Problem
+    conf = ProblemConf.from_dict(globals(), sys.modules[__name__])
 
-        problem = Problem.from_conf(conf)
-        test = Test( problem = problem,
-                     conf = conf, options = options )
-        return test
-    from_conf = staticmethod( from_conf )
+    problem = Problem.from_conf(conf)
+    return problem
 
-    ##
-    # c: 09.05.2007, r: 25.06.2008
-    def _build_rhs( self, sols ):
-        for sol in six.itervalues(sols):
-            assert_( len( sol ) == 3 )
-        return sols
+def _build_rhs(sols):
+    for sol in sols.values():
+        assert_(len(sol) == 3)
+    return sols
 
-    ##
-    # c: 07.05.2007, r: 09.05.2008
-    def test_msm_laplace( self ):
-        import os.path as op
-        problem  = self.problem
+def test_msm_laplace(problem, output_dir):
+    import os.path as op
 
-        variables = problem.get_variables()
-        materials = problem.get_materials()
+    variables = problem.get_variables()
+    materials = problem.get_materials()
 
-        sols = self._build_rhs( self.conf.solutions )
+    sols = _build_rhs(solutions)
 
-        ok = True
-        for sol_name, sol in six.iteritems(sols):
-            self.report( 'testing', sol_name )
-            var_name, sol_expr, rhs_expr = sol
+    ok = True
+    for sol_name, sol in sols.items():
+        tst.report('testing', sol_name)
+        var_name, sol_expr, rhs_expr=sol
 
-            self.report( 'sol:', sol_expr )
-            self.report( 'rhs:', rhs_expr )
-            globals()['solution'][0] = sol_expr
-            materials['rhs'].function.set_extra_args(expression=rhs_expr)
-            problem.time_update()
-            state = problem.solve()
-            coor = variables[var_name].field.get_coor()
-            ana_sol = self.eval_coor_expression( sol_expr, coor )
-            num_sol = state(var_name)
+        tst.report('sol:', sol_expr)
+        tst.report('rhs:', rhs_expr)
+        globals()['solution'][0] = sol_expr
+        materials['rhs'].function.set_extra_args(expression=rhs_expr)
+        problem.time_update()
+        state = problem.solve(save_results=False)
+        coor = variables[var_name].field.get_coor()
+        ana_sol = tst.eval_coor_expression(sol_expr, coor)
+        num_sol = state(var_name)
 
-            ana_norm = nm.linalg.norm( ana_sol, nm.inf )
-            ret = self.compare_vectors( ana_sol, num_sol,
-                                       allowed_error = ana_norm * 1e-2,
-                                       label1 = 'analytical %s' % var_name,
-                                       label2 = 'numerical %s' % var_name,
-                                       norm = nm.inf )
-            if not ret:
-                self.report( 'variable %s: failed' % var_name )
+        ana_norm = nm.linalg.norm(ana_sol, nm.inf)
+        ret = tst.compare_vectors(ana_sol, num_sol,
+                                  allowed_error=ana_norm * 1e-2,
+                                  label1='analytical %s' % var_name,
+                                  label2='numerical %s' % var_name,
+                                  norm=nm.inf)
+        if not ret:
+            tst.report('variable %s: failed' % var_name)
 
-            fname = op.join( self.options.out_dir, self.conf.output_name )
-            out = {}
-            astate = state.copy()
-            astate.set_state(ana_sol)
-            aux = astate.create_output()
-            out['ana_t'] = aux['t']
-            aux = state.create_output()
-            out['num_t'] = aux['t']
+        fname = op.join(output_dir, 'test_msm_laplace_%s.vtk')
+        out = {}
+        astate = state.copy()
+        astate.set_state(ana_sol)
+        aux = astate.create_output()
+        out['ana_t'] = aux['t']
+        aux = state.create_output()
+        out['num_t'] = aux['t']
 
-            problem.domain.mesh.write( fname % sol_name, io = 'auto', out = out )
+        problem.domain.mesh.write(fname % sol_name, io='auto', out=out)
 
-            ok = ok and ret
+        ok = ok and ret
 
-        return ok
+    assert_(ok)
