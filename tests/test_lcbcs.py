6569a3cf9f483ee474d88c6cdeb98cd679a6b3dc
Robert Cimrman <cimrman3@ntc.zcu.cz>
2022-04-20 10:59:00 +0200

    update test_lcbcs.py for pytest, use output_dir for all output

--- tests/test_lcbcs.py
+++ tests/test_lcbcs.py
@@ -1,107 +1,103 @@
-from __future__ import absolute_import
+import os.path as op
 import numpy as nm
+import pytest
 
-from sfepy.base.testing import TestCommon
+import sfepy.base.testing as tst
 
-class Test(TestCommon):
+def test_stokes_slip_bc(output_dir):
+    import scipy.sparse as sp
 
-    @staticmethod
-    def from_conf(conf, options):
-        test = Test(conf=conf, options=options)
-        return test
+    from sfepy.base.conf import ProblemConf
+    from sfepy.discrete import Problem
+    import examples.navier_stokes.stokes_slip_bc as ssb
 
-    def test_stokes_slip_bc(self):
-        import scipy.sparse as sp
+    conf = ProblemConf.from_dict(ssb.define(output_dir=output_dir), ssb)
+    pb = Problem.from_conf(conf, init_solvers=False)
 
-        from sfepy.base.conf import ProblemConf
-        from sfepy.discrete import Problem
-        import examples.navier_stokes.stokes_slip_bc as ssb
+    pb.time_update()
+    variables = pb.get_variables()
 
-        conf = ProblemConf.from_dict(ssb.define(), ssb)
-        pb = Problem.from_conf(conf, init_solvers=False)
-        pb.time_update()
-        variables = pb.get_variables()
+    adi = variables.adi
+    lcdi = variables.lcdi
+    mtx = variables.mtx_lcbc
 
-        adi = variables.adi
-        lcdi = variables.lcdi
-        mtx = variables.mtx_lcbc
+    ok = adi.var_names == lcdi.var_names
+    tst.report('same adi-lcdi ordering:', ok)
 
-        ok = adi.var_names == lcdi.var_names
-        self.report('same adi-lcdi ordering:', ok)
+    ublock = mtx[adi.indx['u']]
+    ir, ic = ublock.nonzero()
+    ir += adi.indx['u'].start
 
-        ublock = mtx[adi.indx['u']]
-        ir, ic = ublock.nonzero()
-        ir += adi.indx['u'].start
+    i0, i1 = adi.indx['u'].start, adi.indx['u'].stop
+    _ok0 = (i0 <= ir).all() and  (ir < i1).all()
+    tst.report('u block rows in [%d %d[: %s' % (i0, i1, _ok0))
 
-        i0, i1 = adi.indx['u'].start, adi.indx['u'].stop
-        _ok0 = (i0 <= ir).all() and  (ir < i1).all()
-        self.report('u block rows in [%d %d[: %s' % (i0, i1, _ok0))
+    i0, i1 = lcdi.indx['u'].start, lcdi.indx['u'].stop
+    _ok1 = (i0 <= ic).all() and  (ic < i1).all()
+    tst.report('u block cols in [%d %d[: %s' % (i0, i1, _ok1))
 
-        i0, i1 = lcdi.indx['u'].start, lcdi.indx['u'].stop
-        _ok1 = (i0 <= ic).all() and  (ic < i1).all()
-        self.report('u block cols in [%d %d[: %s' % (i0, i1, _ok1))
+    ok = ok and _ok0 and _ok1
 
-        ok = ok and _ok0 and _ok1
+    pblock = mtx[adi.indx['p']]
+    ir, ic, iv = sp.find(pblock)
+    ir += adi.indx['p'].start
 
-        pblock = mtx[adi.indx['p']]
-        ir, ic, iv = sp.find(pblock)
-        ir += adi.indx['p'].start
+    i0, i1 = adi.indx['p'].start, adi.indx['p'].stop
+    _ok0 = (i0 <= ir).all() and  (ir < i1).all()
+    tst.report('p block rows in [%d %d[: %s' % (i0, i1, _ok0))
 
-        i0, i1 = adi.indx['p'].start, adi.indx['p'].stop
-        _ok0 = (i0 <= ir).all() and  (ir < i1).all()
-        self.report('p block rows in [%d %d[: %s' % (i0, i1, _ok0))
+    i0, i1 = lcdi.indx['p'].start, lcdi.indx['p'].stop
+    _ok1 = (i0 <= ic).all() and  (ic < i1).all()
+    tst.report('p block cols in [%d %d[: %s' % (i0, i1, _ok1))
 
-        i0, i1 = lcdi.indx['p'].start, lcdi.indx['p'].stop
-        _ok1 = (i0 <= ic).all() and  (ic < i1).all()
-        self.report('p block cols in [%d %d[: %s' % (i0, i1, _ok1))
+    ok = ok and _ok0 and _ok1
 
-        ok = ok and _ok0 and _ok1
+    _ok0 = (len(ir) == adi.n_dof['p'])
+    tst.report('p block size correct:', _ok0)
+    _ok1 = ((ir - adi.indx['p'].start) == (ic - lcdi.indx['p'].start)).all()
+    tst.report('p block diagonal:', _ok1)
+    _ok2 = (iv == 1.0).all()
+    tst.report('p block identity:', _ok2)
 
-        _ok0 = (len(ir) == adi.n_dof['p'])
-        self.report('p block size correct:', _ok0)
-        _ok1 = ((ir - adi.indx['p'].start) == (ic - lcdi.indx['p'].start)).all()
-        self.report('p block diagonal:', _ok1)
-        _ok2 = (iv == 1.0).all()
-        self.report('p block identity:', _ok2)
+    ok = ok and _ok0 and _ok1 and _ok2
 
-        ok = ok and _ok0 and _ok1 and _ok2
+    assert ok
 
-        return ok
+def test_laplace_shifted_periodic(output_dir):
+    from sfepy.mesh.mesh_generators import gen_block_mesh
+    from sfepy.discrete.fem import FEDomain
+    from examples.diffusion.laplace_shifted_periodic import run
 
-    def test_laplace_shifted_periodic(self):
-        from sfepy.mesh.mesh_generators import gen_block_mesh
-        from sfepy.discrete.fem import FEDomain
-        from examples.diffusion.laplace_shifted_periodic import run
+    dims = [2.0, 1.0]
+    shape = [21, 11]
+    centre = [0.0, 0.0]
+    mesh = gen_block_mesh(dims, shape, centre, name='block-fem')
+    domain = FEDomain('test_laplace_shifted_periodic', mesh)
 
-        dims = [2.0, 1.0]
-        shape = [21, 11]
-        centre = [0.0, 0.0]
-        mesh = gen_block_mesh(dims, shape, centre, name='block-fem')
-        fe_domain = FEDomain('domain', mesh)
+    pb, state = run(domain, 3, output_dir=output_dir)
 
-        pb, state = run(fe_domain, 3)
+    gamma3 = pb.domain.regions['Gamma3']
+    gamma4 = pb.domain.regions['Gamma4']
 
-        gamma3 = pb.domain.regions['Gamma3']
-        gamma4 = pb.domain.regions['Gamma4']
+    field = pb.fields['fu']
 
-        field = pb.fields['fu']
+    # Check that the shift equals to one.
+    i3 = field.get_dofs_in_region(gamma3, merge=True)
+    i4 = field.get_dofs_in_region(gamma4, merge=True)
 
-        # Check that the shift equals to one.
-        i3 = field.get_dofs_in_region(gamma3, merge=True)
-        i4 = field.get_dofs_in_region(gamma4, merge=True)
+    i_corners = nm.array([0, shape[0] - 1])
+    ii = nm.setdiff1d(nm.arange(len(i3)), i_corners)
 
-        i_corners = nm.array([0, shape[0] - 1])
-        ii = nm.setdiff1d(nm.arange(len(i3)), i_corners)
+    vals = state()
 
-        vals = state()
+    shift = vals[i3] - vals[i4]
 
-        shift = vals[i3] - vals[i4]
+    ok = (shift[i_corners] == 0.0).all()
 
-        ok = (shift[i_corners] == 0.0).all()
+    ok = ok and nm.allclose(shift[ii], 1.0, rtol=0.0, atol=1e-14)
 
-        ok = ok and nm.allclose(shift[ii], 1.0, rtol=0.0, atol=1e-14)
+    if not ok:
+        tst.report('wrong shift:', shift)
 
-        if not ok:
-            self.report('wrong shift:', shift)
+    assert ok
 
-        return ok
