d3a311335e89143fa4ec3bbe63efb9d0bb0f598d
Robert Cimrman <cimrman3@ntc.zcu.cz>
2022-07-28 11:21:21 +0200

    new sfepy-mesh entry point in setup.py

--- setup.py
+++ setup.py
@@ -223,6 +223,7 @@ def setup_package():
               entry_points={
                   'console_scripts': [
                       'sfepy-convert=sfepy.scripts.convert_mesh:main',
+                      'sfepy-mesh=sfepy.scripts.gen_mesh:main',
                       'sfepy-probe=sfepy.scripts.probe:main',
                       'sfepy-run=sfepy.scripts.simple:main',
                       'sfepy-view=sfepy.scripts.resview:main',
