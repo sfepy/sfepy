96227592fcbd9ac30febc9f102fcf51bdfaae5e7
Robert Cimrman <cimrman3@ntc.zcu.cz>
2022-08-01 19:44:05 +0200

    gen_mesh.py: add show_mesh_info.py as subcommand in main()

--- sfepy/scripts/gen_mesh.py
+++ sfepy/scripts/gen_mesh.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 """
-Simple mesh generators.
+Simple mesh generators and statistics.
 """
 from argparse import ArgumentParser
 
-from . import blockgen, cylindergen, gen_iga_patch
+from . import blockgen, cylindergen, gen_iga_patch, show_mesh_info
 
 def main():
     parser = ArgumentParser(description=__doc__)
@@ -26,6 +26,10 @@ def main():
     parser2 = subparsers.add_parser('iga-patch', help='generate an IGA patch')
     parser2.set_defaults(fun=gen_iga_patch.gen_iga_patch)
     gen_iga_patch.add_args(parser2)
+
+    parser3 = subparsers.add_parser('info', help='show various mesh statistics')
+    parser3.set_defaults(fun=show_mesh_info.show_mesh_info)
+    show_mesh_info.add_args(parser3)
 
     options = parser.parse_args()
 
