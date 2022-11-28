2608e17d0f8ba2b277fc1edcafc84bbf4cf2decf
Robert Cimrman <cimrman3@ntc.zcu.cz>
2022-11-28 14:14:20 +0100

    new ElastodynamicsPIDTSC, new .__init__(), .__call__()
    - update docstrings

--- sfepy/solvers/ts_controllers.py
+++ sfepy/solvers/ts_controllers.py
@@ -1,3 +1,6 @@
+"""
+Time step controllers.
+"""
 import numpy as nm
 
 from sfepy.base.base import Struct
@@ -48,7 +51,10 @@ class TimesSequenceTSC(TimeStepController):
 
 class ElastodynamicsBasicTSC(TimeStepController):
     """
-    Adaptive time step controller for elastodynamics by [1].
+    Adaptive time step I-controller for elastodynamics.
+
+    The implementation is based on [1]. The default parameters correspond to
+    the PID-Controller as implemented in ``tsc.ed_pid`` with P=D=0, I=1.
 
     [1] Grafenhorst, Matthias, Joachim Rang, and Stefan Hartmann.
     “Time-Adaptive Finite Element Simulations of Dynamical Problems for
@@ -115,5 +121,73 @@ class ElastodynamicsBasicTSC(TimeStepController):
         else:
             new_dt = dt * max(fmin, fsafety / nm.sqrt(emax))
             status.result = 'reject'
+
+        return new_dt, status
+
+class ElastodynamicsPIDTSC(ElastodynamicsBasicTSC):
+    """
+    Adaptive time step PID controller for elastodynamics.
+
+    The implementation is based on [1], [2] (PI Controller) and [3] (PID).
+    The default parameters correspond to the I-Controller as implemented in
+    ``tsc.ed_basic``.
+
+    [1] Grafenhorst, Matthias, Joachim Rang, and Stefan Hartmann.
+    “Time-Adaptive Finite Element Simulations of Dynamical Problems for
+    Temperature-Dependent Materials.” Journal of Mechanics of Materials and
+    Structures 12, no. 1 (November 26, 2016): 57–91.
+    https://doi.org/10.2140/jomms.2017.12.57.
+    [2] Hairer, Ernst, Syvert Paul Nørsett, and Gerhard Wanner. Solving
+    Ordinary Differential Equations II: Stiff and Differential-Algebraic
+    Problems. Springer Science & Business Media, 1993.
+    [3] Söderlind, Gustaf. “Digital Filters in Adaptive Time-Stepping.” ACM
+    Transactions on Mathematical Software 29, no. 1 (March 1, 2003): 1–26.
+    https://doi.org/10.1145/641876.641877.
+    """
+    name = 'tsc.ed_pid'
+
+    _parameters = ElastodynamicsBasicTSC._parameters + [
+        ('pcoef', 'float', 0.0, False,
+         'Proportional (P) coefficient of the step size control.'),
+        ('icoef', 'float', 1.0, False,
+         'Intregral (I) coefficient of the step size control.'),
+        ('dcoef', 'float', 0.0, False,
+         'Derivative (D) coefficient of the step size control.'),
+        ('error_order', 'float', 2, False,
+         'The order of the solver error estimate.'),
+    ]
+
+    def __init__(self, conf, **kwargs):
+        ElastodynamicsBasicTSC.__init__(self, conf=conf, **kwargs)
+
+        self.emax0 = 1.0
+        self.emax00 = 1.0
+
+    def __call__(self, ts, vec0, vec1, unpack, **kwargs):
+        conf = self.conf
+        dt = ts.dt
+        fmin, fmax, fsafety, pcoef, icoef, dcoef, error_order = (
+            conf.fmin, conf.fmax, conf.fsafety,
+            conf.pcoef, conf.icoef, conf.dcoef,
+            conf.error_order,
+        )
+        b1 = -(pcoef + icoef + dcoef) / error_order
+        b2 = (pcoef + 2 * dcoef) / error_order
+        b3 = -dcoef / error_order
+        u_err, v_err = self.get_scaled_errors(
+            dt, vec0, vec1, conf.eps_a, conf.eps_r, unpack,
+        )
+        emax = max(u_err, v_err)
+
+        c1 = 1 if b1 == 0 else emax**b1
+        c2 = 1 if b2 == 0 else self.emax0**b2
+        c3 = 1 if b3 == 0 else self.emax00**b3
+        new_dt = dt * min(fmax, max(fmin, fsafety * c1 * c2 * c3))
+
+        status = Struct(u_err=u_err, v_err=v_err, emax=emax)
+        status.result = 'accept' if emax <= 1 else 'reject'
+
+        self.emax00 = self.emax0
+        self.emax0 = emax
 
         return new_dt, status
