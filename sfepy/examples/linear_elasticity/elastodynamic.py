65a4416cf1a165a2a412657bc628d191e4fc4ef6
Robert Cimrman <cimrman3@ntc.zcu.cz>
2022-11-29 11:25:29 +0100

    illustrate time step control in elastodynamic.py example, new define()

--- sfepy/examples/linear_elasticity/elastodynamic.py
+++ sfepy/examples/linear_elasticity/elastodynamic.py
@@ -41,9 +41,24 @@ Solve using the Bathe method::
 
   sfepy-run sfepy/examples/linear_elasticity/elastodynamic.py -O "ts='tsb'"
 
-View the resulting deformation using:
+View the resulting displacements on the deforming mesh (1000x magnified),
+Cauchy strain and stress using::
 
-  sfepy-view output/ed/user_block.h5 -f u:wu:p0 1:vw:p0 cauchy_strain:p1 cauchy_stress:p2 -s 18
+  sfepy-view output/ed/user_block.h5 -f u:wu:f1e3:p0 1:vw:p0 cauchy_strain:p1 cauchy_stress:p2
+
+Solve in 2D using the explicit Velocity-Verlet method with adaptive
+time-stepping and save all time steps (see ``plot_times.py`` use below)::
+
+  sfepy-run sfepy/examples/linear_elasticity/elastodynamic.py -d "dims=(5e-3, 5e-3), shape=(61, 61), tss_name='tsvv', adaptive=True, save_times='all'"
+
+View the resulting velocities on the deforming mesh (1000x magnified) using::
+
+  sfepy-view output/ed/user_block.h5 -2 --grid-vector1=1.2,0,0 -f du:wu:f1e3:p0 1:vw:p0
+
+Plot the adaptive time steps (available at times according to 'save_times'
+option!)::
+
+  python3 sfepy/scripts/plot_times.py output/ed/user_block.h5 -l
 """
 from __future__ import absolute_import
 
@@ -53,243 +68,249 @@ import sfepy.mechanics.matcoefs as mc
 from sfepy.discrete.fem.meshio import UserMeshIO
 from sfepy.mesh.mesh_generators import gen_block_mesh
 
-plane = 'strain'
-dim = 3
-
-# Material parameters.
-E = 200e9
-nu = 0.3
-rho = 7800.0
-
-lam, mu = mc.lame_from_youngpoisson(E, nu, plane=plane)
-# Longitudinal and shear wave propagation speeds.
-cl = nm.sqrt((lam + 2.0 * mu) / rho)
-cs = nm.sqrt(mu / rho)
-
-# Initial velocity.
-v0 = 1.0
-
-# Mesh dimensions and discretization.
-d = 2.5e-3
-if dim == 3:
-    L = 4 * d
-    dims = [L, d, d]
-
-    shape = [21, 6, 6]
-    #shape = [101, 26, 26]
-
-else:
-    L = 2 * d
-    dims = [L, 2 * d]
-
-    shape = [61, 61]
-    # shape = [361, 361]
-
-# Element size.
-H = L / (shape[0] - 1)
-
-# Time-stepping parameters.
-# Note: the Courant number C0 =  dt * cl / H
-dt = H / cl # C0 = 1
-
-if dim == 3:
-    t1 = 0.9 * L / cl
-
-else:
-    t1 = 1.5 * d / cl
-
-def mesh_hook(mesh, mode):
+def define(
+        E=200e9, nu=0.3, rho=7800,
+        plane='strain',
+        dims=(1e-2, 2.5e-3, 2.5e-3),
+        shape=(21, 6, 6),
+        v0=1.0,
+        ct1=1.5,
+        tss_name='tsn',
+        adaptive=False,
+        ls_name='lsd',
+        save_times=20,
+):
     """
-    Generate the block mesh.
+    Parameters
+    ----------
+    E, nu, rho: material parameters
+    plane: plane strain or stress hypothesis
+    dims: physical dimensions of the block (L, d, x)
+    shape: numbers of mesh vertices along each axis
+    v0: initial impact velocity
+    ct1: final time in L / "longitudinal wave speed" units
+    tss_name: time stepping solver name (see "solvers" section)
+    adaptive: use adaptive time step control
+    ls_name: linear system solver name (see "solvers" section)
+    save_times: number of solutions to save
     """
-    if mode == 'read':
-        mesh = gen_block_mesh(dims, shape, 0.5 * nm.array(dims),
-                              name='user_block', verbose=False)
-        return mesh
+    dim = len(dims)
 
-    elif mode == 'write':
-        pass
+    lam, mu = mc.lame_from_youngpoisson(E, nu, plane=plane)
+    # Longitudinal and shear wave propagation speeds.
+    cl = nm.sqrt((lam + 2.0 * mu) / rho)
+    cs = nm.sqrt(mu / rho)
 
-def post_process(out, problem, state, extend=False):
-    """
-    Calculate and output strain and stress for given displacements.
-    """
-    from sfepy.base.base import Struct
+    # Element size.
+    L, d = dims[:2]
+    H = L / (shape[0] - 1)
 
-    ev = problem.evaluate
-    strain = ev('ev_cauchy_strain.i.Omega(u)', mode='el_avg', verbose=False)
-    stress = ev('ev_cauchy_stress.i.Omega(solid.D, u)', mode='el_avg',
-                copy_materials=False, verbose=False)
+    # Time-stepping parameters.
+    # Note: the Courant number C0 =  dt * cl / H
+    dt = H / cl # C0 = 1
 
-    out['cauchy_strain'] = Struct(name='output_data', mode='cell',
-                                  data=strain, dofs=None)
-    out['cauchy_stress'] = Struct(name='output_data', mode='cell',
-                                  data=stress, dofs=None)
+    t1 = ct1 * L / cl
 
-    return out
+    def mesh_hook(mesh, mode):
+        """
+        Generate the block mesh.
+        """
+        if mode == 'read':
+            mesh = gen_block_mesh(dims, shape, 0.5 * nm.array(dims),
+                                  name='user_block', verbose=False)
+            return mesh
 
-filename_mesh = UserMeshIO(mesh_hook)
+        elif mode == 'write':
+            pass
 
-regions = {
-    'Omega' : 'all',
-    'Impact' : ('vertices in (x < 1e-12)', 'facet'),
-}
-if dim == 3:
-    regions.update({
-        'Symmetry-y' : ('vertices in (y < 1e-12)', 'facet'),
-        'Symmetry-z' : ('vertices in (z < 1e-12)', 'facet'),
-    })
+    def post_process(out, problem, state, extend=False):
+        """
+        Calculate and output strain and stress for given displacements.
+        """
+        from sfepy.base.base import Struct
 
-# Iron.
-materials = {
-    'solid' : ({
-            'D': mc.stiffness_from_youngpoisson(dim=dim, young=E, poisson=nu,
-                                                plane=plane),
-            'rho': rho,
-     },),
-}
+        ev = problem.evaluate
+        strain = ev('ev_cauchy_strain.i.Omega(u)', mode='el_avg', verbose=False)
+        stress = ev('ev_cauchy_stress.i.Omega(solid.D, u)', mode='el_avg',
+                    copy_materials=False, verbose=False)
 
-fields = {
-    'displacement': ('real', 'vector', 'Omega', 1),
-}
+        out['cauchy_strain'] = Struct(name='output_data', mode='cell',
+                                      data=strain)
+        out['cauchy_stress'] = Struct(name='output_data', mode='cell',
+                                      data=stress)
 
-integrals = {
-    'i' : 2,
-}
+        return out
 
-variables = {
-    'u' : ('unknown field', 'displacement', 0),
-    'du' : ('unknown field', 'displacement', 1),
-    'ddu' : ('unknown field', 'displacement', 2),
-    'v' : ('test field', 'displacement', 'u'),
-    'dv' : ('test field', 'displacement', 'du'),
-    'ddv' : ('test field', 'displacement', 'ddu'),
-}
+    filename_mesh = UserMeshIO(mesh_hook)
 
-ebcs = {
-    'Impact' : ('Impact', {'u.0' : 0.0, 'du.0' : 0.0, 'ddu.0' : 0.0}),
-}
-if dim == 3:
-    ebcs.update({
-        'Symmtery-y' : ('Symmetry-y',
-                        {'u.1' : 0.0, 'du.1' : 0.0, 'ddu.1' : 0.0}),
-        'Symmetry-z' : ('Symmetry-z',
-                        {'u.2' : 0.0, 'du.2' : 0.0, 'ddu.2' : 0.0}),
-    })
+    regions = {
+        'Omega' : 'all',
+        'Impact' : ('vertices in (x < 1e-12)', 'facet'),
+    }
+    if dim == 3:
+        regions.update({
+            'Symmetry-y' : ('vertices in (y < 1e-12)', 'facet'),
+            'Symmetry-z' : ('vertices in (z < 1e-12)', 'facet'),
+        })
 
-def get_ic(coor, ic, mode='u'):
-    val = nm.zeros_like(coor)
-    if mode == 'u':
-        val[:, 0] = 0.0
+    # Iron.
+    materials = {
+        'solid' : ({
+                'D': mc.stiffness_from_youngpoisson(dim=dim, young=E, poisson=nu,
+                                                    plane=plane),
+                'rho': rho,
+         },),
+    }
 
-    elif mode == 'du':
-        val[:, 0] = -1.0
+    fields = {
+        'displacement': ('real', 'vector', 'Omega', 1),
+    }
 
-    return val
+    integrals = {
+        'i' : 2,
+    }
 
-functions = {
-    'get_ic_u' : (get_ic,),
-    'get_ic_du' : (lambda coor, ic: get_ic(coor, None, mode='du'),),
-}
+    variables = {
+        'u' : ('unknown field', 'displacement', 0),
+        'du' : ('unknown field', 'displacement', 1),
+        'ddu' : ('unknown field', 'displacement', 2),
+        'v' : ('test field', 'displacement', 'u'),
+        'dv' : ('test field', 'displacement', 'du'),
+        'ddv' : ('test field', 'displacement', 'ddu'),
+    }
 
-ics = {
-    'ic' : ('Omega', {'u.all' : 'get_ic_u', 'du.all' : 'get_ic_du'}),
-}
+    ebcs = {
+        'Impact' : ('Impact', {'u.0' : 0.0, 'du.0' : 0.0, 'ddu.0' : 0.0}),
+    }
+    if dim == 3:
+        ebcs.update({
+            'Symmtery-y' : ('Symmetry-y',
+                            {'u.1' : 0.0, 'du.1' : 0.0, 'ddu.1' : 0.0}),
+            'Symmetry-z' : ('Symmetry-z',
+                            {'u.2' : 0.0, 'du.2' : 0.0, 'ddu.2' : 0.0}),
+        })
 
-equations = {
-    'balance_of_forces' :
-    """dw_dot.i.Omega(solid.rho, ddv, ddu)
-     + dw_zero.i.Omega(dv, du)
-     + dw_lin_elastic.i.Omega(solid.D, v, u) = 0""",
-}
+    def get_ic(coor, ic, mode='u'):
+        val = nm.zeros_like(coor)
+        if mode == 'u':
+            val[:, 0] = 0.0
 
-solvers = {
-    'ls' : ('ls.auto_direct', {
-        # Reuse the factorized linear system from the first time step.
-        'use_presolve' : True,
-        # Speed up the above by omitting the matrix digest check used normally
-        # for verification that the current matrix corresponds to the
-        # factorized matrix stored in the solver instance. Use with care!
-        'use_mtx_digest' : False,
-    }),
-    'ls-i' : ('ls.petsc', {
-        'method' : 'cg',
-        'precond' : 'icc',
-        'i_max' : 150,
-        'eps_a' : 1e-32,
-        'eps_r' : 1e-8,
-        'verbose' : 2,
-    }),
-    'newton' : ('nls.newton', {
-        'i_max'      : 1,
-        'eps_a'      : 1e-6,
-        'eps_r'      : 1e-6,
-    }),
-    'tsvv' : ('ts.velocity_verlet', {
-        # Excplicit method -> requires at least 10x smaller dt than the other
-        # time-stepping solvers.
-        't0' : 0.0,
-        't1' : t1,
-        'dt' : dt,
-        'n_step' : None,
+        elif mode == 'du':
+            val[:, 0] = -1.0
 
-        'is_linear'  : True,
+        return val
 
-        'verbose' : 1,
-    }),
-    'tsn' : ('ts.newmark', {
-        't0' : 0.0,
-        't1' : t1,
-        'dt' : dt,
-        'n_step' : None,
+    functions = {
+        'get_ic_u' : (get_ic,),
+        'get_ic_du' : (lambda coor, ic: get_ic(coor, None, mode='du'),),
+    }
 
-        'is_linear'  : True,
+    ics = {
+        'ic' : ('Omega', {'u.all' : 'get_ic_u', 'du.all' : 'get_ic_du'}),
+    }
 
-        'beta' : 0.25,
-        'gamma' : 0.5,
+    equations = {
+        'balance_of_forces' :
+        """dw_dot.i.Omega(solid.rho, ddv, ddu)
+         + dw_zero.i.Omega(dv, du)
+         + dw_lin_elastic.i.Omega(solid.D, v, u) = 0""",
+    }
 
-        'verbose' : 1,
-    }),
-    'tsga' : ('ts.generalized_alpha', {
-        't0' : 0.0,
-        't1' : t1,
-        'dt' : dt,
-        'n_step' : None,
+    solvers = {
+        'lsd' : ('ls.auto_direct', {
+            # Reuse the factorized linear system from the first time step.
+            'use_presolve' : True,
+            # Speed up the above by omitting the matrix digest check used normally
+            # for verification that the current matrix corresponds to the
+            # factorized matrix stored in the solver instance. Use with care!
+            'use_mtx_digest' : False,
+        }),
+        'lsi' : ('ls.petsc', {
+            'method' : 'cg',
+            'precond' : 'icc',
+            'i_max' : 150,
+            'eps_a' : 1e-32,
+            'eps_r' : 1e-8,
+            'verbose' : 2,
+        }),
+        'newton' : ('nls.newton', {
+            'i_max'      : 1,
+            'eps_a'      : 1e-6,
+            'eps_r'      : 1e-6,
+        }),
+        'tsvv' : ('ts.velocity_verlet', {
+            # Excplicit method -> requires at least 10x smaller dt than the other
+            # time-stepping solvers, or an adaptive time step control.
+            't0' : 0.0,
+            't1' : t1,
+            'dt' : dt,
+            'n_step' : None,
 
-        'is_linear'  : True,
+            'is_linear'  : True,
 
-        'rho_inf' : 0.5,
-        'alpha_m' : None,
-        'alpha_f' : None,
-        'beta' : None,
-        'gamma' : None,
+            'verbose' : 1,
+        }),
+        'tsn' : ('ts.newmark', {
+            't0' : 0.0,
+            't1' : t1,
+            'dt' : dt,
+            'n_step' : None,
 
-        'verbose' : 1,
-    }),
-    'tsb' : ('ts.bathe', {
-        't0' : 0.0,
-        't1' : t1,
-        'dt' : dt,
-        'n_step' : None,
+            'is_linear'  : True,
 
-        'is_linear'  : True,
+            'beta' : 0.25,
+            'gamma' : 0.5,
 
-        'verbose' : 1,
-    }),
-}
+            'verbose' : 1,
+        }),
+        'tsga' : ('ts.generalized_alpha', {
+            't0' : 0.0,
+            't1' : t1,
+            'dt' : dt,
+            'n_step' : None,
 
-options = {
-    'ts' : 'tsn',
-    # 'ts' : 'tsb',
-    'nls' : 'newton',
-    # 'ls' : 'ls-i',
-    'ls' : 'ls',
+            'is_linear'  : True,
 
-    'save_times' : 20,
+            'rho_inf' : 0.5,
+            'alpha_m' : None,
+            'alpha_f' : None,
+            'beta' : None,
+            'gamma' : None,
 
-    'active_only' : False,
+            'verbose' : 1,
+        }),
+        'tsb' : ('ts.bathe', {
+            't0' : 0.0,
+            't1' : t1,
+            'dt' : dt,
+            'n_step' : None,
 
-    'output_format' : 'h5',
-    'output_dir' : 'output/ed',
-    'post_process_hook' : 'post_process',
-}
+            'is_linear'  : True,
+
+            'verbose' : 1,
+        }),
+        'tscedb' : ('tsc.ed_basic', {
+            'eps_r' : (1e-4, 1e-1),
+            'eps_a' : (1e-8, 5e-2),
+            'fmin' : 0.3,
+            'fmax' : 2.5,
+            'fsafety' : 0.85,
+        }),
+    }
+
+    options = {
+        'ts' : tss_name,
+        'tsc' : 'tscedb' if adaptive else None,
+        'nls' : 'newton',
+        'ls' : ls_name,
+
+        'save_times' : save_times,
+
+        'active_only' : False,
+
+        'output_format' : 'h5',
+        'output_dir' : 'output/ed',
+        'post_process_hook' : 'post_process',
+    }
+
+    return locals()
