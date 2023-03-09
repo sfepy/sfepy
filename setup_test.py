fa74cf056de3b3400a928727512c42e36540df0d
Yosef Meller <yosefm@gmail.com>
2023-03-09 18:49:21 +0200

    Feature completion

--- setup_test.py
+++ setup_test.py
@@ -14,9 +14,9 @@ from setuptools import find_packages
 
 import sys
 sys.path.append('./tools')
-from build_helpers import INFO, cmdclass
+from build_helpers import INFO, cmdclass, log, package_check
 
-from sfepy import config
+from sfepy import config, version
 
 DOCLINES = __doc__.split("\n")
 
@@ -38,31 +38,88 @@ Operating System :: Microsoft :: Windows
 
 DOWNLOAD_URL = "http://sfepy.org/doc-devel/downloads.html"
 
-install_requires = [
-    'matplotlib',
-    'meshio',
-    'numpy',
-    'pyparsing',
-    'pyvista',
-    'scipy',
-    'sympy',
-    'tables',
-]
+# BEFORE importing distutils, remove MANIFEST. distutils doesn't properly
+# update it when the contents of directories change.
+if os.path.exists('MANIFEST'): os.remove('MANIFEST')
 
-# Create version.h file.
-# There is probably a way to do it with CMake but we'll get to it later.
-filename_in = 'sfepy/discrete/common/extmods/version.h.in'
-filename_out = 'sfepy/discrete/common/extmods/version.h'
-fdi = open(filename_in, 'r')
-fdo = open(filename_out, 'w')
-for line in fdi:
-    if line.find('VERSION "0.0.0"') >= 0:
-        aux = line.split()
-        aux[2] = VERSION
-        line = ' '.join(aux) + '\n'
-    fdo.write(line)
-fdi.close()
-fdo.close()
+
+def _cython_version(pkg_name):
+    from Cython.Compiler.Version import version
+
+    return version
+
+
+def _igakit_version(pkg_name):
+    return '0.1'
+
+
+def _pymetis_version(pkg_name):
+    import pymetis
+
+    return pymetis.version
+
+
+def _scikit_umfpack_version(pkg_name):
+    try:
+        import scikits.umfpack; scikits.umfpack
+        try:
+            return scikits.umfpack.__version__
+
+        except AttributeError:
+            return '<0.3.1'
+
+    except:
+        return None
+
+
+def check_versions(show_only=False):
+    # Cython is a build dependency.
+    package_check('cython', INFO.CYTHON_MIN_VERSION,
+                  version_getter=_cython_version,
+                  show_only=show_only)
+
+    # Check hard and soft dependencies.
+    package_check('numpy', INFO.NUMPY_MIN_VERSION,
+                  show_only=show_only)
+    package_check('scipy', INFO.SCIPY_MIN_VERSION,
+                  show_only=show_only)
+    package_check('matplotlib', INFO.MATPLOTLIB_MIN_VERSION,
+                  show_only=show_only)
+    package_check('pyparsing', INFO.PYPARSING_MIN_VERSION,
+                  show_only=show_only)
+    package_check('tables', INFO.PYTABLES_MIN_VERSION,
+                  show_only=show_only)
+    package_check('sympy', INFO.SYMPY_MIN_VERSION, optional=True,
+                  messages={'opt suffix' : '; some tests are going to fail!'},
+                  show_only=show_only)
+    package_check('igakit', INFO.IGAKIT_MIN_VERSION, optional=True,
+                  version_getter=_igakit_version,
+                  show_only=show_only)
+    package_check('petsc4py', INFO.PETSC4PY_MIN_VERSION, optional=True,
+                  show_only=show_only)
+    package_check('mpi4py', INFO.MPI4PY_MIN_VERSION, optional=True,
+                  show_only=show_only)
+    package_check('slepc4py', INFO.SLEPC4PY_MIN_VERSION, optional=True,
+                  show_only=show_only)
+    package_check('pymetis', INFO.PYMETIS_MIN_VERSION, optional=True,
+                  version_getter=_pymetis_version,
+                  show_only=show_only)
+    package_check('scikits.umfpack', INFO.SCIKIT_UMFPACK_MIN_VERSION,
+                  optional=True,
+                  version_getter=_scikit_umfpack_version,
+                  show_only=show_only)
+    package_check('meshio', INFO.MESHIO_MIN_VERSION,
+                  show_only=show_only)
+    package_check('psutil', INFO.PSUTIL_MIN_VERSION, optional=True,
+                  show_only=show_only)
+    package_check('pyvista', INFO.PYVISTA_MIN_VERSION, optional=True,
+                  show_only=show_only)
+    package_check('opt_einsum', INFO.OPT_EINSUM_MIN_VERSION, optional=True,
+                  show_only=show_only)
+    package_check('jax', INFO.JAX_MIN_VERSION, optional=True,
+                  show_only=show_only)
+    package_check('dask', INFO.DASK_MIN_VERSION, optional=True,
+                  show_only=show_only)
 
 
 def data_dir_walk(dir_name: str, prefix: str) -> list[tuple[str, list[str]]]:
@@ -105,31 +162,71 @@ def compose_cmake_args() -> list[str]:
     return cmake_args
 
 
-setup(
-    name='sfepy',
-    maintainer="Robert Cimrman",
-    maintainer_email="cimrman3@ntc.zcu.cz",
-    description=DOCLINES[0],
-    long_description="\n".join(DOCLINES[2:]),
-    url="http://sfepy.org",
-    download_url=DOWNLOAD_URL,
-    license='BSD',
-    classifiers=list(filter(None, CLASSIFIERS.split('\n'))),
-    platforms=["Linux", "Mac OS-X", 'Windows'],
-    entry_points={
-      'console_scripts': [
-          'sfepy-convert=sfepy.scripts.convert_mesh:main',
-          'sfepy-mesh=sfepy.scripts.gen_mesh:main',
-          'sfepy-probe=sfepy.scripts.probe:main',
-          'sfepy-run=sfepy.scripts.simple:main',
-          'sfepy-test=sfepy.scripts.run_tests:main',
-          'sfepy-view=sfepy.scripts.resview:main',
-      ],
-    },
-    install_requires=install_requires,
-    cmdclass=cmdclass,
-    packages=find_packages(),
-    data_files=compose_data_files(),
-    setup_requires=['cython'],
-    cmake_args=compose_cmake_args()
-)
+def setup_package():
+    # Create version.h file.
+    # There is probably a way to do it with CMake but we'll get to it later.
+    filename_in = 'sfepy/discrete/common/extmods/version.h.in'
+    filename_out = 'sfepy/discrete/common/extmods/version.h'
+    fdi = open(filename_in, 'r')
+    fdo = open(filename_out, 'w')
+    for line in fdi:
+        if line.find('VERSION "0.0.0"') >= 0:
+            aux = line.split()
+            aux[2] = VERSION
+            line = ' '.join(aux) + '\n'
+        fdo.write(line)
+    fdi.close()
+    fdo.close()
+
+    install_requires = [
+        'matplotlib',
+        'meshio',
+        'numpy',
+        'pyparsing',
+        'pyvista',
+        'scipy',
+        'sympy',
+        'tables',
+    ]
+
+    setup(
+        name='sfepy',
+        version=version.__version__,
+        maintainer="Robert Cimrman",
+        maintainer_email="cimrman3@ntc.zcu.cz",
+        description=DOCLINES[0],
+        long_description="\n".join(DOCLINES[2:]),
+        url="http://sfepy.org",
+        download_url=DOWNLOAD_URL,
+        license='BSD',
+        classifiers=list(filter(None, CLASSIFIERS.split('\n'))),
+        platforms=["Linux", "Mac OS-X", 'Windows'],
+        entry_points={
+          'console_scripts': [
+              'sfepy-convert=sfepy.scripts.convert_mesh:main',
+              'sfepy-mesh=sfepy.scripts.gen_mesh:main',
+              'sfepy-probe=sfepy.scripts.probe:main',
+              'sfepy-run=sfepy.scripts.simple:main',
+              'sfepy-test=sfepy.scripts.run_tests:main',
+              'sfepy-view=sfepy.scripts.resview:main',
+          ],
+        },
+        install_requires=install_requires,
+        cmdclass=cmdclass,
+        packages=find_packages(),
+        data_files=compose_data_files(),
+        setup_requires=['cython'],
+        cmake_args=compose_cmake_args()
+    )
+
+
+if __name__ == '__main__':
+    check_versions()
+    setup_package()
+
+    from sfepy import Config
+    site_config = Config()
+    log.info('\nUsing Python {}.'.format(site_config.python_version()))
+
+    log.info('\nRequired and optional packages found:\n')
+    check_versions(show_only=True)
