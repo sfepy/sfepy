75e38beb325cfb7e9ed588d2139857d4ccec1896
Vladimir Lukes <vlukes@kme.zcu.cz>
2024-02-06 15:33:18 +0100

    example: update docstring and remove obsolete import

--- sfepy/examples/linear_elasticity/wedge_mesh.py
+++ sfepy/examples/linear_elasticity/wedge_mesh.py
@@ -1,6 +1,7 @@
 r"""
-A linear elastic beam loaded with a continuous force. The FE mesh consists
-of hexehedral and tetrahedral elements.
+A linear elastic beam loaded with a continuous force. The FE meshes consisting
+of hexehedral, tetrahedral, and wedge elements are used in the simulation and
+the results are compared.
 
 The displacement at the beam end is compared to the reference
 solution calculated on the homogeneous hexahedral mesh.
@@ -13,7 +14,6 @@ Viewing the results::
 
     sfepy-view beam_h7.vtk beam_t42.vtk beam_w14.vtk -f u:s0:wu:e:p0 u:s1:wu:e:p0 u:s2:wu:e:p0 --camera-position="1.2,-0.6,0.1,0.4,0.1,-0.1,-0.2,0.1,1"
 """
-from __future__ import absolute_import
 import os.path as osp
 import numpy as nm
 from sfepy import data_dir
