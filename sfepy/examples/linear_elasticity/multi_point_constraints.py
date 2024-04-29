a52ffab09b23d96cb114ad4ab734edfa2259d7e4
Robert Cimrman <cimrman3@ntc.zcu.cz>
2024-04-29 22:41:12 +0200

    add average_force LCBCs to multi_point_constraints.py example

--- sfepy/examples/linear_elasticity/multi_point_constraints.py
+++ sfepy/examples/linear_elasticity/multi_point_constraints.py
@@ -4,7 +4,7 @@ Example usage::
   sfepy-run sfepy/examples/linear_elasticity/multi_point_constraints.py
   sfepy-run sfepy/examples/linear_elasticity/multi_point_constraints.py -d "dofs=(0,1)"
   sfepy-run sfepy/examples/linear_elasticity/multi_point_constraints.py -d "dofs=2"
-  sfepy-view annulus-c.vtk -2
+  sfepy-view annulus-c.vtk -2e
   sfepy-view annulus-c.vtk -2e -f u:wu:f1:p0 1:vw:p0 u:gu:p0
 """
 import numpy as nm
@@ -15,24 +15,34 @@ from sfepy.discrete.fem.mesh import Mesh
 from sfepy.linalg import get_coors_in_ball
 from sfepy.mechanics.matcoefs import stiffness_from_lame
 
-def define(dims=(1, 1, 2, 2, 0), shape=(3, 12, 0), order=1, dofs=(0, 1, 2),
-           output_dir='.'):
+def define(dims=(1, 1, 2, 2, 0), shape=(5, 32, 0), order=1, dofs=(0, 1, 2),
+           is_rot=True, output_dir='.'):
+    if not isinstance(dofs, tuple):
+        dofs = (dofs,)
 
-    if isinstance(dofs, tuple):
-        dofs = ','.join([f'{ii}' for ii in dofs])
+    nuc = 2 + is_rot
+    dofs0 = tuple(set(range(nuc)).difference(dofs))
+
+    sdofs = ','.join([f'{ii}' for ii in dofs])
+    sdofs0 = ','.join([f'{ii}' for ii in dofs0])
 
     def mesh_hook(mesh, mode):
         if mode == 'read':
             _mesh = gen_cylinder_mesh(dims, shape, (0, 0, 0),
                                      make_2d=True)
+            _mesh2 = _mesh.copy()
+            _mesh2.transform_coors(nm.eye(2, dtype=nm.float64) * 2.5)
+            _mesh = _mesh + _mesh2
+
             # Add constraint vertices (and cells) to the base mesh.
             coors, vgroups, conns, mat_ids, descs = _mesh._get_io_data()
             nc = coors.shape[0]
-#            coors = nm.r_[coors, [[0, 0], [0.0, 0.001]]]
-            coors = nm.r_[coors, [[0, 0], [0.001, 0.0]]]
-            vgroups = nm.r_[vgroups, [1, 2]]
-            conns.append([[nc, nc + 1]])
-            mat_ids.append([1])
+
+            coors = nm.r_[coors, nm.zeros((4, 2), dtype=nm.float64)]
+            vgroups = nm.r_[vgroups, [1, 2, 3, 4]]
+            conns.append([[nc, nc + 1],
+                          [nc + 2, nc + 3]])
+            mat_ids.append([1, 2])
             descs.append('1_2')
             mesh = Mesh.from_data('annulus-c',
                                   coors, vgroups, conns, mat_ids, descs)
@@ -47,24 +57,34 @@ def define(dims=(1, 1, 2, 2, 0), shape=(3, 12, 0), order=1, dofs=(0, 1, 2),
         'Omega' : 'all',
         'Gamma1' : ('vertices by get_gamma1', 'facet'),
         'Gamma2' : ('vertices by get_gamma2', 'facet'),
-        'OmegaC' : ('vertices by get_omegac', 'cell', None, {'cell_tdim': 1}),
+        'Gamma3' : ('vertices by get_gamma3', 'facet'),
+        'Gamma4' : ('vertices by get_gamma4', 'facet'),
+        'Omega12C' : ('(vertices of group 1) +v (vertices of group 2)',
+                      'cell', None, {'cell_tdim': 1}),
         'Gamma1C' : ('vertices of group 1', 'vertex'),
         'Gamma2C' : ('vertices of group 2', 'vertex'),
+        'Omega34C' : ('(vertices of group 3) +v (vertices of group 4)',
+                      'cell', None, {'cell_tdim': 1}),
+        'Gamma3C' : ('vertices of group 3', 'vertex'),
+        'Gamma4C' : ('vertices of group 4', 'vertex'),
+        'OmegaC' : ('r.Omega12C +c r.Omega34C', 'cell', None, {'cell_tdim': 1})
     }
 
     centre = [0, 0]
     functions = {
-        'get_omegac' : (lambda coors, domain:
-                        get_coors_in_ball(coors, centre, 0.1),),
         'get_gamma1' : (lambda coors, domain:
-                        get_coors_in_ball(coors, centre, 1.1),),
+                        get_coors_in_ball(coors, centre, 1.1, 0.9),),
         'get_gamma2' : (lambda coors, domain:
-                        get_coors_in_ball(coors, centre, 1.9, inside=False),),
+                        get_coors_in_ball(coors, centre, 2.1, 1.9),),
+        'get_gamma3' : (lambda coors, domain:
+                        get_coors_in_ball(coors, centre, 2.6, 2.4),),
+        'get_gamma4' : (lambda coors, domain:
+                        get_coors_in_ball(coors, centre, 5.1, 4.9),),
     }
 
     fields = {
         'fu' : ('real', 2, 'Omega', order),
-        'fc' : ('real', 3, 'OmegaC', 1),
+        'fc' : ('real', nuc, 'OmegaC', 1),
     }
 
     variables = {
@@ -75,18 +95,31 @@ def define(dims=(1, 1, 2, 2, 0), shape=(3, 12, 0), order=1, dofs=(0, 1, 2),
     }
 
     ebcs = {
-        'u0' : ('Gamma2', {'u.all' : 0.0}),
-        'uc' : ('Gamma2C', {f'uc.[{dofs}]' : 0.1},),
+        'uc' : ('Gamma1C', {f'uc.[{sdofs}]' : 0.1},),
     }
+    if dofs0:
+        ebcs.update({
+            'uc0' : ('Gamma1C', {f'uc.[{sdofs0}]' : 0.0},),
+        })
+    if not is_rot:
+        ebcs.update({
+            'u0' : ('Gamma4', {'u.all' : 0.0}),
+        })
 
     lcbcs = {
-        'rigid' : (('Gamma1', 'Gamma1C'), {'u.all' : 'uc.all'}, None, 'rigid2'),
+        'rigid' : (('Gamma1', 'Gamma2C'), {'u.all' : 'uc.all'},
+                   None, 'rigid2'),
+        'avg1' : (('Gamma2', 'Gamma3C'), {'u.all' : 'uc.all'},
+                  None, 'average_force'),
+        'avg2' : (('Gamma3', 'Gamma4C'), {'u.all' : 'uc.all'},
+                  None, 'average_force'),
     }
 
     materials = {
         'm' : ({
             'D' : stiffness_from_lame(dim=2, lam=1e1, mu=1e0),
-            'ks' : [[1e+5], [1e+5], [1e+5]],
+            'ks' : [[1e+5], [1e+5], [1e+5]][:nuc],
+            'dvec' : [[0.01], [0.01]],
         },),
     }
 
@@ -94,13 +127,25 @@ def define(dims=(1, 1, 2, 2, 0), shape=(3, 12, 0), order=1, dofs=(0, 1, 2),
         'i' : 2 * order,
     }
 
-    equations = {
-        'eq1' :
-        """dw_lin_elastic.i.Omega(m.D, v, u)
-         + dw_lin_dspring_rot.0.OmegaC(m.ks, vc, uc)
-         = 0
-        """,
-    }
+    if is_rot:
+        equations = {
+            'eq1' :
+            """dw_lin_elastic.i.Omega(m.D, v, u)
+             + dw_lin_dspring_rot.0.Omega12C(m.dvec, m.ks, vc, uc)
+             + dw_lin_dspring_rot.0.Omega34C(m.dvec, m.ks, vc, uc)
+             = 0
+            """,
+        }
+
+    else:
+        equations = {
+            'eq1' :
+            """dw_lin_elastic.i.Omega(m.D, v, u)
+             + dw_lin_dspring.0.Omega12C(m.dvec, m.ks, vc, uc)
+             + dw_lin_dspring.0.Omega34C(m.dvec, m.ks, vc, uc)
+             = 0
+            """,
+        }
 
     solvers = {
         'ls' : ('ls.auto_direct', {}),
