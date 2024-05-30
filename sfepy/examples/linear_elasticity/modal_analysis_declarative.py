c6d10a49bfd6891b97deca6cf621b97c559f21e7
Vladimir Lukes <vlukes@kme.zcu.cz>
2024-05-30 08:43:08 +0200

    update declarative example

--- sfepy/examples/linear_elasticity/modal_analysis_declarative.py
+++ sfepy/examples/linear_elasticity/modal_analysis_declarative.py
@@ -107,6 +107,7 @@ def define(n_eigs=6, approx_order=1, density=7850., young=210e9, poisson=0.3):
             'which': 'LM',
             'sigma': 0,
             'tol': 1e-8,
+            'linear_solver': 'cholesky',
             'maxiter': 1000,
         }),
     }
