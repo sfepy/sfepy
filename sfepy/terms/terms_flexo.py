2ed9ed3286ca3b4b842b86bbfb5fa03a4f8ac704
Robert Cimrman <cimrman3@ntc.zcu.cz>
2024-06-12 16:50:54 +0200

    new MixedFlexoCouplingTerm (de_m_flexo_coupling)

--- sfepy/terms/terms_flexo.py
+++ sfepy/terms/terms_flexo.py
@@ -61,3 +61,46 @@ class MixedStrainGradElasticTerm(ETermBase):
             return self.make_function(
                 'Ii,i', (aux, 'aux1'), state, mode=mode, diff_var=None,
             )
+
+class MixedFlexoCouplingTerm(ETermBase):
+    r"""
+    Flexoelectric coupling term, mixed formulation.
+
+    :Definition:
+
+    .. math::
+        \int_{\Omega} f_{ijkl}\ e_{jk,l}(\ull{\delta w}) \nabla_i p \\
+        \int_{\Omega} f_{ijkl}\ e_{jk,l}(\ull{w}) \nabla_i q
+
+    :Arguments 1:
+        - material: :math:`f_{ijkl}`
+        - virtual/parameter_t: :math:`\ull{\delta w}`
+        - state/parameter_s: :math:`p`
+
+    :Arguments 2:
+        - material: :math:`f_{ijkl}`
+        - state    : :math:`\ull{w}`
+        - virtual  : :math:`q`
+    """
+    name = 'de_m_flexo_coupling'
+    arg_types = (('material', 'virtual', 'state'),
+                 ('material', 'state', 'virtual'),
+                 ('material', 'parameter_t', 'parameter_s'))
+    arg_shapes = [{'material' : 'D, SD',
+                   'virtual/dw-p' : ('D2', None), 'state/dw-p' : 1,
+                   'virtual/dp-w' : (1, None), 'state/dp-w' : 'D2',
+                   'parameter_t' : 'D2', 'parameter_s' : 1}]
+    modes = ('dw-p', 'dp-w', 'eval')
+
+    def get_function(self, mat, tvar, svar, mode=None, term_mode=None,
+                     diff_var=None, **kwargs):
+        aux = make_grad2strain()
+        n_qp = mat.shape[1]
+        mat = mat.reshape((-1, n_qp, 3, 3, 6))
+
+        fun = self.make_function(
+            'jkI,Ii,i.k,0.j',
+            (mat, 'mat'), (aux, 'aux'), tvar, svar, diff_var=diff_var,
+        )
+
+        return fun
