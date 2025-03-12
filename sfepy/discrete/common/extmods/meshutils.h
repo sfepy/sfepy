ff6df223e9762edb3833c713f7db1d6a9d84b38e
Robert Cimrman <cimrman3@ntc.zcu.cz>
2025-03-12 19:58:41 +0100

    remove graph_components() from cmesh.pyx, meshutils.[ch]

--- sfepy/discrete/common/extmods/meshutils.h
+++ sfepy/discrete/common/extmods/meshutils.h
@@ -13,10 +13,4 @@ int32 orient_elements(int32 *flag, int32 flag_n_row,
                       int32 *swap_to,
                       int32 swap_to_n_row, int32 swap_to_n_col);
 
-int32 graph_components(int32 *p_n_comp,
-                       int32 *flag, int32 flag_len,
-                       int32 *row, int32 row_len,
-                       int32 *col, int32 col_len,
-                       int32 *pos, int32 pos_len);
-
 #endif /* Header */
