751dc1587159961fb754eed6f123307ea849fc03
Vladimir Lukes <vlukes@kme.zcu.cz>
2026-02-24 17:56:01 +0100

    simplify example: use 'dw_ul_he_by_fun' term

--- sfepy/examples/homogenization/nonlinear_hyperelastic_mM.py
+++ sfepy/examples/homogenization/nonlinear_hyperelastic_mM.py
@@ -1,138 +1,103 @@
 """
 Homogenized nonlinear hyperelastic material with evolving microstructure
 deformation in each macroscopic quadrature point.
-
-Run in parallel using::
-
-  mpiexec -n 4 sfepy-run --app=bvp-mM --debug-mpi sfepy/examples/homogenization/nonlinear_hyperelastic_mM.py
 """
 import numpy as nm
-
+from functools import partial
 from sfepy import data_dir, base_dir
 from sfepy.base.base import Struct, output
-from sfepy.terms.terms_hyperelastic_ul import HyperElasticULFamilyData
-from sfepy.homogenization.micmac import get_homog_coefs_nonlinear
+from sfepy.homogenization.micmac import (get_homog_coefs_nonlinear,
+    get_homogen_app_form_cache)
 import sfepy.linalg as la
 from sfepy.discrete.evaluate import Evaluator
 
-hyperelastic_data = {}
-
+hyperelastic_data = {'coefs': {}}
 
 def post_process(out, pb, state, extend=False):
-    if isinstance(state, dict):
-        pass
-    else:
-        pb.update_materials_flag = 2
-        stress = pb.evaluate('ev_integrate_mat.1.Omega(solid.S, u)',
-                             mode='el_avg')
+    ev = partial(pb.evaluate, mode='el_avg',
+                 get_homog_mat=pb.conf.get_homog_mat)
+    stress = ev('dw_ul_he_by_fun.1.Omega(get_homog_mat, v, u)',
+                term_mode='stress')
+    strain = ev('dw_ul_he_by_fun.1.Omega(get_homog_mat, v, u)',
+                term_mode='strain')
 
-        out['cauchy_stress'] = Struct(name='output_data',
-                                      mode='cell',
-                                      data=stress,
-                                      dofs=None)
+    out['cauchy_stress'] = Struct(name='output_data', mode='cell',
+                                  data=stress)
+    out['green_strain'] = Struct(name='output_data', mode='cell',
+                                 data=strain)
 
-        strain = pb.evaluate('ev_integrate_mat.1.Omega(solid.E, u)',
-                             mode='el_avg')
-
-        out['green_strain'] = Struct(name='output_data',
-                                     mode='cell',
-                                     data=strain,
-                                     dofs=None)
-
-        pb.update_materials_flag = 0
-
-        if pb.conf.options.get('recover_micro', False):
-            happ = pb.homogen_app
-            if pb.ts.step == 0:
+    if pb.conf.options.get('recover_micro', False):
+        happ = get_homogen_app_form_cache(pb.conf.options['micro_filename'])
+        if pb.ts.step == 0:
+            rname = pb.conf.options.recovery_region
+            rcells = pb.domain.regions[rname].get_cells()
+            happ.app_options.store_micro_idxs = strain.shape[1] * rcells
+        else:
+            hpb = happ.problem
+            recovery_hook = hpb.conf.options.get('recovery_hook', None)
+            if recovery_hook is not None:
+                recovery_hook = hpb.conf.get_function(recovery_hook)
                 rname = pb.conf.options.recovery_region
-                rcells = pb.domain.regions[rname].get_cells()
-                sh = hyperelastic_data['homog_mat_shape']
+                rcoors = []
+                for ii in happ.app_options.store_micro_idxs:
+                    key = happ.get_micro_cache_key('coors', ii, pb.ts.step)
+                    if key in happ.micro_state_cache:
+                        rcoors.append(happ.micro_state_cache[key])
 
-                happ.app_options.store_micro_idxs = sh[1] * rcells
-            else:
-                hpb = happ.problem
-                recovery_hook = hpb.conf.options.get('recovery_hook', None)
-                if recovery_hook is not None:
-                    recovery_hook = hpb.conf.get_function(recovery_hook)
-                    rname = pb.conf.options.recovery_region
-                    rcoors = []
-                    for ii in happ.app_options.store_micro_idxs:
-                        key = happ.get_micro_cache_key('coors', ii, pb.ts.step)
-                        if key in happ.micro_state_cache:
-                            rcoors.append(happ.micro_state_cache[key])
-
-                    recovery_hook(hpb, rcoors, pb.domain.regions[rname], pb.ts)
+                recovery_hook(hpb, rcoors, pb.domain.regions[rname], pb.ts)
 
     return out
 
 
-def get_homog_mat(ts, coors, mode, term=None, problem=None, **kwargs):
-    if problem.update_materials_flag == 2 and mode == 'qp':
-        out = hyperelastic_data['homog_mat']
-        return {k: nm.array(v) for k, v in out.items()}
-    elif problem.update_materials_flag == 0 or not mode == 'qp':
-        return
+def get_homog_mat(family_data, mode):
+    pb = hyperelastic_data['problem']
+    ts = pb.get_timestepper()
+    output(f'macro mat. fun: step={ts.step}, iiter={pb.iiter}')
 
-    output('get_homog_mat')
-    dim = problem.domain.mesh.dim
+    ckey = (ts.step, pb.iiter)
+    ccache = hyperelastic_data['coefs']
+    n_el, n_qp, dim, _ = family_data.mtx_f.shape
+    sym = family_data.green_strain.shape[2]
+    dim2 = dim**2
 
-    update_var = problem.conf.options.mesh_update_variables[0]
-    state_u = problem.equations.variables[update_var]
-    state_u.field.clear_mappings()
-    family_data = problem.family_data(state_u, term.region, term.integral,
-                                      term.geometry_types['u'])
+    if ckey not in ccache:
+        ccache.clear()
 
-    mtx_f = family_data.mtx_f.reshape((coors.shape[0],)
-                                      + family_data.mtx_f.shape[-2:])
+        mtx_f = family_data.mtx_f.reshape((n_el * n_qp, dim, dim))
+        if hasattr(pb, 'mtx_f_prev'):
+            rel_mtx_f = la.dot_sequences(mtx_f, nm.linalg.inv(pb.mtx_f_prev),
+                                         'AB')
+        else:
+            rel_mtx_f = mtx_f
 
-    if hasattr(problem, 'mtx_f_prev'):
-        rel_mtx_f = la.dot_sequences(mtx_f, nm.linalg.inv(problem.mtx_f_prev),
-                                     'AB')
+        pb.mtx_f_prev = mtx_f.copy()
+
+        macro_data = {'mtx_e': rel_mtx_f - nm.eye(dim)}
+        ccache[ckey] = get_homog_coefs_nonlinear(ts, mtx_f, 'qp', macro_data,
+                                                 problem=pb,
+                                                 iteration=pb.iiter)
+
+    coefs = ccache[ckey]
+
+    if mode == 'tan_mod':
+        out = coefs['A'].reshape((n_el, n_qp, dim2, dim2))
+    elif mode == 'stress':
+        out = coefs['S'].reshape((n_el, n_qp, sym, 1))
     else:
-        rel_mtx_f = mtx_f
-
-    problem.mtx_f_prev = mtx_f.copy()
-
-    macro_data = {'mtx_e': rel_mtx_f - nm.eye(dim)}  # '*' - macro strain
-    out = get_homog_coefs_nonlinear(ts, coors, mode, macro_data,
-                                    term=term, problem=problem,
-                                    iteration=problem.iiter, **kwargs)
-
-    out['E'] = 0.5 * (la.dot_sequences(mtx_f, mtx_f, 'ATB') - nm.eye(dim))
-
-    hyperelastic_data['time'] = ts.step
-    hyperelastic_data['homog_mat_shape'] = family_data.det_f.shape[:2]
-    hyperelastic_data['homog_mat'] = \
-        {k: nm.array(v) for k, v in out.items()}
+        raise ValueError()
 
     return out
 
 
 def ulf_iteration_hook(pb, nls, vec, it, err, err0):
     Evaluator.new_ulf_iteration(pb, nls, vec, it, err, err0)
-
     pb.iiter = it
-    pb.update_materials_flag = True
-    pb.update_materials()
-    pb.update_materials_flag = False
-
-
-class MyEvaluator(Evaluator):
-    def eval_residual(self, vec, is_full=False):
-        if not is_full:
-            vec = self.problem.equations.make_full_vec(vec)
-        vec_r = self.problem.equations.eval_residuals(vec * 0)
-
-        return vec_r
 
 
 def ulf_init(pb):
-    pb.family_data = HyperElasticULFamilyData()
-    pb_vars = pb.get_variables()
-    pb_vars['u'].init_data()
-
-    pb.update_materials_flag = True
+    pb.domain.mesh.coors_act = pb.domain.mesh.coors.copy()
     pb.iiter = 0
+    hyperelastic_data['problem'] = pb
 
 
 options = {
@@ -142,15 +107,12 @@ options = {
     'pre_process_hook': ulf_init,
     'micro_filename': (base_dir +
                        '/examples/homogenization/nonlinear_homogenization.py'),
+    'post_process_hook': post_process,
     'recover_micro': True,
     'recovery_region': 'Recovery',
-    'post_process_hook': post_process,
-    'user_evaluator': MyEvaluator,
 }
 
-materials = {
-    'solid': 'get_homog',
-}
+materials = {}
 
 fields = {
     'displacement': ('real', 'vector', 'Omega', 1),
@@ -193,13 +155,10 @@ def move_bottom(ts, coor, **kwargs):
 
 functions = {
     'move_bottom': (move_bottom,),
-    'get_homog': (get_homog_mat,),
 }
 
 equations = {
-    'balance_of_forces':
-    """dw_nonsym_elastic.1.Omega(solid.A, v, u)
-    = - dw_lin_prestress.1.Omega(solid.S, v)""",
+    'balance_of_forces': 'dw_ul_he_by_fun.1.Omega(get_homog_mat, v, u) = 0'
 }
 
 solvers = {
