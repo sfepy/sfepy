2fa83dd87b68b0812eaf47f86823d0cd2f582056
Robert Cimrman <cimrman3@ntc.zcu.cz>
2026-03-12 15:27:31 +0100

    gen_mesh.py: add combine_meshes.py as subcommand in main(), add descriptions

--- sfepy/scripts/gen_mesh.py
+++ sfepy/scripts/gen_mesh.py
@@ -2,9 +2,10 @@
 """
 Simple mesh generators and statistics.
 """
-from argparse import ArgumentParser
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
 
-from . import blockgen, cylindergen, gen_iga_patch, show_mesh_info
+from . import (blockgen, cylindergen, gen_iga_patch, combine_meshes,
+               show_mesh_info)
 
 def main():
     parser = ArgumentParser(description=__doc__)
@@ -15,21 +16,35 @@ def main():
                                        dest='mesh_kind',
                                        required=True)
 
-    parser0 = subparsers.add_parser('block', help='generate a block mesh')
-    parser0.set_defaults(fun=blockgen.gen_block)
-    blockgen.add_args(parser0)
+    sub = subparsers.add_parser('block', help='generate a block mesh',
+                                description=blockgen.__doc__,
+                                formatter_class=RawDescriptionHelpFormatter)
+    sub.set_defaults(fun=blockgen.gen_block)
+    blockgen.add_args(sub)
 
-    parser1 = subparsers.add_parser('cylinder', help='generate a cylinder mesh')
-    parser1.set_defaults(fun=cylindergen.gen_cylinder)
-    cylindergen.add_args(parser1)
+    sub = subparsers.add_parser('cylinder', help='generate a cylinder mesh',
+                                description=cylindergen.__doc__,
+                                formatter_class=RawDescriptionHelpFormatter)
+    sub.set_defaults(fun=cylindergen.gen_cylinder)
+    cylindergen.add_args(sub)
 
-    parser2 = subparsers.add_parser('iga-patch', help='generate an IGA patch')
-    parser2.set_defaults(fun=gen_iga_patch.gen_iga_patch)
-    gen_iga_patch.add_args(parser2)
+    sub = subparsers.add_parser('iga-patch', help='generate an IGA patch',
+                                description=gen_iga_patch.__doc__,
+                                formatter_class=RawDescriptionHelpFormatter)
+    sub.set_defaults(fun=gen_iga_patch.gen_iga_patch)
+    gen_iga_patch.add_args(sub)
 
-    parser3 = subparsers.add_parser('info', help='show various mesh statistics')
-    parser3.set_defaults(fun=show_mesh_info.show_mesh_info)
-    show_mesh_info.add_args(parser3)
+    sub = subparsers.add_parser('combine', help='combine meshes',
+                                description=combine_meshes.__doc__,
+                                formatter_class=RawDescriptionHelpFormatter)
+    sub.set_defaults(fun=combine_meshes.combine_meshes)
+    combine_meshes.add_args(sub)
+
+    sub = subparsers.add_parser('info', help='show various mesh statistics',
+                                description=show_mesh_info.__doc__,
+                                formatter_class=RawDescriptionHelpFormatter)
+    sub.set_defaults(fun=show_mesh_info.show_mesh_info)
+    show_mesh_info.add_args(sub)
 
     options = parser.parse_args()
 
